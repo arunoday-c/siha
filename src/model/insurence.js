bba6d359313142d9692f1267e6fa98e80055a8e1
unknown <irfan.algaeh@gmail.com>
2018-07-12 13:40:49 +0530

    changes in get patient insurence API

--- src/model/insurence.js
+++ src/model/insurence.js
@@ -32,10 +32,11 @@ let getPatientInsurence = (req, res, next) => {
       extend(patientInsurenceModel, req.query);
 
       connection.query(
-        "(select  mIns.patient_id,mIns.primary_insurance_provider_id,Ins.insurance_provider_name,\
-          mIns.primary_sub_id, sIns.insurance_sub_name,\
-          mIns.primary_network_id,  net.network_type,netoff.policy_number,mIns.primary_card_number,mIns.primary_inc_card_path,\
-         mIns.primary_effective_start_date,mIns.primary_effective_end_date  from ((((\
+        "(select  mIns.patient_id,mIns.primary_insurance_provider_id as insurance_provider_id,Ins.insurance_provider_name,\
+          mIns.primary_sub_id as sub_insurance_provider_id, sIns.insurance_sub_name as sub_insurance_provider_name,\
+          mIns.primary_network_id as network_id,  net.network_type,netoff.policy_number,mIns.primary_card_number as card_number,\
+          mIns.primary_inc_card_path as insurance_card_path,\
+         mIns.primary_effective_start_date as effective_start_date,mIns.primary_effective_end_date as effective_end_date  from ((((\
           hims_d_insurance_provider Ins \
           INNER JOIN  hims_m_patient_insurance_mapping mIns ON mIns.primary_insurance_provider_id=Ins.hims_d_insurance_provider_id)\
            INNER JOIN  hims_d_insurance_sub sIns ON mIns.primary_sub_id= sIns.hims_d_insurance_sub_id) \
