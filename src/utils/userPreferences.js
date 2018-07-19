c84a3c63c8d6e1dea4be5ed474aea1bcfe1c6f66
Syed Noor <syednoor@Syeds-MacBook-Air.local>
2018-07-19 10:31:58 +0530

    spotlight search bug fix

--- src/utils/userPreferences.js
+++ src/utils/userPreferences.js
@@ -2,13 +2,22 @@ import loki from "lokijs";
 import extend from "extend";
 import path from "path";
 import fs from "fs";
+import httpStatus from "../utils/httpStatus";
 let saveUserPreferences = (req, res, next) => {
+  let header = req.headers["x-app-user-identity"];
+
+  if (header == null) {
+    next(httpStatus.generateError(httpStatus.badRequest, "unknown user.."));
+  }
+  header = JSON.parse(header);
   let settings = extend(
     {
       dbName: "algaeh_users"
     },
     req.body
   );
+
+  settings.userId = header.user_id;
   let db = new loki("algaeh_users.json", { autoload: true });
   let userCollection = db.addCollection("User_" + settings.userId, {
     disableMeta: true
@@ -24,13 +33,23 @@ let saveUserPreferences = (req, res, next) => {
     records: "done"
   });
 };
-let getUserPreferences = (req, res) => {
+let getUserPreferences = (req, res, next) => {
+  let header = req.headers["x-app-user-identity"];
+
+  if (header == null) {
+    next(httpStatus.generateError(httpStatus.badRequest, "unknown user.."));
+  }
+  header = JSON.parse(header);
+
   let settings = extend(
     {
       dbName: "algaeh_users"
     },
     req.query
   );
+
+  settings.userId = header.user_id;
+
   let filePath = path.join(__dirname, "../../algaeh_users.json");
 
   if (!fs.existsSync(filePath)) {
@@ -44,7 +63,12 @@ let getUserPreferences = (req, res) => {
   db.loadJSON(fs.readFileSync(filePath));
 
   let userCollection = db.getCollection("User_" + settings.userId);
-
+  if (userCollection == null) {
+    res.status(200).json({
+      success: true,
+      records: {}
+    });
+  }
   let row;
   if (settings.identifier != null) {
     row = userCollection.where(obj => {
@@ -55,10 +79,12 @@ let getUserPreferences = (req, res) => {
     });
     if (row != null && row.length != 0) row = row[0];
   } else {
-    row = userCollection.where(obj => {
-      return obj.screenName == settings.screenName;
+    res.status(400).json({
+      success: false,
+      message: "Please provide identifier"
     });
   }
+
   res.status(200).json({
     success: true,
     records: row
