82269388ee63b6a7355db4636bccde4d734b843a
unknown <irfan.algaeh@gmail.com>
2018-07-26 13:15:58 +0530

    added ICD master

---
+++ src/EHR/controller/episode.js
@@ -0,0 +1,25 @@
+import { Router } from "express";
+import { releaseConnection } from "../../utils";
+import httpStatus from "../../utils/httpStatus";
+import { addIcd } from "../model/episode";
+
+export default ({ config, db }) => {
+  let api = Router();
+  //code
+
+  // created by irfan : add ICD
+  api.post(
+    "/addIcd",
+    addIcd,
+    (req, res, next) => {
+      let result = req.records;
+      res.status(httpStatus.ok).json({
+        success: true,
+        records: result
+      });
+      next();
+    },
+    releaseConnection
+  );
+  return api;
+};
