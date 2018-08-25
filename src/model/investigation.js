0091c560fa7f6b8255e1984dc7b235275a64a513
unknown <irfan.algaeh@gmail.com>
2018-08-25 15:28:27 +0530

    added api for adding invesgtigation

--- src/model/investigation.js
+++ src/model/investigation.js
@@ -57,11 +57,14 @@ let addInvestigationTest = (req, res, next) => {
             next(error);
           });
         }
-
+        //hims_d_investigation_test_id, short_description, description, investigation_type, lab_section_id,
+        // send_out_test, available_in_house, restrict_order, restrict_by, external_facility_required,
+        // facility_description, priority, cpt_id, category_id,
+        //film_category, screening_test, film_used, created_date, created_by, updated_date, updated_by, record_status
         connection.query(
           "insert into hims_d_investigation_test(short_description,description,investigation_type,lab_section_id,\
           send_out_test,available_in_house,restrict_order,restrict_by,\
-          external_facility_required,facility_description,priority,cpt_id,category_id,created_by,updated_by)values(\
+          external_facility_required,facility_description,priority,cpt_id,category_id,film_category, screening_test, film_used,created_by,updated_by)values(\
           ?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
           [
             input.short_description,
@@ -77,6 +80,9 @@ let addInvestigationTest = (req, res, next) => {
             input.priority,
             input.cpt_id,
             input.category_id,
+            input.film_category,
+            input.screening_test,
+            input.film_used,
             input.created_by,
             input.updated_by
           ],
