340a83e529f1d7822abe5d4c695d35e6b1988899
unknown <irfan.algaeh@gmail.com>
2018-09-12 17:21:54 +0530

    fixed get patient insurace api

--- src/model/insurance.js
+++ src/model/insurance.js
@@ -55,8 +55,8 @@ let getPatientInsurance = (req, res, next) => {
         INNER JOIN hims_d_insurance_network net ON mIns.primary_network_id=net.hims_d_insurance_network_id)\
         INNER JOIN hims_d_insurance_network_office netoff ON mIns.primary_policy_num=netoff.policy_number) where mIns.patient_id=?  and mIns.patient_visit_id =?\
         GROUP BY mIns.primary_policy_num)  AS A\
-        ,\
-         (select  mIns.secondary_insurance_provider_id , \
+        left join\
+        (select mIns.patient_id, mIns.patient_visit_id, mIns.secondary_insurance_provider_id , \
          Ins.insurance_provider_name as secondary_insurance_provider_name,\
          mIns.secondary_sub_id as secondary_sub_insurance_provider_id,\
          sIns.insurance_sub_name as secondary_sub_insurance_provider_name, \
@@ -69,8 +69,7 @@ let getPatientInsurance = (req, res, next) => {
          INNER JOIN  hims_d_insurance_sub sIns ON mIns.secondary_sub_id= sIns.hims_d_insurance_sub_id) \
          INNER JOIN hims_d_insurance_network net ON mIns.secondary_network_id=net.hims_d_insurance_network_id)\
          INNER JOIN hims_d_insurance_network_office netoff ON mIns.secondary_policy_num=netoff.policy_number) where mIns.patient_id=? and mIns.patient_visit_id =?\
-         GROUP BY mIns.secondary_policy_num) AS B  ;",
-
+         GROUP BY mIns.secondary_policy_num) AS B  on A.patient_id=B.patient_id ;",
           [
             patientInsuranceModel.patient_id,
             patientInsuranceModel.patient_visit_id,
@@ -85,7 +84,8 @@ let getPatientInsurance = (req, res, next) => {
               });
             }
             req.records = result;
-
+            debugLog("output", result);
+            debugLog("er", error);
             next();
           }
         );
@@ -124,7 +124,6 @@ let getPatientInsurance = (req, res, next) => {
               });
             }
             req.records = result;
-
             next();
           }
         );
