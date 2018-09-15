dc0b55a53eba87e55c9288acd1153815aed63e5a
unknown <irfan.algaeh@gmail.com>
2018-09-15 12:03:31 +0530

    get prescription api

--- src/controller/orderMedication.js
+++ src/controller/orderMedication.js
@@ -3,7 +3,10 @@ import { releaseConnection } from "../utils";
 import httpStatus from "../utils/httpStatus";
 import { LINQ } from "node-linq";
 
-import { addPatientPrescription } from "../model/orderMedication";
+import {
+  addPatientPrescription,
+  getPatientPrescription
+} from "../model/orderMedication";
 
 export default ({ config, db }) => {
   let api = Router();
@@ -22,5 +25,19 @@ export default ({ config, db }) => {
     releaseConnection
   );
 
+  // created by irfan: to  getPatientPrescription
+  api.get(
+    "/getPatientPrescription",
+    getPatientPrescription,
+    (req, res, next) => {
+      let result = req.records;
+      res.status(httpStatus.ok).json({
+        success: true,
+        records: result
+      });
+      next();
+    },
+    releaseConnection
+  );
   return api;
 };
--- src/model/orderMedication.js
+++ src/model/orderMedication.js
@@ -64,8 +64,6 @@ let addPatientPrescription = (req, res, next) => {
             // debugLog("Results are recorded...");
 
             if (results.insertId != null) {
-              debugLog("  inserted id ", results.insertId);
-              debugLog(" body ", req.body);
               req.body.prescription_id = results.insertId;
 
               const insurtColumns = [
@@ -119,4 +117,51 @@ let addPatientPrescription = (req, res, next) => {
   }
 };
 
-module.exports = { addPatientPrescription };
+//created by irfan: getPatientPrescription
+let getPatientPrescription = (req, res, next) => {
+  let selectWhere = {
+    provider_id: "ALL",
+    patient_id: "ALL",
+    prescription_date: "ALL"
+  };
+
+  try {
+    if (req.db == null) {
+      next(httpStatus.dataBaseNotInitilizedError());
+    }
+    let db = req.db;
+    req.query["date(prescription_date)"] = req.query.prescription_date;
+
+    delete req.query.prescription_date;
+
+    let where = whereCondition(extend(selectWhere, req.query));
+
+    db.getConnection((error, connection) => {
+      if (error) {
+        next(error);
+      }
+      db.query(
+        "SELECT H.hims_f_prescription_id,H.patient_id, H.encounter_id, H.provider_id, H.episode_id,\
+        H.prescription_date,H.prescription_status,H.cancelled,D.hims_f_prescription_detail_id, D.prescription_id, D.item_id, D.generic_id, D.dosage,\
+        D.frequency, D.no_of_days,D.dispense, D.frequency_type, D.frequency_time, D.start_date, D.item_status \
+        from hims_f_prescription H,hims_f_prescription_detail D WHERE H.hims_f_prescription_id = D.prescription_id and " +
+          where.condition,
+        where.values,
+
+        (error, result) => {
+          releaseDBConnection(db, connection);
+          if (error) {
+            next(error);
+          }
+
+          req.records = result;
+          next();
+        }
+      );
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports = { addPatientPrescription, getPatientPrescription };
