c22d6ea01f1f31bc15f66edf910ff28e3c941db7
adnan-algaeh <adnan.algaeh@gmail.com>
2018-09-22 09:46:58 +0530

    Appointment Rooms and Clinics Structure

---
+++ client/src/Components/AppointmentSetup/AppointmentRoomAndClinics/AppointmentRoomAndClinics.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./appt_room_clinics";
+
+class AppointmentRoomAndClinics extends Component {
+  render() {
+    return <div className="appt-room-clinics" />;
+  }
+}
+
+export default AppointmentRoomAndClinics;
