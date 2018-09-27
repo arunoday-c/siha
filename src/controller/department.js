5bfba0c34b10ccd4d9ced691173b2a03df768d23
unknown <irfan.algaeh@gmail.com>
2018-09-27 12:16:10 +0530

    changes in selectDoctorsAndClinic

--- src/controller/department.js
+++ src/controller/department.js
@@ -174,7 +174,7 @@ export default ({ config, db }) => {
         let firstItem = new LINQ(departmets[item]).FirstOrDefault();
         let subDept = new Object();
         subDept["department_id"] = firstItem.department_id;
-        subDept["sub_department_id"] = firstItem.sub_department_id;
+        subDept["sub_dept_id"] = firstItem.sub_dept_id;
         subDept["sub_department_name"] = firstItem.sub_department_name;
         subDept["arabic_sub_department_name"] =
           firstItem.arabic_sub_department_name;
@@ -186,7 +186,7 @@ export default ({ config, db }) => {
       doc_keys.forEach((item, index) => {
         let firstItem = new LINQ(doctors[item]).FirstOrDefault();
         let doc = new Object();
-        doc["employee_id"] = firstItem.employee_id;
+        doc["provider_id"] = firstItem.provider_id;
         doc["full_name"] = firstItem.full_name;
         doc["arabic_name"] = firstItem.arabic_name;
         doc["services_id"] = firstItem.services_id;
