62ff760f3d54f8d2f00686014e81b8ffb601fb5e
unknown <irfan.algaeh@gmail.com>
2018-09-27 11:25:34 +0530

    changed api get doctors commission

--- src/model/doctorsCommission.js
+++ src/model/doctorsCommission.js
@@ -50,12 +50,12 @@ let getDoctorsCommission = (req, res, next) => {
             }
 
             connection.query(
-              "select hims_f_billing_header_id, hims_f_billing_details_id, service_type_id as servtype_id, services_id as service_id,\
-              quantity, unit_cost ,gross_amount as extended_cost,discount_amout as discount_amount,net_amout as net_amount,\
-              patient_payable as patient_share,company_payble as company_share,sec_company_paybale\
-                from hims_f_billing_details where record_status='A' and " +
+              "select BD.hims_f_billing_header_id,hims_f_billing_details_id,BH.bill_number,BH.bill_date,service_type_id as servtype_id,services_id as service_id,quantity,\
+              unit_cost,gross_amount as extended_cost,discount_amout as discount_amount,net_amout as net_amount,BD.patient_payable as patient_share,company_payble as company_share,sec_company_paybale\
+              from hims_f_billing_details BD,hims_f_billing_header BH where BH.record_status='A' and BD.record_status='A' and \
+               BD.hims_f_billing_header_id=BH.hims_f_billing_header_id and " +
                 service_type_id +
-                " hims_f_billing_header_id in (" +
+                "BD.hims_f_billing_header_id in (" +
                 bill_header_id_all +
                 ");",
               (error, results) => {
