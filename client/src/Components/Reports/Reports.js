fa439e1c972108cb43d765532380b2a912345322
adnan-algaeh <adnan.algaeh@gmail.com>
2018-10-24 12:01:34 +0530

    Reports File added in the menu and Reports.js file created

---
+++ client/src/Components/Reports/Reports.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./reports.css";
+
+class Reports extends Component {
+  render() {
+    return <div className="reports">REPORTS</div>;
+  }
+}
+
+export default Reports;
