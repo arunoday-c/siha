8a8e3e04188e44699dabde0335cd3f939a38a604
--replace-all <irfan.algaeh@gmail.com>
2018-11-03 17:08:49 +0530

    changes in get patient appointment

--- src/model/appointment.js
+++ src/model/appointment.js
@@ -2626,8 +2626,8 @@ let getPatientAppointment = (req, res, next) => {
       connection.query(
         "select hims_f_patient_appointment_id,patient_id,patient_code,provider_id,sub_department_id,number_of_slot,appointment_date,\
             appointment_from_time,appointment_to_time,appointment_status_id,patient_name,arabic_name,date_of_birth,age,\
-        contact_number,email,send_to_provider,gender,confirmed,\
-        confirmed_by,comfirmed_date,cancelled,cancelled_by,cancelled_date,cancel_reason\
+        contact_number,email,send_to_provider,gender,confirmed,visit_created,\
+        confirmed_by,comfirmed_date,cancelled,cancelled_by,cancelled_date,appointment_remarks,cancel_reason,is_stand_by\
         from hims_f_patient_appointment where record_status='A' and " +
           selectDoctor +
           "" +
