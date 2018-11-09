67d55a2396f9bba51a0155406846cc48b0b26fd2
--replace-all <irfan.algaeh@gmail.com>
2018-11-09 15:15:48 +0530

    changes in module.exports = { getVisitWiseBillDetailS };

--- src/model/invoiceGeneration.js
+++ src/model/invoiceGeneration.js
@@ -8,11 +8,11 @@ import {
 } from "../utils";
 //import moment from "moment";
 import httpStatus from "../utils/httpStatus";
-//import { LINQ } from "node-linq";
+import { LINQ } from "node-linq";
 import { debugLog } from "../utils/logging";
 
 //created by irfan: to getVisitWiseBillDetailS
-let getVisitWiseBillDetailS = (req, res, next) => {
+let getVisitWiseBillDetailS_BACKUP = (req, res, next) => {
   try {
     if (req.db == null) {
       next(httpStatus.dataBaseNotInitilizedError());
@@ -64,6 +64,62 @@ let getVisitWiseBillDetailS = (req, res, next) => {
                 }
               );
             }
+          } else {
+            releaseDBConnection(db, connection);
+            req.records = result;
+            next();
+          }
+        }
+      );
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+//created by irfan: to getVisitWiseBillDetailS
+let getVisitWiseBillDetailS = (req, res, next) => {
+  try {
+    if (req.db == null) {
+      next(httpStatus.dataBaseNotInitilizedError());
+    }
+    let db = req.db;
+
+    db.getConnection((error, connection) => {
+      connection.query(
+        "select hims_f_billing_header_id, patient_id, visit_id from hims_f_billing_header where record_status='A' and visit_id=?",
+        [req.query.visit_id],
+        (error, result) => {
+          if (error) {
+            releaseDBConnection(db, connection);
+            next(error);
+          }
+
+          if (result.length > 0) {
+            let bill_header_ids = new LINQ(result)
+              .Select(s => s.hims_f_billing_header_id)
+              .ToArray();
+
+            connection.query(
+              "select hims_f_billing_details_id, hims_f_billing_header_id, service_type_id, services_id, quantity,\
+                  unit_cost, insurance_yesno, gross_amount, discount_amout, discount_percentage, net_amout, copay_percentage,\
+                  copay_amount, deductable_amount, deductable_percentage, tax_inclusive, patient_tax, company_tax, total_tax,\
+                    patient_resp, patient_payable, comapany_resp, company_payble, sec_company, sec_deductable_percentage, \
+                    sec_deductable_amount, sec_company_res, sec_company_tax, sec_company_paybale, sec_copay_percntage,\
+                    sec_copay_amount, pre_approval, commission_given from hims_f_billing_details where record_status='A'\
+                    and hims_f_billing_header_id in (?)",
+              [bill_header_ids],
+              (error, detailResult) => {
+                if (error) {
+                  releaseDBConnection(db, connection);
+                  next(error);
+                }
+
+                releaseDBConnection(db, connection);
+                req.records = detailResult;
+                next();
+              }
+            );
           } else {
             releaseDBConnection(db, connection);
             req.records = result;
