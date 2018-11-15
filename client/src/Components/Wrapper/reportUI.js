374f99412710fdea42dfdd2a6c946dbf548f1040
syed noor <syednoor.algaeh@gmail.com>
2018-11-15 16:30:42 +0530

    Mongoose Setup Partial and Doc Mgmt Module Setup

--- client/src/Components/Wrapper/reportUI.js
+++ client/src/Components/Wrapper/reportUI.js
@@ -97,94 +97,96 @@ export default class ReportUI extends Component {
       }
     });
   }
+
   render() {
     if (this.state.hasError) {
       return null;
     }
-
-    return (
-      <Modal open={this.state.openPopup}>
-        <div className="algaeh-modal">
-          <div className="popupHeader">
-            <div className="row">
-              <div className="col-lg-8">
-                <h4>Print Preview</h4>
-              </div>
-              <div className="col-lg-4">
-                <button
-                  type="button"
-                  className=""
-                  onClick={this.handleClose.bind(this)}
-                >
-                  <i className="fas fa-times-circle" />
-                </button>
-              </div>
-            </div>
-          </div>
-          <div />
-          <div>
-            {this.props.options !== undefined &&
-            this.props.options.plotUI !== undefined ? (
-              <div id="report_generation_interface">
-                {this.props.options.plotUI.paramters()}
-
-                <button
-                  style={{ textAlign: "center", margin: "16px" }}
-                  className="btn btn-primary"
-                  onClick={this.generateReport.bind(this)}
-                >
-                  Generate Report
-                </button>
-              </div>
-            ) : null}
-            {this.props.plotui !== undefined ? this.props.plotui : null}
-          </div>
-          <div
-            className="popupInner "
-            ref={el => (this.algehPrintRef = el)}
-            {...this.state._htmlString}
-          >
-            <div className="col-lg-12">
+    if (this.state.openPopup)
+      return (
+        <div className="algaehModalWrapper">
+          <div className="algaeh-modal">
+            <div className="popupHeader">
               <div className="row">
-                <div className="col-lg-12 popRightDiv">
-                  {this.props.children ? this.props.children : null}
+                <div className="col-lg-8">
+                  <h4>Print Preview</h4>
+                </div>
+                <div className="col-lg-4">
+                  <button
+                    type="button"
+                    className=""
+                    onClick={this.handleClose.bind(this)}
+                  >
+                    <i className="fas fa-times-circle" />
+                  </button>
                 </div>
               </div>
             </div>
-          </div>
+            <div />
+            <div>
+              {this.props.options !== undefined &&
+              this.props.options.plotUI !== undefined ? (
+                <div id="report_generation_interface">
+                  {this.props.options.plotUI.paramters()}
 
-          <div className="popupFooter">
-            <div className="col-lg-12">
-              <div className="row">
-                <div className="col-lg-4"> &nbsp;</div>
-                <div className="col-lg-8">
-                  <ReactToPrint
-                    trigger={() => (
-                      <button
-                        type="button"
-                        className="btn btn-primary"
-                        onClick={e => {
-                          this.onClose(e);
-                        }}
-                      >
-                        Print
-                      </button>
-                    )}
-                    content={() => this.algehPrintRef}
-                  />
                   <button
-                    type="button"
-                    className="btn btn-default"
-                    onClick={this.handleClose.bind(this)}
+                    style={{ textAlign: "center", margin: "16px" }}
+                    className="btn btn-primary"
+                    onClick={this.generateReport.bind(this)}
                   >
-                    Close
+                    Generate Report
                   </button>
+                </div>
+              ) : null}
+              {this.props.plotui !== undefined ? this.props.plotui : null}
+            </div>
+            <div
+              className="popupInner "
+              ref={el => (this.algehPrintRef = el)}
+              {...this.state._htmlString}
+            >
+              <div className="col-lg-12">
+                <div className="row">
+                  <div className="col-lg-12 popRightDiv">
+                    {this.props.children ? this.props.children : null}
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="popupFooter">
+              <div className="col-lg-12">
+                <div className="row">
+                  <div className="col-lg-4"> &nbsp;</div>
+                  <div className="col-lg-8">
+                    <ReactToPrint
+                      trigger={() => (
+                        <button
+                          type="button"
+                          className="btn btn-primary"
+                          onClick={e => {
+                            this.onClose(e);
+                          }}
+                        >
+                          Print
+                        </button>
+                      )}
+                      content={() => this.algehPrintRef}
+                    />
+                    <button
+                      type="button"
+                      className="btn btn-default"
+                      onClick={this.handleClose.bind(this)}
+                    >
+                      Close
+                    </button>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </Modal>
-    );
+      );
+    else return null;
   }
 }
