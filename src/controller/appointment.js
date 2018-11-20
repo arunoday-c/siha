e5f4487b6459b358f365903999f3b87f73737cb7
irfan72 <irfan.algaeh@gmail.com>
2018-11-20 14:55:53 +0530

    changes in update patient appointment

--- src/controller/appointment.js
+++ src/controller/appointment.js
@@ -332,22 +332,22 @@ export default ({ config, db }) => {
     addPatientAppointment,
     (req, res, next) => {
       let result = req.records;
-if(req.records.slotExist==true)
-{
-  res.status(httpStatus.ok).json({
-    success: false,
-    records: result
-  });
-
-  next();
-}else{
+    if(req.records.slotExist==true)
+    {
       res.status(httpStatus.ok).json({
-        success: true,
+        success: false,
         records: result
       });
 
       next();
-    }
+    }else{
+          res.status(httpStatus.ok).json({
+            success: true,
+            records: result
+          });
+
+          next();
+        }
     },
     releaseConnection
   );
@@ -373,11 +373,22 @@ if(req.records.slotExist==true)
     updatePatientAppointment,
     (req, res, next) => {
       let result = req.records;
+if(req.records.slotExist==true)
+    {
+    res.status(httpStatus.ok).json({
+      success: false,
+      records: result
+    });
+
+       next();
+      }else{
       res.status(httpStatus.ok).json({
         success: true,
         records: result
       });
+
       next();
+    }
     },
     releaseConnection
   );
