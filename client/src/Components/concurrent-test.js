4c55ce0e1396b4318539529e957653510e601d67
syed noor <syednoor.algaeh@gmail.com>
2018-11-22 15:17:54 +0530

    concurrency test

--- client/src/Components/concurrent-test.js
+++ client/src/Components/concurrent-test.js
@@ -2,18 +2,136 @@ import React, { Component } from "react";
 import "./concurrent-test.css";
 
 import {
-  AlgaehDataGrid,
-  AlgaehLabel,
   AlagehFormGroup,
-  AlagehAutoComplete,
-  AlgaehDateHandler
+  AlagehAutoComplete
 } from "../Components/Wrapper/algaehWrapper";
+import { algaehApiCall } from "../utils/algaehApiCall";
 
 class ConcurrentTest extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      methodType: null,
+      enterURL: "",
+      enterHeader: "",
+      enterInput: "",
+      enterConcurrent: 1,
+      maxLines: 1000,
+      appendComponent: [],
+      FireDisabled: false,
+      spinner: ""
+    };
   }
+
+  clearTerminal() {
+    this.setState({ spinner: "fa-spin", appendComponent: [] }, () => {
+      this.setState({ spinner: "" });
+    });
+  }
+
+  HandleMethodTypes(selector) {
+    this.setState({ methodType: selector.value });
+  }
+  HandleMethodTypesClear(name) {
+    this.setState({ methodType: null });
+  }
+  TextHandler(e) {
+    const _name = e.target.name;
+    const _value = e.target.value;
+    if (_name !== undefined) {
+      this.setState({ [_name]: _value });
+    }
+  }
+
+  checkArrayToClear(callback) {
+    let _componentAppend = this.state.appendComponent;
+
+    if (this.state.appendComponent.length >= this.state.maxLines) {
+      _componentAppend = [];
+    }
+
+    _componentAppend.push(
+      "Server Request Started for '" +
+        this.state.enterConcurrent +
+        "' \
+    Concurrent Service Calls  \n  \
+    URI : '" +
+        this.state.enterURL +
+        " ' \
+    \n  INPUT : '" +
+        this.state.enterInput +
+        "' \n  .......... Started Time.........." +
+        new Date().toLocaleTimeString()
+    );
+    this.setState(
+      {
+        FireDisabled: true,
+        appendComponent: _componentAppend,
+        spinner: "fa-spin"
+      },
+      () => {
+        callback();
+      }
+    );
+  }
+
+  runApiCalls(e) {
+    if (this.state.enterURL === "") return;
+    this.checkArrayToClear(() => {
+      const _concorrent = parseInt(this.state.enterConcurrent);
+      const _data =
+        this.state.enterInput !== "" ? JSON.parse(this.state.enterInput) : {};
+      const _that = this;
+      for (let i = 1; i <= _concorrent; i++) {
+        algaehApiCall({
+          timerNotRequired: true,
+          uri: _that.state.enterURL,
+          method:
+            _that.state.methodType === null ? "GET" : _that.state.methodType,
+          data: _data,
+          onSuccess: response => {
+            let _componentAppend = _that.state.appendComponent;
+
+            if (_that.state.appendComponent.length >= _that.state.maxLines) {
+              _componentAppend = [];
+            }
+            _componentAppend.push(
+              "Status '" +
+                response.status +
+                "' (" +
+                response.statusText +
+                ") \n  \
+            INTERNAL STATUS : " +
+                response.data.success +
+                " ................ : " +
+                i +
+                " : ....... Elapsed time  : " +
+                new Date().toLocaleTimeString()
+            );
+            _that.setState({
+              FireDisabled: false,
+              appendComponent: _componentAppend,
+              spinner: ""
+            });
+          },
+          onFailure: error => {
+            let _componentAppend = _that.state.appendComponent;
+
+            if (_that.state.appendComponent.length >= _that.state.maxLines) {
+              _componentAppend = [];
+            }
+            _componentAppend.push("Error  Details : " + JSON.stringify(error));
+            _that.setState({
+              FireDisabled: false,
+              appendComponent: _componentAppend,
+              spinner: ""
+            });
+          }
+        });
+      }
+    });
+  }
+
   render() {
     return (
       <div className="concurrentTest">
@@ -26,18 +144,19 @@ class ConcurrentTest extends Component {
                 selector={{
                   name: "methodType",
                   className: "select-fld",
-                  value: this.state.modules,
+                  value: this.state.methodType,
                   dataSource: {
                     textField: "text",
                     valueField: "value",
                     data: [
                       { value: "GET", text: "GET" },
-                      { value: "GET", text: "GET" },
-                      { value: "GET", text: "GET" }
+                      { value: "POST", text: "POST" },
+                      { value: "PUT", text: "PUT" },
+                      { value: "DELETE", text: "DELETE" }
                     ]
                   },
-                  onChange: null,
-                  onClear: null
+                  onChange: this.HandleMethodTypes.bind(this),
+                  onClear: this.HandleMethodTypesClear.bind(this)
                 }}
               />
               <AlagehFormGroup
@@ -48,12 +167,9 @@ class ConcurrentTest extends Component {
                 textBox={{
                   className: "txt-fld",
                   name: "enterURL",
-                  //value: this.state.batchno,
+                  value: this.state.enterURL,
                   events: {
-                    onChange: null
-                  },
-                  others: {
-                    // disabled: true
+                    onChange: this.TextHandler.bind(this)
                   }
                 }}
               />
@@ -65,12 +181,9 @@ class ConcurrentTest extends Component {
                 textBox={{
                   className: "txt-fld",
                   name: "enterHeader",
-                  //value: this.state.batchno,
+                  value: this.state.enterHeader,
                   events: {
-                    onChange: null
-                  },
-                  others: {
-                    // disabled: true
+                    onChange: this.TextHandler.bind(this)
                   }
                 }}
               />
@@ -82,12 +195,9 @@ class ConcurrentTest extends Component {
                 textBox={{
                   className: "txt-fld",
                   name: "enterInput",
-                  //value: this.state.batchno,
+                  value: this.state.enterInput,
                   events: {
-                    onChange: null
-                  },
-                  others: {
-                    // disabled: true
+                    onChange: this.TextHandler.bind(this)
                   }
                 }}
               />
@@ -99,17 +209,19 @@ class ConcurrentTest extends Component {
                 textBox={{
                   className: "txt-fld",
                   name: "enterConcurrent",
-                  //value: this.state.batchno,
+                  value: this.state.enterConcurrent,
                   events: {
-                    onChange: null
-                  },
-                  others: {
-                    // disabled: true
+                    onChange: this.TextHandler.bind(this)
                   }
                 }}
               />
               <div className="col">
-                <button className="btn btn-primary" style={{ marginTop: 21 }}>
+                <button
+                  className="btn btn-primary"
+                  style={{ marginTop: 21 }}
+                  disabled={this.state.FireDisabled}
+                  onClick={this.runApiCalls.bind(this)}
+                >
                   FIRE
                 </button>
               </div>
@@ -119,13 +231,13 @@ class ConcurrentTest extends Component {
           <div className="portlet portlet-bordered box-shadow-normal margin-bottom-15 margin-top-15 console-log-Cntr">
             <div className="row">
               <div className="col console-log-div">
-                <p>
-                  127.0.0.1 - devteam [22/Nov/2018:05:08:45 +0000] "GET
-                  /api/v1/apiAuth HTTP/1.1" 200 511 "http://localhost:1313/"
-                  "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_1)
-                  AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.102
-                  Safari/537.36"
-                </p>
+                {this.state.appendComponent.map((component, index) => (
+                  <p key={index}>{component}</p>
+                ))}
+                <i
+                  className={"fas fa-sync-alt " + this.state.spinner}
+                  onClick={this.clearTerminal.bind(this)}
+                />
               </div>
             </div>
           </div>
