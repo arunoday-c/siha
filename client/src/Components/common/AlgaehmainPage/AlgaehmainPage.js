521e023dbb6618a4aa80ddc3f0031f4eb74cfde6
Aboobacker Sidhiqe <>
2018-11-23 15:02:31 +0530

    changing appbar

--- client/src/Components/common/AlgaehmainPage/AlgaehmainPage.js
+++ client/src/Components/common/AlgaehmainPage/AlgaehmainPage.js
@@ -322,82 +322,108 @@ class PersistentDrawer extends React.Component {
 
     return (
       <div className="">
+        <nav className="navbar navbar-default">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <button
+                type="button"
+                className="navbar-toggle collapsed"
+                data-toggle="collapse"
+                data-target="#bs-example-navbar-collapse-1"
+                aria-expanded="false"
+              >
+                <span className="sr-only">Toggle navigation</span>
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+                <span className="icon-bar" />
+              </button>
+              <a className="navbar-brand" href="#">
+                Brand
+              </a>
+            </div>
+
+            <div
+              className="collapse navbar-collapse"
+              id="bs-example-navbar-collapse-1"
+            >
+              <ul className="nav navbar-nav">
+                <li className="active">
+                  <a href="#">
+                    Link <span className="sr-only">(current)</span>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li className="dropdown">
+                  <a
+                    href="#"
+                    className="dropdown-toggle"
+                    data-toggle="dropdown"
+                    role="button"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Dropdown <span className="caret" />
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a href="#">Action</a>
+                    </li>
+                    <li>
+                      <a href="#">Another action</a>
+                    </li>
+                    <li>
+                      <a href="#">Something else here</a>
+                    </li>
+                    <li role="separator" className="divider" />
+                    <li>
+                      <a href="#">Separated link</a>
+                    </li>
+                    <li role="separator" className="divider" />
+                    <li>
+                      <a href="#">One more separated link</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+              <ul className="nav navbar-nav navbar-right">
+                <li>
+                  <a href="#">Link</a>
+                </li>
+                <li className="dropdown">
+                  <a
+                    href="#"
+                    className="dropdown-toggle"
+                    data-toggle="dropdown"
+                    role="button"
+                    aria-haspopup="true"
+                    aria-expanded="false"
+                  >
+                    Dropdown <span className="caret" />
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li>
+                      <a href="#">Action</a>
+                    </li>
+                    <li>
+                      <a href="#">Another action</a>
+                    </li>
+                    <li>
+                      <a href="#">Something else here</a>
+                    </li>
+                    <li role="separator" className="divider" />
+                    <li>
+                      <a href="#">Separated link</a>
+                    </li>
+                  </ul>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
         <div className={classes.root}>
           <div className={classNames(classes.appFrame, "sticky-top")}>
-            <AppBar style={{ backgroundColor: "#00babe" }}>
-              <Toolbar
-                disableGutters={!sideopen}
-                style={{
-                  minHeight: "50px",
-                  padding: "0px",
-                  backgroundColor: "#00babe"
-                }}
-              >
-                <div className="screenDisplay">
-                  <div className="appLogoCntr">
-                    <p className="appLogoOnly" />
-                  </div>
-                  <i
-                    className="fas fa-bars"
-                    onClick={this.handleDrawerOpen}
-                    className={classNames(
-                      classes.menuButton,
-                      sideopen && classes.hide + "float - left"
-                    )}
-                  >
-                    <MenuIcon />
-                  </i>
-
-                  <h5>{this.state.title}</h5>
-                </div>
-                {/* <span className="selectedLang">{this.state.languageName}</span> */}
-                <div className="loginProfileInfo">
-                  <span>{getCookie("userName")},</span>
-                  <span>Apollo Hospitals - Bannerghatta Road</span>
-                </div>
-                <button
-                  className="btn btn-userProfile"
-                  onClick={this.handleOpenClick}
-                >
-                  {/* <i className="fas fa-user-circle" /> */}
-                  <i className="fas fa-angle-down" />
-                </button>
-
-                <Menu
-                  id="simple-menu"
-                  anchorEl={this.state.anchorEl}
-                  open={Boolean(this.state.anchorEl)}
-                  onClose={this.handleClose}
-                >
-                  <MenuItem>
-                    <i className="fas fa-user" /> User Profile
-                  </MenuItem>
-                  <MenuItem>
-                    <i className="fas fa-cog" /> Preference
-                  </MenuItem>
-                  <hr />
-                  <MenuItem onClick={this.handleClose.bind(this, "en")}>
-                    <i className="fas fa-globe-asia" />
-                    {this.state.languageName === "English"
-                      ? this.renderCheck()
-                      : null}
-                    &nbsp; English
-                  </MenuItem>
-                  <MenuItem onClick={this.handleClose.bind(this, "ar")}>
-                    <i className="fas fa-globe-asia" />
-                    {this.state.languageName === "عربي"
-                      ? this.renderCheck()
-                      : null}
-                    &nbsp; عربي
-                  </MenuItem>
-                  <hr />
-
-                  <MenuItem onClick={this.logoutLink.bind(this)}>
-                    <i className="fas fa-sign-out-alt" /> Logout
-                  </MenuItem>
-                </Menu>
-              </Toolbar>
-            </AppBar>
             {/* Side Bar Functionality */}
             {this.state.sideopen === true ? (
               <div
