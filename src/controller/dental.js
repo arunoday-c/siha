2f9f9a0e172a3d2b8078a828346e81310e355c2c
irfan72 <irfan.algaeh@gmail.com>
2018-12-06 12:58:07 +0530

    added api for dental plan update

--- src/controller/dental.js
+++ src/controller/dental.js
@@ -6,7 +6,9 @@ import {
   addDentalTreatment,
   getTreatmentPlan,
   getDentalTreatment,
-  approveTreatmentPlan
+  approveTreatmentPlan,
+  deleteDentalPlan,
+  updateDentalPlanStatus
 } from "../model/dental";
 
 export default ({ config, db }) => {
@@ -76,6 +78,44 @@ export default ({ config, db }) => {
   api.put(
     "/approveTreatmentPlan",
     approveTreatmentPlan,
+    (req, res, next) => {
+      let result = req.records;
+      if (result.invalid_input == true) {
+        res.status(httpStatus.ok).json({
+          success: false,
+          records: "please provide valid input"
+        });
+      } else {
+        res.status(httpStatus.ok).json({
+          success: true,
+          records: result
+        });
+      }
+      next();
+    },
+    releaseConnection
+  );
+
+  // created by irfan :to
+  api.delete(
+    "/deleteDentalPlan",
+    deleteDentalPlan,
+    (req, res, next) => {
+      let result = req.records;
+      res.status(httpStatus.ok).json({
+        success: true,
+        records: result
+      });
+
+      next();
+    },
+    releaseConnection
+  );
+
+  // created by irfan :to
+  api.put(
+    "/updateDentalPlanStatus",
+    updateDentalPlanStatus,
     (req, res, next) => {
       let result = req.records;
       if (result.invalid_input == true) {
