00d14f86081c6929980ceb35acf8d51dab35e188
nowshad <nowshad.algaeh@gmail.com>
2018-12-10 15:23:19 +0530

    front desk clean up done

--- client/src/Components/RegistrationPatient/Billing/AddBillingDetails.js
+++ client/src/Components/RegistrationPatient/Billing/AddBillingDetails.js
@@ -320,6 +320,7 @@ const ProcessInsurance = ($this, context, ctrl, e) => {
       type: "error"
     });
   } else {
+    debugger;
     let serviceInput = [
       {
         insured: $this.state.insured,
@@ -343,18 +344,24 @@ const ProcessInsurance = ($this, context, ctrl, e) => {
       data: serviceInput,
       onSuccess: response => {
         if (response.data.success) {
+          debugger;
+          // response.data.records.billdetails[0].insured =
+          //   response.data.records.billdetails[0].insurance_yesno;
+          $this.setState({ ...response.data.records });
           if (context !== null) {
             context.updateState({ ...response.data.records });
           }
-
+          debugger;
           algaehApiCall({
             uri: "/billing/billingCalculations",
             method: "POST",
             data: response.data.records,
             onSuccess: response => {
               if (response.data.success) {
+                debugger;
                 response.data.records.saveEnable = false;
                 response.data.records.ProcessInsure = true;
+                $this.setState({ ...response.data.records });
                 if (context !== null) {
                   context.updateState({ ...response.data.records });
                 }
