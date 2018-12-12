ebbfe4022998e4066f921d6724bf911d1ffa72fb
nowshad <nowshad.algaeh@gmail.com>
2018-12-12 17:10:17 +0530

    Item setup issue fixed

--- client/src/Components/ItemSetup/ItemMaster/ItemDetails/ItemDetailsEvents.js
+++ client/src/Components/ItemSetup/ItemMaster/ItemDetails/ItemDetailsEvents.js
@@ -1,5 +1,6 @@
 import AlgaehSearch from "../../../Wrapper/globalSearch";
 import spotlightSearch from "../../../../Search/spotlightSearch.json";
+import { SetBulkState } from "../../../../utils/GlobalFunctions";
 
 const radioChange = ($this, context, e) => {
   let radioActive = true;
@@ -30,14 +31,27 @@ const radioChange = ($this, context, e) => {
   }
 };
 
-const BatchExpRequired = ($this, e) => {
-  let required_batchno_expiry = "N";
-  if (!$this.state.batchexpreq === true) {
-    required_batchno_expiry = "Y";
-  }
-  $this.setState({
-    required_batchno_expiry: required_batchno_expiry,
-    batchexpreq: !$this.state.batchexpreq
+const BatchExpRequired = ($this, context) => {
+  SetBulkState({
+    state: $this,
+    callback: () => {
+      let required_batchno_expiry = "N";
+      if (!$this.state.batchexpreq === true) {
+        required_batchno_expiry = "Y";
+      }
+      $this.setState({
+        required_batchno_expiry: required_batchno_expiry,
+        batchexpreq: !$this.state.batchexpreq,
+        ...$this.state
+      });
+
+      if (context !== undefined) {
+        context.updateState({
+          required_batchno_expiry: required_batchno_expiry,
+          ...$this.state
+        });
+      }
+    }
   });
 };
 
@@ -67,29 +81,39 @@ const CptCodesSearch = ($this, context) => {
 };
 
 const VatAppilicable = ($this, context, e) => {
-  let Applicable = false;
-  let Value = "N";
+  let name = e.target.name;
+  SetBulkState({
+    state: $this,
+    callback: () => {
+      debugger;
+      let Applicable = false;
+      let Value = "N";
 
-  if ($this.state.Applicable === true) {
-    Applicable = false;
-    Value = "N";
-  } else if ($this.state.Applicable === false) {
-    Applicable = true;
-    Value = "Y";
-  }
-  $this.setState({
-    [e.target.name]: Value,
-    Applicable: Applicable,
-    vat_percent: 0
+      if ($this.state.Applicable === true) {
+        Applicable = false;
+        Value = "N";
+      } else if ($this.state.Applicable === false) {
+        Applicable = true;
+        Value = "Y";
+      }
+      $this.setState({
+        [name]: Value,
+        // Applicable: Applicable,
+        vat_percent: 0,
+        ...$this.state
+      });
+
+      // $this.state.
+      if (context !== undefined) {
+        context.updateState({
+          [name]: Value,
+          // Applicable: Applicable,
+          vat_percent: 0,
+          ...$this.state
+        });
+      }
+    }
   });
-
-  if (context !== undefined) {
-    context.updateState({
-      [e.target.name]: Value,
-      Applicable: Applicable,
-      vat_percent: 0
-    });
-  }
 };
 
 export { radioChange, BatchExpRequired, CptCodesSearch, VatAppilicable };
