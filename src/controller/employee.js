3a891067592359b216eb214171949bc11c212736
irfan72 <irfan.algaeh@gmail.com>
2018-12-19 16:29:02 +0530

    adde apis for EarningDeduction

--- src/controller/employee.js
+++ src/controller/employee.js
@@ -11,7 +11,11 @@ import {
   addEmployeeGroups,
   getEmployeeGroups,
   updateEmployeeGroup,
-  deleteEmployeeGroup
+  deleteEmployeeGroup,
+  addEarningDeduction,
+  getEarningDeduction,
+  updateEarningDeduction,
+  deleteEarningDeduction
 } from "../model/employee";
 import httpStatus from "../utils/httpStatus";
 export default ({ config, db }) => {
@@ -181,5 +185,78 @@ export default ({ config, db }) => {
     }
     next();
   });
+
+  // created by irfan
+  api.post("/addEarningDeduction", addEarningDeduction, (req, res, next) => {
+    let result = req.records;
+    if (result.invalid_input == true) {
+      res.status(httpStatus.ok).json({
+        success: false,
+        records: "please provide valid input"
+      });
+    } else {
+      res.status(httpStatus.ok).json({
+        success: true,
+        records: result
+      });
+    }
+    next();
+  });
+
+  api.get(
+    "/getEarningDeduction",
+    getEarningDeduction,
+    (req, res, next) => {
+      let result = req.records;
+      res.status(httpStatus.ok).json({
+        success: true,
+        records: result
+      });
+      next();
+    },
+    releaseConnection
+  );
+
+  // created by irfan
+  api.put(
+    "/updateEarningDeduction",
+    updateEarningDeduction,
+    (req, res, next) => {
+      let result = req.records;
+      if (result.invalid_input == true) {
+        res.status(httpStatus.ok).json({
+          success: false,
+          records: "please provide valid input"
+        });
+      } else {
+        res.status(httpStatus.ok).json({
+          success: true,
+          records: result
+        });
+      }
+      next();
+    }
+  );
+
+  // created by irfan
+  api.delete(
+    "/deleteEarningDeduction",
+    deleteEarningDeduction,
+    (req, res, next) => {
+      let result = req.records;
+      if (result.invalid_input == true) {
+        res.status(httpStatus.ok).json({
+          success: false,
+          records: "please provide valid input"
+        });
+      } else {
+        res.status(httpStatus.ok).json({
+          success: true,
+          records: result
+        });
+      }
+      next();
+    }
+  );
   return api;
 };
