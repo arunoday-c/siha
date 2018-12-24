acedd700f53b1a147a6edde833152f39f2b6704d
adnan-algaeh <adnan.algaeh@gmail.com>
2018-12-24 19:35:44 +0530

    Loan Master APIs and UI integration

--- client/src/Components/PayrollManagement/PayrollSettings/LoanMaster/LoanMaster.js
+++ client/src/Components/PayrollManagement/PayrollSettings/LoanMaster/LoanMaster.js
@@ -1,14 +1,180 @@
 import React, { Component } from "react";
 import "./loan_master.css";
-
 import {
-  AlgaehDateHandler,
   AlagehFormGroup,
   AlgaehLabel,
   AlagehAutoComplete,
   AlgaehDataGrid
 } from "../../../Wrapper/algaehWrapper";
+import { algaehApiCall, swalMessage } from "../../../../utils/algaehApiCall";
+import GlobalVariables from "../../../../utils/GlobalVariables.json";
+import { AlgaehValidation } from "../../../../utils/GlobalFunctions";
+import swal from "sweetalert2";
+
 class LoanMaster extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.getLoanMaster();
+  }
+
+  clearState() {
+    this.setState({
+      loan_code: null,
+      loan_description: null,
+      loan_account: null,
+      loan_limit_type: null,
+      loan_maximum_amount: null
+    });
+  }
+
+  getLoanMaster() {
+    algaehApiCall({
+      uri: "/employee/getLoanMaster",
+      method: "GET",
+      onSuccess: res => {
+        if (res.data.success) {
+          this.setState({
+            loan_master: res.data.records
+          });
+        }
+      },
+      onFailure: err => {
+        swalMessage({
+          title: err.message,
+          type: "error"
+        });
+      }
+    });
+  }
+
+  addLoanMaster() {
+    AlgaehValidation({
+      alertTypeIcon: "warning",
+      onSuccess: () => {
+        algaehApiCall({
+          uri: "/employee/addLoanMaster",
+          method: "POST",
+          data: {
+            loan_code: this.state.loan_code,
+            loan_description: this.state.loan_description,
+            loan_account: this.state.loan_account,
+            loan_limit_type: this.state.loan_limit_type,
+            loan_maximum_amount: this.state.loan_maximum_amount
+          },
+          onSuccess: res => {
+            if (res.data.success) {
+              this.clearState();
+              this.getLoanMaster();
+              swalMessage({
+                title: "Record added successfully",
+                type: "success"
+              });
+            }
+          },
+          onFailure: err => {
+            swalMessage({
+              title: err.message,
+              type: "error"
+            });
+          }
+        });
+      }
+    });
+  }
+
+  updateLoanMater(data) {
+    algaehApiCall({
+      uri: "/employee/updateLoanMaster",
+      method: "PUT",
+      data: {
+        hims_d_loan_id: data.hims_d_loan_id,
+        loan_code: data.loan_code,
+        loan_description: data.loan_description,
+        loan_account: data.loan_account,
+        loan_limit_type: data.loan_limit_type,
+        loan_maximum_amount: data.loan_maximum_amount
+      },
+      onSuccess: response => {
+        if (response.data.success) {
+          swalMessage({
+            title: "Record updated successfully",
+            type: "success"
+          });
+
+          this.getLoanMaster();
+        }
+      },
+      onFailure: error => {
+        swalMessage({
+          title: error.message,
+          type: "error"
+        });
+      }
+    });
+  }
+
+  deleteLoanMaster(data) {
+    swal({
+      title: "Are you sure you want to delete " + data.loan_description + " ?",
+      type: "warning",
+      showCancelButton: true,
+      confirmButtonText: "Yes!",
+      confirmButtonColor: "#44b8bd",
+      cancelButtonColor: "#d33",
+      cancelButtonText: "No"
+    }).then(willDelete => {
+      if (willDelete.value) {
+        algaehApiCall({
+          uri: "/employee/deleteLoanMaster",
+          data: {
+            hims_d_loan_id: data.hims_d_loan_id
+          },
+          method: "DELETE",
+          onSuccess: response => {
+            if (response.data.records.success) {
+              swalMessage({
+                title: "Record deleted successfully . .",
+                type: "success"
+              });
+              this.getLoanMaster();
+            }
+          },
+          onFailure: error => {
+            swalMessage({
+              title: error.message,
+              type: "error"
+            });
+          }
+        });
+      } else {
+        swalMessage({
+          title: "Delete request cancelled",
+          type: "error"
+        });
+      }
+    });
+  }
+
+  changeTexts(e) {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  dropDownHandle(value) {
+    this.setState({
+      [value.name]: value.value
+    });
+  }
+
+  changeGridEditors(row, e) {
+    let name = e.name || e.target.name;
+    let value = e.value || e.target.value;
+    row[name] = value;
+    row.update();
+  }
+
   render() {
     return (
       <div className="loan_master">
@@ -21,14 +187,10 @@ class LoanMaster extends Component {
             }}
             textBox={{
               className: "txt-fld",
-              //decimal: { allowNegative: false },
-              name: "limit_amount",
-              //value: this.state.limit_amount,
+              name: "loan_code",
+              value: this.state.loan_code,
               events: {
-                //  onChange: this.changeTexts.bind(this)
-              },
-              others: {
-                // type: "number"
+                onChange: this.changeTexts.bind(this)
               }
             }}
           />
@@ -41,34 +203,30 @@ class LoanMaster extends Component {
             }}
             textBox={{
               className: "txt-fld",
-              //decimal: { allowNegative: false },
-              name: "limit_amount",
-              //value: this.state.limit_amount,
+              name: "loan_description",
+              value: this.state.loan_description,
               events: {
-                //  onChange: this.changeTexts.bind(this)
-              },
-              others: {
-                // type: "number"
+                onChange: this.changeTexts.bind(this)
               }
             }}
           />
 
           <AlagehAutoComplete
-            div={{ className: "col " }}
+            div={{ className: "col" }}
             label={{
               forceLabel: "Loan Limit Type",
-              isImp: false
+              isImp: true
             }}
             selector={{
-              name: "sub_department_id",
+              name: "loan_limit_type",
               className: "select-fld",
-              //value : '',
+              value: this.state.loan_limit_type,
               dataSource: {
-                textField: "sub_department_name",
-                Field: "sub_department_id"
-                //data: this.state.departments
-              }
-              // onChange: this.dropDownHandle.bind(this)
+                textField: "name",
+                valueField: "value",
+                data: GlobalVariables.LOAN_LMT_TYP
+              },
+              onChange: this.dropDownHandle.bind(this)
             }}
           />
 
@@ -80,14 +238,13 @@ class LoanMaster extends Component {
             }}
             textBox={{
               className: "txt-fld",
-              //decimal: { allowNegative: false },
-              name: "limit_amount",
-              //value: this.state.limit_amount,
+              name: "loan_maximum_amount",
+              value: this.state.loan_maximum_amount,
               events: {
-                //  onChange: this.changeTexts.bind(this)
+                onChange: this.changeTexts.bind(this)
               },
               others: {
-                // type: "number"
+                type: "number"
               }
             }}
           />
@@ -100,45 +257,21 @@ class LoanMaster extends Component {
             }}
             textBox={{
               className: "txt-fld",
-              //decimal: { allowNegative: false },
-              name: "limit_amount",
-              //value: this.state.limit_amount,
+              name: "loan_account",
+              value: this.state.loan_account,
               events: {
-                //  onChange: this.changeTexts.bind(this)
-              },
-              others: {
-                // type: "number"
-              }
-            }}
-          />
-
-          <AlagehFormGroup
-            div={{ className: "col-10" }}
-            label={{
-              forceLabel: "Comments",
-              isImp: true
-            }}
-            textBox={{
-              className: "txt-fld",
-              //decimal: { allowNegative: false },
-              name: "limit_amount",
-              //value: this.state.limit_amount,
-              events: {
-                //  onChange: this.changeTexts.bind(this)
-              },
-              others: {
-                // type: "number"
+                onChange: this.changeTexts.bind(this)
               }
             }}
           />
 
           <div className="col-2 form-group">
             <button
-              // onClick={this.loadPatients.bind(this)}
+              onClick={this.addLoanMaster.bind(this)}
               style={{ marginTop: 21 }}
               className="btn btn-primary"
             >
-              LOAD
+              ADD TO LIST
             </button>
           </div>
         </div>
@@ -150,61 +283,155 @@ class LoanMaster extends Component {
           </div>
           <div className="portlet-body">
             <div className="row">
-              <div className="col-lg-12" id="loanMasterList_cntr">
+              <div
+                className="col-lg-12"
+                id="loanMasterList_cntr"
+                data-validate="loanGrid"
+              >
                 <AlgaehDataGrid
                   id="loanMasterList_grid"
+                  datavalidate="data-validate='loanGrid'"
                   columns={[
                     {
-                      fieldName: "",
-                      label: "Loan Code"
-                      //disabled: true
+                      fieldName: "loan_code",
+                      label: (
+                        <AlgaehLabel label={{ forceLabel: "Loan Code" }} />
+                      ),
+                      editorTemplate: row => {
+                        return (
+                          <AlagehFormGroup
+                            div={{ className: "col" }}
+                            textBox={{
+                              className: "txt-fld",
+                              name: "loan_code",
+                              value: row.loan_code,
+                              events: {
+                                onChange: this.changeGridEditors.bind(this, row)
+                              },
+                              others: {
+                                errormessage: "Code - cannot be blank",
+                                required: true
+                              }
+                            }}
+                          />
+                        );
+                      }
                     },
                     {
-                      fieldName: "",
-                      label: "Loan Description"
-                      //disabled: true
+                      fieldName: "loan_description",
+                      label: (
+                        <AlgaehLabel
+                          label={{ forceLabel: "Loan Description" }}
+                        />
+                      )
                     },
                     {
-                      fieldName: "",
-                      label: "Loan Limit Type"
-                      //disabled: true
+                      fieldName: "loan_limit_type",
+                      label: (
+                        <AlgaehLabel
+                          label={{ forceLabel: "Loan Limit Type" }}
+                        />
+                      ),
+                      displayTemplate: row => {
+                        return (
+                          <span>
+                            {row.loan_limit_type === "L"
+                              ? "Loan Limit"
+                              : row.loan_limit_type === "B"
+                              ? "Basic"
+                              : row.loan_limit_type === "G"
+                              ? "Gratuity"
+                              : "------"}
+                          </span>
+                        );
+                      },
+                      editorTemplate: row => {
+                        return (
+                          <AlagehAutoComplete
+                            div={{ className: "col" }}
+                            selector={{
+                              name: "loan_limit_type",
+                              className: "select-fld",
+                              value: row.loan_limit_type,
+                              dataSource: {
+                                textField: "name",
+                                valueField: "value",
+                                data: GlobalVariables.LOAN_LMT_TYP
+                              },
+                              others: {
+                                errormessage: "Field cannot be blank",
+                                required: true
+                              },
+                              onChange: this.changeGridEditors.bind(this, row)
+                            }}
+                          />
+                        );
+                      }
                     },
                     {
-                      fieldName: "",
-                      label: "Loan Limit Value"
-                      //disabled: true
+                      fieldName: "loan_maximum_amount",
+                      label: (
+                        <AlgaehLabel
+                          label={{ forceLabel: "Loan Limit Value" }}
+                        />
+                      ),
+                      editorTemplate: row => {
+                        return (
+                          <AlagehFormGroup
+                            div={{ className: "col" }}
+                            textBox={{
+                              className: "txt-fld",
+                              name: "loan_maximum_amount",
+                              value: row.loan_maximum_amount,
+                              events: {
+                                onChange: this.changeGridEditors.bind(this, row)
+                              },
+                              others: {
+                                type: "number",
+                                errormessage: "Value - cannot be blank",
+                                required: true
+                              }
+                            }}
+                          />
+                        );
+                      }
                     },
                     {
-                      fieldName: "",
-                      label: "G/L Account"
-                      //disabled: true
-                    },
-                    {
-                      fieldName: "",
-                      label: "Leave Reason"
-                      //disabled: true
-                    },
-                    {
-                      fieldName: "",
-                      label: "Loan Comments"
-                      //disabled: true
-                    },
-                    {
-                      fieldName: "",
-                      label: "Status"
-                      //disabled: true
+                      fieldName: "loan_account",
+                      label: (
+                        <AlgaehLabel label={{ forceLabel: "G/L Account" }} />
+                      ),
+                      editorTemplate: row => {
+                        return (
+                          <AlagehFormGroup
+                            div={{ className: "col" }}
+                            textBox={{
+                              className: "txt-fld",
+                              name: "loan_account",
+                              value: row.loan_account,
+                              events: {
+                                onChange: this.changeGridEditors.bind(this, row)
+                              },
+                              others: {
+                                errormessage: "Account - cannot be blank",
+                                required: true
+                              }
+                            }}
+                          />
+                        );
+                      }
                     }
                   ]}
-                  keyId="algaeh_d_module_id"
+                  keyId="hims_d_loan_id"
                   dataSource={{
-                    data: []
+                    data: this.state.loan_master
                   }}
-                  isEditable={false}
+                  isEditable={true}
                   paging={{ page: 0, rowsPerPage: 10 }}
                   events={{
                     onEdit: () => {},
-                    onDelete: () => {},
-                    onDone: () => {}
+                    onDelete: this.deleteLoanMaster.bind(this),
+                    onDone: this.updateLoanMater.bind(this)
                   }}
                 />
               </div>
--- src/model/employee.js
+++ src/model/employee.js
@@ -8,7 +8,6 @@ import {
   jsonArrayToObject
 } from "../utils";
 import httpStatus from "../utils/httpStatus";
-
 import { debugLog } from "../utils/logging";
 import Promise from "bluebird";
 import department from "../controller/department";
@@ -1693,6 +1692,172 @@ let deleteEmployeeIdentification = (req, res, next) => {
   }
 };
 
+//created by Adnan
+let addLoanMaster = (req, res, next) => {
+  try {
+    if (req.db == null) {
+      next(httpStatus.dataBaseNotInitilizedError());
+    }
+    let db = req.db;
+    let input = extend({}, req.body);
+    db.getConnection((error, connection) => {
+      if (error) {
+        next(error);
+      }
+
+      connection.query(
+        "INSERT  INTO hims_d_loan (loan_code,loan_description,\
+          loan_account,loan_limit_type,loan_maximum_amount,\
+          created_date,created_by,updated_date,updated_by) values(\
+            ?,?,?,?,?,?,?,?,?)",
+        [
+          input.loan_code,
+          input.loan_description,
+          input.loan_account,
+          input.loan_limit_type,
+          input.loan_maximum_amount,
+          new Date(),
+          input.created_by,
+          new Date(),
+          input.updated_by
+        ],
+        (error, result) => {
+          releaseDBConnection(db, connection);
+          if (error) {
+            next(error);
+          }
+          req.records = result;
+          next();
+        }
+      );
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+//created by Adnan
+let getLoanMaster = (req, res, next) => {
+  try {
+    if (req.db == null) {
+      next(httpStatus.dataBaseNotInitilizedError());
+    }
+    let db = req.db;
+
+    db.getConnection((error, connection) => {
+      connection.query(
+        "select hims_d_loan_id,loan_code,\
+          loan_description,loan_account,loan_limit_type,loan_maximum_amount,loan_status from hims_d_loan\
+        where record_status='A' order by hims_d_loan_id desc",
+
+        (error, result) => {
+          releaseDBConnection(db, connection);
+          if (error) {
+            next(error);
+          }
+          req.records = result;
+          next();
+        }
+      );
+    });
+  } catch (e) {
+    next(e);
+  }
+};
+
+//created by Adnan
+let updateLoanMaster = (req, res, next) => {
+  try {
+    if (req.db == null) {
+      next(httpStatus.dataBaseNotInitilizedError());
+    }
+    let db = req.db;
+
+    let input = extend({}, req.body);
+
+    if (input.hims_d_loan_id != "null" && input.hims_d_loan_id != undefined) {
+      db.getConnection((error, connection) => {
+        connection.query(
+          "update hims_d_loan set  hims_d_loan_id=?,\
+          loan_code=?,loan_description=?,loan_account=?,loan_limit_type=?,loan_maximum_amount=?,\
+            updated_date=?, updated_by=?  WHERE record_status='A' and  hims_d_loan_id = ?",
+
+          [
+            input.hims_d_loan_id,
+            input.loan_code,
+            input.loan_description,
+            input.loan_account,
+            input.loan_limit_type,
+            input.loan_maximum_amount,
+            new Date(),
+            input.updated_by,
+            input.hims_d_loan_id
+          ],
+          (error, result) => {
+            releaseDBConnection(db, connection);
+            if (error) {
+              next(error);
+            }
+
+            if (result.affectedRows > 0) {
+              req.records = result;
+              next();
+            } else {
+              req.records = { invalid_input: true };
+              next();
+            }
+          }
+        );
+      });
+    } else {
+      req.records = { invalid_input: true };
+      next();
+    }
+  } catch (e) {
+    next(e);
+  }
+};
+
+//created by Adnan
+let deleteLoanMaster = (req, res, next) => {
+  try {
+    if (req.db == null) {
+      next(httpStatus.dataBaseNotInitilizedError());
+    }
+    let input = extend({}, req.body);
+    if (input.hims_d_loan_id != "null" && input.hims_d_loan_id != undefined) {
+      deleteRecord(
+        {
+          db: req.db,
+          tableName: "hims_d_loan",
+          id: req.body.hims_d_loan_id,
+          query:
+            "UPDATE hims_d_loan SET  record_status='I' WHERE hims_d_loan_id=?",
+          values: [req.body.hims_d_loan_id]
+        },
+        result => {
+          if (result.records.affectedRows > 0) {
+            req.records = result;
+            next();
+          } else {
+            req.records = { invalid_input: true };
+            next();
+          }
+        },
+        error => {
+          next(error);
+        },
+        true
+      );
+    } else {
+      req.records = { invalid_input: true };
+      next();
+    }
+  } catch (e) {
+    next(e);
+  }
+};
+
 module.exports = {
   addEmployee,
   addEmployeeMaster,
@@ -1714,5 +1879,9 @@ module.exports = {
   getEmployeeIdentification,
   updateEmployeeIdentification,
   deleteEmployeeIdentification,
-  addEmployeeInfo
+  addEmployeeInfo,
+  addLoanMaster,
+  getLoanMaster,
+  updateLoanMaster,
+  deleteLoanMaster
 };
