83488b3ddf7f3ac893b2b0807a1b85b7eeae30ae
adnan-algaeh <adnan.algaeh@gmail.com>
2018-12-24 10:30:42 +0530

    Overtime group master APIs for add get and delete, Employee Master Index Changes

--- src/controller/employeesetups.js
+++ src/controller/employeesetups.js
@@ -4,7 +4,10 @@ import {
   getEmpSpeciality,
   getEmpCategory,
   addDesignation,
-  deleteDesignation
+  deleteDesignation,
+  getOvertimeGroups,
+  addOvertimeGroups,
+  deleteOvertimeGroups
 } from "../model/employeesetups";
 import { releaseConnection } from "../utils";
 import httpStatus from "../utils/httpStatus";
@@ -96,6 +99,60 @@ export default ({ config, db }) => {
     }
     next();
   });
+
+  //created by Adnan
+  api.get(
+    "/getOvertimeGroups",
+    getOvertimeGroups,
+    (req, res, next) => {
+      let result = req.records;
+      if (result.length == 0) {
+        next(httpStatus.generateError(httpStatus.notFound, "No records found"));
+      } else {
+        res.status(httpStatus.ok).json({
+          success: true,
+          records: result
+        });
+      }
+      next();
+    },
+    releaseConnection
+  );
+
+  //created by Adnan
+  api.post(
+    "/addOvertimeGroups",
+    addOvertimeGroups,
+    (req, res, next) => {
+      let result = req.records;
+      res.status(httpStatus.ok).json({
+        success: true,
+        records: result
+      });
+      next();
+    },
+    releaseConnection
+  );
+
+  api.delete(
+    "/deleteOvertimeGroups",
+    deleteOvertimeGroups,
+    (req, res, next) => {
+      let result = req.records;
+      if (result.invalid_input == true) {
+        res.status(httpStatus.ok).json({
+          success: false,
+          records: "please provide valid input"
+        });
+      } else {
+        res.status(httpStatus.ok).json({
+          success: true,
+          records: result
+        });
+      }
+      next();
+    }
+  );
 
   return api;
 };
