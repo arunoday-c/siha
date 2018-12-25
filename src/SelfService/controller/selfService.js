14fbe32c301920c9b72c1602ff9797273ca7d720
irfan72 <irfan.algaeh@gmail.com>
2018-12-25 12:31:33 +0530

    adde getEmployeeBasicDetails

--- src/SelfService/controller/selfService.js
+++ src/SelfService/controller/selfService.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { releaseConnection } from "../../utils";
 import httpStatus from "../../utils/httpStatus";
-import {} from "../model/selfService";
+import { getEmployeeBasicDetails } from "../model/selfService";
 
 export default ({ config, db }) => {
   let api = Router();
@@ -9,8 +9,8 @@ export default ({ config, db }) => {
 
   // created by irfan :
   api.get(
-    "/aaaaaaaaa",
-
+    "/getEmployeeBasicDetails",
+    getEmployeeBasicDetails,
     (req, res, next) => {
       let result = req.records;
       res.status(httpStatus.ok).json({
--- src/SelfService/model/selfService.js
+++ src/SelfService/model/selfService.js
@@ -8,52 +8,39 @@ import {
   jsonArrayToObject
 } from "../../utils";
 import httpStatus from "../../utils/httpStatus";
-import { LINQ } from "node-linq";
 
 import { debugLog } from "../../utils/logging";
-import moment from "moment";
 
 //created by irfan: to get eployee basic details
 let getEmployeeBasicDetails = (req, res, next) => {
-  let employeeWhereCondition = {
-    employee_code: "ALL",
-    sex: "ALL",
-    blood_group: "ALL",
-    employee_status: "ALL",
-    date_of_joining: "ALL",
-    date_of_resignation: "ALL",
-    primary_contact_no: "ALL",
-    email: "ALL"
-  };
   try {
     if (req.db == null) {
       next(httpStatus.dataBaseNotInitilizedError());
     }
     let db = req.db;
 
-    let where = whereCondition(extend(employeeWhereCondition, req.query));
-
     db.getConnection((error, connection) => {
       connection.query(
         "SELECT E.hims_d_employee_id,E.employee_code,E.title_id,E.full_name,E.arabic_name,E.employee_designation_id,\
-        E.license_number,E.sex,E.date_of_birth,E.date_of_joining,E.date_of_resignation,E.present_address,E.present_address2,\
-        E.present_pincode,E.present_pincode,E.present_state_id,E.present_country_id,\
-        E.permanent_address , E.permanent_address2, E.permanent_pincode, E.permanent_city_id, E.permanent_state_id,\
-        E.permanent_country_id, E.primary_contact_no, E.secondary_contact_no,E.email,\
-        E.emergency_contact_person,E.emergency_contact_no,E.blood_group,\
-        E.isdoctor,E.employee_status,E.exclude_machine_data ,E.company_bank_id ,E.employee_bank_name , E.effective_start_date,E.effective_end_date,\
-        E.employee_bank_ifsc_code , E.employee_account_number, E.mode_of_payment, E.accomodation_provided,\
-         E.late_coming_rule, E.leave_salary_process, E.entitled_daily_ot, E.suspend_salary, E.gratuity_applicable, E.contract_type, E.employee_group_id,\
-         E.weekoff_from,E.overtime_group_id, E.reporting_to_id, E.hospital_id,\
-        ED.hims_d_employee_department_id,ED.employee_id,ED.sub_department_id,ED.category_speciality_id,ED.user_id,\
-         ED.services_id,CS.hims_m_category_speciality_mappings_id,CS.category_id,CS.speciality_id,\
-        CS.category_speciality_status,CS.effective_start_date,CS.effective_end_date\
-        from hims_d_employee E,hims_m_employee_department_mappings ED,hims_m_category_speciality_mappings CS\
-         Where E.record_status='A' and ED.record_status='A' and CS.record_status='A' and E.hims_d_employee_id=ED.employee_id\
-          and ED.category_speciality_id=CS.hims_m_category_speciality_mappings_id AND " +
-          where.condition +
-          "order by E.hims_d_employee_id desc ",
-        where.values,
+         D.designation_code,D.designation,\
+            E.license_number,E.sex,E.date_of_birth,E.date_of_joining,E.date_of_resignation,E.present_address,E.present_address2,\
+            E.present_pincode,E.present_pincode,E.present_state_id,S.state_name as present_state_name ,E.present_country_id,C.country_name present_country_name,\
+            E.permanent_address , E.permanent_address2, E.permanent_pincode, E.permanent_city_id, E.permanent_state_id,\
+            E.permanent_country_id, E.primary_contact_no, E.secondary_contact_no,E.email,\
+            E.emergency_contact_person,E.emergency_contact_no,E.blood_group,\
+            E.isdoctor,E.employee_status,E.exclude_machine_data ,E.company_bank_id ,E.employee_bank_name , E.effective_start_date,E.effective_end_date,\
+            E.employee_bank_ifsc_code , E.employee_account_number, E.mode_of_payment, E.accomodation_provided,\
+            E.late_coming_rule, E.leave_salary_process, E.entitled_daily_ot, E.suspend_salary, E.gratuity_applicable, E.contract_type, E.employee_group_id,\
+            E.weekoff_from,E.overtime_group_id, E.reporting_to_id,REP.full_name as reporting_to_name, E.hospital_id ,\
+            H.hospital_code,H.hospital_name ,E.sub_department_id  ,DEP.sub_department_name \
+            from hims_d_employee E left join hims_d_designation D on E.employee_designation_id=D.hims_d_designation_id\
+            left join hims_d_country C on E.present_country_id=C.hims_d_country_id  \
+            left join hims_d_state S on  E.present_state_id=S.hims_d_state_id\
+            left join hims_d_employee REP on E.reporting_to_id=REP.hims_d_employee_id and REP.record_status='A'\
+            left join hims_d_hospital H on  E.hospital_id  =H.hims_d_hospital_id and H.record_status='A'\
+            left join hims_d_sub_department DEP on E.sub_department_id=DEP.hims_d_sub_department_id and DEP.record_status='A'\
+        where E.record_status='A' and E.hims_d_employee_id=?",
+        req.userIdentity.employee_id,
         (error, result) => {
           releaseDBConnection(db, connection);
           if (error) {
@@ -70,4 +57,4 @@ let getEmployeeBasicDetails = (req, res, next) => {
   }
 };
 
-module.exports = {};
+module.exports = { getEmployeeBasicDetails };
