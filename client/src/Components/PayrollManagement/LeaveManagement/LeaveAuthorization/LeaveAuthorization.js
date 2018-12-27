7a4f8672c819877aaefb4e2d2f64345e86be25c3
irfan72 <irfan.algaeh@gmail.com>
2018-12-27 11:15:41 +0530

    Leave Management Structure

---
+++ client/src/Components/PayrollManagement/LeaveManagement/LeaveAuthorization/LeaveAuthorization.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+
+export default class LeaveAuthorization extends Component {
+  render() {
+    return <div className="leave_auth">LEAVE AUTH</div>;
+  }
+}
