d1b42027e9270e9163b1862252bb8fbf25e87c7a
irfan72 <irfan.algaeh@gmail.com>
2019-01-02 16:06:51 +0530

    added api for loan authorization

--- src/Payroll/controller/loan.js
+++ src/Payroll/controller/loan.js
@@ -4,7 +4,8 @@ import httpStatus from "../../utils/httpStatus";
 import {
   addLoanApplication,
   getLoanApplication,
-  getLoanLevels
+  getLoanLevels,
+  authorizeLoan
 } from "../model/loan";
 
 export default ({ config, db }) => {
@@ -50,5 +51,21 @@ export default ({ config, db }) => {
     next();
   });
 
+  // created by irfan
+  api.put("/authorizeLoan", authorizeLoan, (req, res, next) => {
+    let result = req.records;
+    if (result.invalid_input == true) {
+      res.status(httpStatus.ok).json({
+        success: false,
+        records: "please provide valid input"
+      });
+    } else {
+      res.status(httpStatus.ok).json({
+        success: true,
+        records: result
+      });
+    }
+    next();
+  });
   return api;
 };
--- src/Payroll/model/loan.js
+++ src/Payroll/model/loan.js
@@ -216,4 +216,213 @@ let getLoanLevels = (req, res, next) => {
   }
 };
 
-module.exports = { addLoanApplication, getLoanApplication, getLoanLevels };
+//created by irfan:
+let authorizeLoan = (req, res, next) => {
+  try {
+    if (req.db == null) {
+      next(httpStatus.dataBaseNotInitilizedError());
+    }
+    let db = req.db;
+
+    let input = extend({}, req.body);
+
+    if (input.auth_level != "L1" && input.auth_level != "L2") {
+      debugLog("L1 and L2 not defind");
+      req.records = { invalid_input: true };
+      next();
+    } else if (input.auth_level == "L1") {
+      db.getConnection((error, connection) => {
+        connection.beginTransaction(error => {
+          if (error) {
+            connection.rollback(() => {
+              releaseDBConnection(db, connection);
+              next(error);
+            });
+          }
+          connection.query(
+            "UPDATE hims_f_loan_application SET authorized1_by=?,authorized1_date=?,\
+          authorized1=?,approved_amount=?,start_year=?,start_month=?,installment_amount=?,\
+          loan_tenure=?, updated_date=?, updated_by=?  WHERE hims_f_loan_application_id=?",
+
+            [
+              input.updated_by,
+              new Date(),
+              input.authorized1,
+              input.approved_amount,
+              input.start_year,
+              input.start_month,
+              input.installment_amount,
+              input.loan_tenure,
+              new Date(),
+              input.updated_by,
+              input.hims_f_loan_application_id
+            ],
+            (error, result) => {
+              if (error) {
+                connection.rollback(() => {
+                  releaseDBConnection(db, connection);
+                  next(error);
+                });
+              }
+              debugLog("L1 result:", result);
+              if (result.affectedRows > 0 && input.authorized1 == "R") {
+                connection.query(
+                  "update hims_f_loan_application set loan_authorized='REJ'\
+                  where record_status='A' and hims_f_loan_application_id=?",
+                  [input.hims_f_loan_application_id],
+                  (error, rejResult) => {
+                    if (error) {
+                      connection.rollback(() => {
+                        releaseDBConnection(db, connection);
+                        next(error);
+                      });
+                    }
+
+                    connection.commit(error => {
+                      if (error) {
+                        connection.rollback(() => {
+                          releaseDBConnection(db, connection);
+                          next(error);
+                        });
+                      }
+                      releaseDBConnection(db, connection);
+                      req.records = rejResult;
+                      debugLog("L1 rejResult:", rejResult);
+                      next();
+                    });
+                  }
+                );
+              } else if (result.affectedRows > 0) {
+                connection.commit(error => {
+                  if (error) {
+                    connection.rollback(() => {
+                      releaseDBConnection(db, connection);
+                      next(error);
+                    });
+                  }
+                  releaseDBConnection(db, connection);
+                  req.records = result;
+                  next();
+                });
+              } else {
+                req.records = { invalid_input: true };
+                connection.rollback(() => {
+                  releaseDBConnection(db, connection);
+                  next(error);
+                });
+              }
+            }
+          );
+        });
+      });
+    } else if (input.auth_level == "L2") {
+      db.getConnection((error, connection) => {
+        connection.beginTransaction(error => {
+          if (error) {
+            connection.rollback(() => {
+              releaseDBConnection(db, connection);
+              next(error);
+            });
+          }
+          connection.query(
+            "UPDATE hims_f_loan_application SET authorized2_by=?,authorized2_date=?,\
+            authorized2=?,approved_amount=?,start_year=?,start_month=?,installment_amount=?,\
+          loan_tenure=?, updated_date=?, updated_by=?  WHERE hims_f_loan_application_id=?",
+
+            [
+              input.updated_by,
+              new Date(),
+              input.authorized2,
+              input.approved_amount,
+              input.start_year,
+              input.start_month,
+              input.installment_amount,
+              input.loan_tenure,
+              new Date(),
+              input.updated_by,
+              input.hims_f_loan_application_id
+            ],
+            (error, result) => {
+              if (error) {
+                connection.rollback(() => {
+                  releaseDBConnection(db, connection);
+                  next(error);
+                });
+              }
+              debugLog("L2 result:", result);
+              if (
+                result.affectedRows > 0 &&
+                (input.authorized2 == "R" || input.authorized2 == "A")
+              ) {
+                let qry = "";
+
+                if (input.authorized2 == "R") {
+                  qry = `update hims_f_loan_application set loan_authorized='REJ'\
+                  where record_status='A' and hims_f_loan_application_id=${
+                    input.hims_f_loan_application_id
+                  }`;
+                } else if (input.authorized2 == "A") {
+                  qry = `update hims_f_loan_application set loan_authorized='APR'\
+                  where record_status='A' and hims_f_loan_application_id=${
+                    input.hims_f_loan_application_id
+                  }`;
+                }
+                connection.query(qry, (error, rejResult) => {
+                  if (error) {
+                    connection.rollback(() => {
+                      releaseDBConnection(db, connection);
+                      next(error);
+                    });
+                  }
+
+                  connection.commit(error => {
+                    if (error) {
+                      connection.rollback(() => {
+                        releaseDBConnection(db, connection);
+                        next(error);
+                      });
+                    }
+                    releaseDBConnection(db, connection);
+                    req.records = rejResult;
+                    next();
+                  });
+                });
+              } else if (result.affectedRows > 0) {
+                connection.commit(error => {
+                  if (error) {
+                    connection.rollback(() => {
+                      releaseDBConnection(db, connection);
+                      next(error);
+                    });
+                  }
+                  releaseDBConnection(db, connection);
+                  req.records = result;
+                  next();
+                });
+              } else {
+                req.records = { invalid_input: true };
+                connection.rollback(() => {
+                  releaseDBConnection(db, connection);
+                  next(error);
+                });
+              }
+            }
+          );
+        });
+      });
+    } else {
+      debugLog("top else");
+      req.records = { invalid_input: true };
+      next();
+    }
+  } catch (e) {
+    next(e);
+  }
+};
+
+module.exports = {
+  addLoanApplication,
+  getLoanApplication,
+  getLoanLevels,
+  authorizeLoan
+};
