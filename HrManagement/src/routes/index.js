a520bbb58e9e98b25b8246cb33c8c6cfc0912510
syed noor <syednoor.algaeh@gmail.com>
2019-01-04 17:14:55 +0530

    leave approval

--- HrManagement/src/routes/index.js
+++ HrManagement/src/routes/index.js
@@ -1,7 +1,9 @@
 import express from "express";
 import generalLedger from "../controllers/general_ledger";
+import leave_approval from "../controllers/leave_approval";
 import attendance from "../controllers/attandance";
 const router = express();
 router.use("/attendance", attendance());
+router.use("/leave", leave_approval());
 router.use("/testapi", generalLedger());
 export default router;
