b610fee4cb877794fc4e1e014dcc691059be5303
irfan72 <irfan.algaeh@gmail.com>
2019-01-09 14:50:50 +0530

    added api for

--- HrManagement/src/controllers/attandance.js
+++ HrManagement/src/controllers/attandance.js
@@ -1,6 +1,9 @@
 import { Router } from "express";
 import utlities from "algaeh-utilities";
-import { processAttendance } from "../models/attendance";
+import {
+  processAttendance
+  //addAttendanceRegularization
+} from "../models/attendance";
 export default () => {
   const api = Router();
   api.get("/processAttendance", processAttendance, (req, res, next) => {
@@ -9,5 +12,22 @@ export default () => {
       result: req.records
     });
   });
+
+  api.get(
+    "/addAttendanceRegularization",
+    (req, res, next) => {
+      console.log("Here in function");
+      res.status(200).json({
+        success: true
+      });
+    },
+    //  addAttendanceRegularization,
+    (req, res, next) => {
+      res.status(utlities.AlgaehUtilities().httpStatus().ok).json({
+        success: true,
+        result: req.records
+      });
+    }
+  );
   return api;
 };
