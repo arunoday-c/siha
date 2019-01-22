3a7d41d58b8f99fad0893d68e258f6d65a5c8b98
Aboobacker Sidhiqe <>
2019-01-22 10:46:13 +0530

    Added Payroll Options Transaction and Attendence

--- client/src/Components/PayrollManagement/PayrollOptions/AttendanceSettings/AttendanceSettings.js
+++ client/src/Components/PayrollManagement/PayrollOptions/AttendanceSettings/AttendanceSettings.js
@@ -12,16 +12,236 @@ import {
 export default class AttendanceSettings extends Component {
   render() {
     return (
-      <div className="row AttendanceSettings">
-        <div className="col-8">
-          <div className="row">
-            <div className="col-12">
-              <div className="portlet portlet-bordered margin-bottom-15 margin-top-15">
-                <div className="portlet-body">
+      <div className="row TransactionAttendanceScreen">
+        <div className="col-12">
+          <div className="portlet portlet-bordered  transactionSettings">
+            <div className="portlet-title">
+              <div className="caption">
+                <h3 className="caption-subject">Transaction Settings</h3>
+              </div>
+            </div>
+            <div className="portlet-body">
+              <div className="row">
+                <AlagehAutoComplete
+                  div={{ className: "col-2 form-group" }}
+                  label={{ forceLabel: "Salary Starts from", isImp: false }}
+                  selector={{
+                    name: "",
+                    className: "select-fld",
+                    dataSource: {},
+                    others: {}
+                  }}
+                />
+                <AlagehAutoComplete
+                  div={{ className: "col-2 form-group" }}
+                  label={{ forceLabel: "Process Leaves", isImp: false }}
+                  selector={{
+                    name: "",
+                    className: "select-fld",
+                    dataSource: {},
+                    others: {}
+                  }}
+                />
+                <AlagehAutoComplete
+                  div={{ className: "col-2 form-group" }}
+                  label={{ forceLabel: "Salary Process Date", isImp: false }}
+                  selector={{
+                    name: "",
+                    className: "select-fld",
+                    dataSource: {},
+                    others: {}
+                  }}
+                />
+                <AlagehAutoComplete
+                  div={{ className: "col-2 form-group" }}
+                  label={{ forceLabel: "Payroll Payment Date", isImp: false }}
+                  selector={{
+                    name: "",
+                    className: "select-fld",
+                    dataSource: {},
+                    others: {}
+                  }}
+                />
+
+                <AlagehAutoComplete
+                  div={{ className: "col-2 form-group" }}
+                  label={{ forceLabel: "Salary Calc. based on", isImp: false }}
+                  selector={{
+                    name: "",
+                    className: "select-fld",
+                    dataSource: {},
+                    others: {}
+                  }}
+                />
+
+                <AlagehAutoComplete
+                  div={{ className: "col-2 form-group" }}
+                  label={{
+                    forceLabel: "Salary hourly calc. factor",
+                    isImp: false
+                  }}
+                  selector={{
+                    name: "",
+                    className: "select-fld",
+                    dataSource: {},
+                    others: {}
+                  }}
+                />
+
+                <div className="col-4">
+                  <label>Salary Calculation with</label>
+                  <div className="customRadio">
+                    <label className="radio inline">
+                      <input type="radio" />
+                      <span>Periodical</span>
+                    </label>
+
+                    <label className="radio inline">
+                      <input type="radio" />
+                      <span>Fixed</span>
+                    </label>
+                  </div>
+                  <div className="row">
+                    <AlagehAutoComplete
+                      div={{ className: "col form-group" }}
+                      label={{ forceLabel: "Select Date", isImp: false }}
+                      selector={{
+                        name: "",
+                        className: "select-fld",
+                        dataSource: {},
+                        others: {}
+                      }}
+                    />
+                  </div>
+                </div>
+
+                <div className="col-4">
+                  <label>Salary Calculation with</label>
+                  <div className="customCheckbox">
+                    <label className="checkbox inline">
+                      <input type="checkbox" />
+                      <span>Pay salary before processing </span>
+                    </label>
+                  </div>
+                </div>
+
+                <div className="col-4">
+                  <label>Salary Calculation with</label>
+                  <div className="customCheckbox">
+                    <label className="checkbox inline">
+                      <input type="checkbox" />
+                      <span>Validate Dept. wise </span>
+                    </label>
+                  </div>
+                </div>
+              </div>
+              <div className="row">
+                <AlagehAutoComplete
+                  div={{ className: "col-3 form-group" }}
+                  label={{
+                    forceLabel: "Leave Authorization level",
+                    isImp: false
+                  }}
+                  selector={{
+                    name: "",
+                    className: "select-fld",
+                    dataSource: {},
+                    others: {}
+                  }}
+                />
+                <AlagehAutoComplete
+                  div={{ className: "col-3 form-group" }}
+                  label={{
+                    forceLabel: "Loan Authorization level",
+                    isImp: false
+                  }}
+                  selector={{
+                    name: "",
+                    className: "select-fld",
+                    dataSource: {},
+                    others: {}
+                  }}
+                />
+                <AlagehAutoComplete
+                  div={{ className: "col-3 form-group" }}
+                  label={{
+                    forceLabel: "Review Authorization level",
+                    isImp: false
+                  }}
+                  selector={{
+                    name: "",
+                    className: "select-fld",
+                    dataSource: {},
+                    others: {}
+                  }}
+                />
+                <AlagehAutoComplete
+                  div={{ className: "col-3 form-group" }}
+                  label={{ forceLabel: "Leave encashment level", isImp: false }}
+                  selector={{
+                    name: "",
+                    className: "select-fld",
+                    dataSource: {},
+                    others: {}
+                  }}
+                />
+                <AlagehAutoComplete
+                  div={{ className: "col-3 form-group" }}
+                  label={{ forceLabel: "Advance deduction", isImp: false }}
+                  selector={{
+                    name: "",
+                    className: "select-fld",
+                    dataSource: {},
+                    others: {}
+                  }}
+                />
+                <AlagehAutoComplete
+                  div={{ className: "col-3 form-group" }}
+                  label={{
+                    forceLabel: "Appraisal Process through",
+                    isImp: false
+                  }}
+                  selector={{
+                    name: "",
+                    className: "select-fld",
+                    dataSource: {},
+                    others: {}
+                  }}
+                />
+
+                <AlagehAutoComplete
+                  div={{ className: "col-3 form-group" }}
+                  label={{
+                    forceLabel: "Leave Encashment Process",
+                    isImp: false
+                  }}
+                  selector={{
+                    name: "",
+                    className: "select-fld",
+                    dataSource: {},
+                    others: {}
+                  }}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="portlet portlet-bordered attendanceSettings">
+            <div className="portlet-title">
+              <div className="caption">
+                <h3 className="caption-subject">Attendance Settings</h3>
+              </div>
+            </div>
+            <div className="portlet-body">
+              <div className="row">
+                <div className="col-8">
                   <div className="row">
                     <AlagehAutoComplete
                       div={{ className: "col-4 form-group" }}
-                      label={{ forceLabel: "Type of Attendance", isImp: false }}
+                      label={{
+                        forceLabel: "Type of Attendance",
+                        isImp: false
+                      }}
                       selector={{
                         name: "",
                         className: "select-fld",
@@ -85,7 +305,10 @@ export default class AttendanceSettings extends Component {
 
                     <AlagehAutoComplete
                       div={{ className: "col form-group" }}
-                      label={{ forceLabel: "Daily OT Calc Type", isImp: false }}
+                      label={{
+                        forceLabel: "Daily OT Calc Type",
+                        isImp: false
+                      }}
                       selector={{
                         name: "",
                         className: "select-fld",
@@ -212,137 +435,135 @@ export default class AttendanceSettings extends Component {
                     </div>
                   </div>
                 </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="col-4">
-          <div className="portlet portlet-bordered margin-bottom-15 margin-top-15">
-            <div className="portlet-body">
-              <div
-                className="col-12 algaehLabelFormGroup margin-top-15"
-                style={{ marginBottom: 25 }}
-              >
-                <label className="algaehLabelGroup">
-                  Bio Metric Database Setup
-                </label>
-                <div className="row">
-                  <AlagehAutoComplete
-                    div={{ className: "col-12 form-group" }}
-                    label={{ forceLabel: "Database Type", isImp: false }}
-                    selector={{
-                      name: "",
-                      className: "select-fld",
-                      dataSource: {},
-                      others: {}
-                    }}
-                  />
+                <div className="col-4">
+                  <div
+                    className="col-12 algaehLabelFormGroup margin-top-15"
+                    style={{ marginBottom: 25 }}
+                  >
+                    <label className="algaehLabelGroup">
+                      Bio Metric Database Setup
+                    </label>
+                    <div className="row">
+                      <AlagehAutoComplete
+                        div={{ className: "col-12 form-group" }}
+                        label={{ forceLabel: "Database Type", isImp: false }}
+                        selector={{
+                          name: "",
+                          className: "select-fld",
+                          dataSource: {},
+                          others: {}
+                        }}
+                      />
 
-                  <AlagehFormGroup
-                    div={{ className: "col-12 form-group" }}
-                    label={{
-                      forceLabel: "Server Name",
-                      isImp: false
-                    }}
-                    textBox={{
-                      className: "txt-fld",
-                      name: "",
-                      value: "",
-                      events: {},
-                      others: {
-                        type: "text"
-                      }
-                    }}
-                  />
+                      <AlagehFormGroup
+                        div={{ className: "col-12 form-group" }}
+                        label={{
+                          forceLabel: "Server Name",
+                          isImp: false
+                        }}
+                        textBox={{
+                          className: "txt-fld",
+                          name: "",
+                          value: "",
+                          events: {},
+                          others: {
+                            type: "text"
+                          }
+                        }}
+                      />
 
-                  <AlagehFormGroup
-                    div={{ className: "col-12 form-group" }}
-                    label={{
-                      forceLabel: "Login ID",
-                      isImp: false
-                    }}
-                    textBox={{
-                      className: "txt-fld",
-                      name: "",
-                      value: "",
-                      events: {},
-                      others: {
-                        type: "text"
-                      }
-                    }}
-                  />
+                      <AlagehFormGroup
+                        div={{ className: "col-12 form-group" }}
+                        label={{
+                          forceLabel: "Login ID",
+                          isImp: false
+                        }}
+                        textBox={{
+                          className: "txt-fld",
+                          name: "",
+                          value: "",
+                          events: {},
+                          others: {
+                            type: "text"
+                          }
+                        }}
+                      />
 
-                  <AlagehFormGroup
-                    div={{ className: "col-12 form-group" }}
-                    label={{
-                      forceLabel: "Password",
-                      isImp: false
-                    }}
-                    textBox={{
-                      className: "txt-fld",
-                      name: "",
-                      value: "",
-                      events: {},
-                      others: {
-                        type: "password"
-                      }
-                    }}
-                  />
+                      <AlagehFormGroup
+                        div={{ className: "col-12 form-group" }}
+                        label={{
+                          forceLabel: "Password",
+                          isImp: false
+                        }}
+                        textBox={{
+                          className: "txt-fld",
+                          name: "",
+                          value: "",
+                          events: {},
+                          others: {
+                            type: "password"
+                          }
+                        }}
+                      />
 
-                  <AlagehFormGroup
-                    div={{ className: "col-12 form-group" }}
-                    label={{
-                      forceLabel: "Database",
-                      isImp: false
-                    }}
-                    textBox={{
-                      className: "txt-fld",
-                      name: "",
-                      value: "",
-                      events: {},
-                      others: {
-                        type: "text"
-                      }
-                    }}
-                  />
+                      <AlagehFormGroup
+                        div={{ className: "col-12 form-group" }}
+                        label={{
+                          forceLabel: "Database",
+                          isImp: false
+                        }}
+                        textBox={{
+                          className: "txt-fld",
+                          name: "",
+                          value: "",
+                          events: {},
+                          others: {
+                            type: "text"
+                          }
+                        }}
+                      />
 
-                  <AlagehAutoComplete
-                    div={{ className: "col-12 form-group" }}
-                    label={{ forceLabel: "Swipe card type", isImp: false }}
-                    selector={{
-                      name: "",
-                      className: "select-fld",
-                      dataSource: {},
-                      others: {}
-                    }}
-                  />
+                      <AlagehAutoComplete
+                        div={{ className: "col-12 form-group" }}
+                        label={{ forceLabel: "Swipe card type", isImp: false }}
+                        selector={{
+                          name: "",
+                          className: "select-fld",
+                          dataSource: {},
+                          others: {}
+                        }}
+                      />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
 
-        <div className="hptl-phase1-footer">
-          <div className="row">
-            <div className="col-lg-12">
-              <button
-                type="button"
-                className="btn btn-primary"
-                //   onClick={SaveDoctorCommission.bind(this, this)}
-                //disabled={this.state.saveEnable}
-              >
-                <AlgaehLabel label={{ forceLabel: "Save", returnText: true }} />
-              </button>
+          <div className="hptl-phase1-footer">
+            <div className="row">
+              <div className="col-lg-12">
+                <button
+                  type="button"
+                  className="btn btn-primary"
+                  //   onClick={SaveDoctorCommission.bind(this, this)}
+                  //disabled={this.state.saveEnable}
+                >
+                  <AlgaehLabel
+                    label={{ forceLabel: "Save", returnText: true }}
+                  />
+                </button>
 
-              <button
-                type="button"
-                className="btn btn-default"
-                //onClick={ClearData.bind(this, this)}
-              >
-                <AlgaehLabel
-                  label={{ forceLabel: "Clear", returnText: true }}
-                />
-              </button>
+                <button
+                  type="button"
+                  className="btn btn-default"
+                  //onClick={ClearData.bind(this, this)}
+                >
+                  <AlgaehLabel
+                    label={{ forceLabel: "Clear", returnText: true }}
+                  />
+                </button>
+              </div>
             </div>
           </div>
         </div>
