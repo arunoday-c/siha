7c095cbdc994eeba3d5f7c068ea6af01979466f4
Adnan <adnan.algaeh@gmail.com>
2019-02-04 17:44:30 +0530

    MRD Micro Service Structure and API Base Created

--- algaeh-installation-windows.sh
+++ algaeh-installation-windows.sh
@@ -8,7 +8,12 @@ cd ./AlgaehUtilities && echo " ----- deleting package-lock.json in Algaeh Utilit
 npm i && echo "---- linking Keys in Algaeh Utilities ----"; npm link algaeh-keys && echo "---- updating utilities in node modules ----";  npm link &&  cd ..
 cd ./DocumentManagement &&echo "---- deleting package-lock.json in Document Management ----" ; rm -f "package-lock.json"  &&  echo "---- installing node modules in Document Management ----"; npm i &&
 echo "---- linking Keys in Document Management ----"; npm link algaeh-keys && cd ..
+
 cd ./HrManagement && echo " ----- deleting package-lock.json in HR Management ---- "; rm -f "package-lock.json"  && echo "-------installing node modules in HR Management---------"; 
 npm i && echo "---- linking Keys and utilities in HR Management ----"; npm link algaeh-keys algaeh-utilities && cd ..
+
+cd ./MRD && echo " ----- deleting package-lock.json in MRD ---- "; rm -f "package-lock.json"  && echo "-------installing node modules in MRD---------"; 
+npm i && echo "---- linking Keys and utilities in MRD ----"; npm link algaeh-keys algaeh-utilities && cd ..
+
 echo "------------------------- Installation Completed -------------------------"
 
