c7b58ab99687e695e418f0c83aa28fa26f8c8150
unknown <irfan.algaeh@gmail.com>
2019-02-06 17:19:24 +0530

    changes in user logins

--- src/model/account.js
+++ src/model/account.js
@@ -174,18 +174,18 @@ let authUser = (req, res, next) => {
     let db = req.db;
     let inputData = extend(authModel, req.body);
 
-    //   SELECT algaeh_d_app_user_id, username, user_display_name,  locked, login_attempts,\
-    //   password_expiry_rule, algaeh_m_group_user_mappings_id,algaeh_d_app_group_id,\
-    //   app_group_name,group_type,app_d_app_roles_id,role_name,role_type,\
-    //   hims_d_employee_department_id, employee_id, services_id, sub_department_id \
-    //   FROM algaeh_d_app_user U inner join algaeh_m_group_user_mappings GUP \
-    //   on GUP.user_id=U.algaeh_d_app_user_id\
-    //   inner join algaeh_d_app_group G on GUP.app_group_id= G.algaeh_d_app_group_id \
-    //   inner join algaeh_d_app_roles R on GUP.role_id=R.app_d_app_roles_id\
-    //   inner join hims_m_employee_department_mappings EDM on  GUP.user_id=EDM.user_id\
-    //   inner join  algaeh_d_app_password P on U.algaeh_d_app_user_id=P.userid\
-    //  WHERE P.password=md5(?) AND U.username=? AND U.record_status='A' \
-    //   AND P.record_status='A' AND G.record_status='A' AND R.record_status='A'
+    // SELECT algaeh_d_app_user_id, username, user_display_name,  locked, user_type,login_attempts,\
+    // password_expiry_rule, algaeh_m_role_user_mappings_id,app_d_app_roles_id,app_group_id,\
+    // role_code, role_name, role_discreption, role_type,loan_authorize_privilege,leave_authorize_privilege,\
+    // algaeh_d_app_group_id, app_group_code, app_group_name, app_group_desc, group_type, \
+    // hims_d_employee_department_id, employee_id, services_id, sub_department_id \
+    // FROM  algaeh_d_app_user U inner join algaeh_m_role_user_mappings RU on RU.user_id=U.algaeh_d_app_user_id\
+    // inner join algaeh_d_app_roles R on RU.role_id=R.app_d_app_roles_id\
+    // inner join algaeh_d_app_group G on R.app_group_id=G.algaeh_d_app_group_id\
+    // inner join hims_m_employee_department_mappings EDM on  RU.user_id=EDM.user_id\
+    // inner join  algaeh_d_app_password P on U.algaeh_d_app_user_id=P.userid\
+    // WHERE P.password=md5(?) AND U.username=? AND U.record_status='A' \
+    // AND P.record_status='A' AND G.record_status='A' AND R.record_status='A'
 
     db.getConnection((error, connection) => {
       let query =
@@ -193,14 +193,14 @@ let authUser = (req, res, next) => {
         password_expiry_rule, algaeh_m_role_user_mappings_id,app_d_app_roles_id,app_group_id,\
         role_code, role_name, role_discreption, role_type,loan_authorize_privilege,leave_authorize_privilege,\
         algaeh_d_app_group_id, app_group_code, app_group_name, app_group_desc, group_type, \
-        hims_d_employee_department_id, employee_id, services_id, sub_department_id \
+         employee_id,  sub_department_id \
         FROM  algaeh_d_app_user U inner join algaeh_m_role_user_mappings RU on RU.user_id=U.algaeh_d_app_user_id\
         inner join algaeh_d_app_roles R on RU.role_id=R.app_d_app_roles_id\
         inner join algaeh_d_app_group G on R.app_group_id=G.algaeh_d_app_group_id\
-        inner join hims_m_employee_department_mappings EDM on  RU.user_id=EDM.user_id\
+        inner join hims_m_user_employee UEM on  RU.user_id=UEM.user_id\
         inner join  algaeh_d_app_password P on U.algaeh_d_app_user_id=P.userid\
         WHERE P.password=md5(?) AND U.username=? AND U.record_status='A' \
-        AND P.record_status='A' AND G.record_status='A' AND R.record_status='A'  ";
+        AND P.record_status='A' AND G.record_status='A' AND R.record_status='A' ";
       connection.query(
         query,
         [inputData.password, inputData.username],
