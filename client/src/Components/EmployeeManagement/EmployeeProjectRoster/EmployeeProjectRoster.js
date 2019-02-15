824291ca40db8cf01b955ce2322d54fd9b80c63f
Adnan <adnan.algaeh@gmail.com>
2019-02-15 16:53:44 +0530

    Project Rostering Screen Design

---
+++ client/src/Components/EmployeeManagement/EmployeeProjectRoster/EmployeeProjectRoster.js
@@ -0,0 +1,411 @@
+import React, { Component } from "react";
+import "./EmployeeProjectRoster.css";
+import { AlagehAutoComplete, AlgaehLabel } from "../../Wrapper/algaehWrapper";
+import AlgaehSearch from "../../Wrapper/globalSearch";
+import { getYears } from "../../../utils/GlobalFunctions";
+import { MONTHS } from "../../../utils/GlobalVariables.json";
+import Employee from "../../../Search/Employee.json";
+import moment from "moment";
+import { algaehApiCall, swalMessage } from "../../../utils/algaehApiCall";
+import Enumerable from "linq";
+
+class EmployeeProjectRoster extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      employees: [],
+      hospitals: [],
+      projects: [],
+      loading: false,
+      hospital_id: JSON.parse(sessionStorage.getItem("CurrencyDetail"))
+        .hims_d_hospital_id,
+      year: moment().year(),
+      month: moment(new Date()).format("M"),
+      formatingString: this.monthFormatorString(moment().startOf("month"))
+    };
+    this.getSubDepartments();
+    this.getHospitals();
+  }
+
+  getHospitals() {
+    algaehApiCall({
+      uri: "/organization/getOrganization",
+      method: "GET",
+      onSuccess: res => {
+        if (res.data.success) {
+          this.setState({
+            hospitals: res.data.records
+          });
+        }
+      },
+
+      onFailure: err => {}
+    });
+  }
+
+  dropDownHandler(value) {
+    switch (value.name) {
+      case "month":
+        this.setState(
+          {
+            [value.name]: value.value
+          },
+          () => {
+            this.getStartandMonthEnd();
+            this.getEmployeesForShiftRoster();
+          }
+        );
+        break;
+      case "year":
+        this.setState(
+          {
+            [value.name]: value.value
+          },
+          () => {
+            this.getStartandMonthEnd();
+            this.getEmployeesForShiftRoster();
+          }
+        );
+        break;
+
+      default:
+        this.setState({
+          [value.name]: value.value
+        });
+        break;
+    }
+  }
+
+  employeeSearch(e) {
+    if (
+      this.state.sub_department_id === null ||
+      this.state.sub_department_id === undefined
+    ) {
+      swalMessage({
+        title: "Please Select a department",
+        type: "warning"
+      });
+    } else {
+      AlgaehSearch({
+        searchGrid: {
+          columns: Employee
+        },
+        searchName: "users",
+        uri: "/gloabelSearch/get",
+        inputs: " E.sub_department_id=" + this.state.sub_department_id,
+        onContainsChange: (text, serchBy, callBack) => {
+          callBack(text);
+        },
+        onRowSelect: row => {
+          let arr = this.state.employees;
+          arr.push(row);
+
+          this.setState({
+            hims_d_employee_id: row.hims_d_employee_id,
+            emp_name: row.full_name,
+            employees: arr
+          });
+        }
+      });
+    }
+  }
+
+  getSubDepartments() {
+    algaehApiCall({
+      uri: "/department/get/subdepartment",
+      method: "GET",
+      module: "masterSettings",
+      onSuccess: res => {
+        if (res.data.success) {
+          this.setState({
+            sub_depts: res.data.records
+          });
+        }
+      },
+      onFailure: err => {
+        swalMessage({
+          title: err.message,
+          type: "error"
+        });
+      }
+    });
+  }
+
+  monthFormatorString(yearAndMonth) {
+    const _start = moment(yearAndMonth)
+      .startOf("month")
+      .format("MMM DD YYYY");
+    const _end = moment(yearAndMonth)
+      .endOf("month")
+      .format("MMM DD YYYY");
+
+    return _start + " - " + _end;
+  }
+
+  getStartandMonthEnd() {
+    let yearMonth = this.state.year + "-" + this.state.month + "-01";
+    let startDate = moment(yearMonth)
+      .startOf("month")
+      .format("MMM DD YYYY");
+    let endDate = moment(yearMonth)
+      .endOf("month")
+      .format("MMM DD YYYY");
+
+    this.setState({
+      formatingString: startDate + " - " + endDate
+    });
+  }
+
+  render() {
+    let allYears = getYears();
+    return (
+      <div className="EmployeeProjectRoster">
+        <div className="row  inner-top-search">
+          <AlagehAutoComplete
+            div={{ className: "col" }}
+            label={{
+              forceLabel: "Select a Year.",
+              isImp: true
+            }}
+            selector={{
+              name: "year",
+              className: "select-fld",
+              value: this.state.year,
+              dataSource: {
+                textField: "name",
+                valueField: "value",
+                data: allYears
+              },
+              onChange: this.dropDownHandler.bind(this),
+              onClear: () => {
+                this.setState({
+                  year: null
+                });
+              }
+            }}
+          />
+          <AlagehAutoComplete
+            div={{ className: "col" }}
+            label={{
+              forceLabel: "Select a Month.",
+              isImp: true
+            }}
+            selector={{
+              sort: "off",
+              sort: "off",
+              name: "month",
+              className: "select-fld",
+              value: this.state.month,
+              dataSource: {
+                textField: "name",
+                valueField: "value",
+                data: MONTHS
+              },
+              onChange: this.dropDownHandler.bind(this),
+              onClear: () => {
+                this.setState({
+                  month: null
+                });
+              }
+            }}
+          />
+          <AlagehAutoComplete
+            div={{ className: "col form-group" }}
+            label={{
+              forceLabel: "Filter by Branch",
+              isImp: true
+            }}
+            selector={{
+              name: "hospital_id",
+              className: "select-fld",
+              value: this.state.hospital_id,
+              dataSource: {
+                textField: "hospital_name",
+                valueField: "hims_d_hospital_id",
+                data: this.state.hospitals
+              },
+              onChange: this.dropDownHandler.bind(this)
+            }}
+            showLoading={true}
+          />
+
+          <AlagehAutoComplete
+            div={{ className: "col form-group" }}
+            label={{ forceLabel: "Select Department", isImp: true }}
+            selector={{
+              name: "sub_department_id",
+              value: this.state.sub_department_id,
+              className: "select-fld",
+              dataSource: {
+                textField: "sub_department_name",
+                valueField: "hims_d_sub_department_id",
+                data: this.state.sub_depts
+              },
+              onChange: this.dropDownHandler.bind(this),
+              onClear: () => {
+                this.setState({
+                  hims_d_sub_department_id: null
+                });
+              }
+            }}
+          />
+
+          <AlagehAutoComplete
+            div={{ className: "col form-group" }}
+            label={{ forceLabel: "Select Project", isImp: true }}
+            selector={{
+              name: "project_id",
+              value: this.state.project_id,
+              className: "select-fld",
+              dataSource: {
+                textField: "shift_description",
+                valueField: "hims_d_shift_id",
+                data: this.state.projects
+              },
+              onChange: this.dropDownHandler.bind(this),
+              onClear: () => {
+                this.setState({
+                  project_id: null
+                });
+              }
+            }}
+          />
+
+          <div className="col-3" style={{ marginTop: 10 }}>
+            <div
+              className="row"
+              style={{
+                border: " 1px solid #ced4d9",
+                borderRadius: 5,
+                marginLeft: 0
+              }}
+            >
+              <div className="col">
+                <AlgaehLabel label={{ forceLabel: "Select a Employee." }} />
+                <h6> {this.state.emp_name ? this.state.emp_name : "------"}</h6>
+              </div>
+              <div
+                className="col-lg-3"
+                style={{ borderLeft: "1px solid #ced4d8" }}
+              >
+                <i
+                  className="fas fa-search fa-lg"
+                  style={{
+                    paddingTop: 17,
+                    paddingLeft: 3,
+                    cursor: "pointer"
+                  }}
+                  onClick={this.employeeSearch.bind(this)}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="col form-group">
+            <button
+              // onClick={this.getEmployeesForShiftRoster.bind(this)}
+              style={{ marginTop: 21 }}
+              className="btn btn-primary"
+            >
+              {!this.state.loading ? (
+                <span>Load</span>
+              ) : (
+                <i className="fas fa-spinner fa-spin" />
+              )}
+            </button>
+            <button
+              //  onClick={this.clearState.bind(this)}
+              style={{ marginTop: 21, marginLeft: 5 }}
+              className="btn btn-default"
+            >
+              Clear
+            </button>
+          </div>
+        </div>
+
+        <div className="row">
+          <div className="col-12">
+            <div className="portlet portlet-bordered margin-bottom-15">
+              <div className="portlet-title">
+                <div className="caption">
+                  <h3 className="caption-subject">
+                    Project Rostering List :{" "}
+                    <b style={{ color: "#33b8bc" }}>
+                      {this.state.formatingString}
+                    </b>
+                  </h3>
+                </div>
+                <div className="actions">
+                  <span style={{ background: "#3f9c62" }} className="legends">
+                    Weekly Off (WO)
+                  </span>
+                  <span style={{ background: "#3f789c" }} className="legends">
+                    Holiday (H)
+                  </span>
+                  <span style={{ background: "#879c3f" }} className="legends">
+                    Leave Authorized (LV)
+                  </span>
+                  <span style={{ background: "#9c7d3f" }} className="legends">
+                    Leave Applied (LA)
+                  </span>
+                </div>
+              </div>
+              <div className="portlet-body">
+                <div className="col-12" id="projectRosterTable">
+                  <div className="row">
+                    {this.state.employees.length === 0 ? (
+                      <div className="noTimeSheetData">
+                        <h1>Employee Project Roster</h1>
+                        <i className="fas fa-user-clock" />
+                      </div>
+                    ) : (
+                      <table>
+                        <thead id="tHdRstr">
+                          <tr>
+                            <th>Employee Code</th>
+                            <th>Employee Name</th>
+                            {this.getDaysOfMonth()}
+                            <th>Joining Date</th>
+                            <th>Exit Date</th>
+                          </tr>
+                        </thead>
+                        {/* <div className="tbodyScrollCntr"> */}
+                        <tbody>
+                          {this.state.employees.map((row, index) => (
+                            <tr key={row.hims_d_employee_id}>
+                              <td>{row.employee_code}</td>
+                              <td>{row.employee_name}</td>
+
+                              {this.plotEmployeeDates(
+                                row.hims_d_employee_id,
+                                row.holidays,
+                                row.employeeLeaves
+                              )}
+                              <td>
+                                {moment(row.date_of_joining).format(
+                                  "DD-MM-YYYY"
+                                )}
+                              </td>
+                              <td>
+                                {row.exit_date
+                                  ? moment(row.exit_date).format("DD-MM-YYYY")
+                                  : "------"}
+                              </td>
+                            </tr>
+                          ))}
+                        </tbody>
+                        {/* </div> */}
+                      </table>
+                    )}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default EmployeeProjectRoster;
