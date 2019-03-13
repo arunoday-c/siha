cd365db58faa09dcf5a0c6ca79ca1c33e215839e
syed noor <syednoor.algaeh@gmail.com>
2019-03-13 13:13:18 +0530

    test commit

--- src/model/algaehMasters.js
+++ src/model/algaehMasters.js
@@ -1,11 +1,6 @@
 "use strict";
 import extend from "extend";
-import {
-  whereCondition,
-  deleteRecord,
-  releaseDBConnection,
-  jsonArrayToObject
-} from "../utils";
+import { whereCondition, deleteRecord, releaseDBConnection } from "../utils";
 //import moment from "moment";
 import httpStatus from "../utils/httpStatus";
 //import { LINQ } from "node-linq";
