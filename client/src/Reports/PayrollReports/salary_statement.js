989ec15a9141d2f2ab57a7cf51009fcd8ff1aee4
Aboobacker Sidhiqe <>
2019-04-09 12:46:59 +0530

    Salary Statement fix

--- client/src/Reports/PayrollReports/salary_statement.js
+++ client/src/Reports/PayrollReports/salary_statement.js
@@ -25,12 +25,14 @@ export function printReport(data) {
                 <th style="width:250px">Employee Name</th>
                 <th>Department</th>
                 <th>Nationality</th>
+                <th>Branch</th>
+                <th>Advance Due</th>
+                <th style="width:135px">Loan Due Amount</th>
                 <th>Total Earnings</th>
                 <th style="width:135px">Total Deductions</th>
                 <th style="width:150px">Total Contributions</th>
                 <th>Net Salary</th>
-                <th>Advance Due</th>
-                <th style="width:135px">Loan Due Amount</th>
+                
             </tr>
         </thead></table></div><div class="tbl-content" style="max-height:26vh" algaeh-report-table="true" >
         <table  class="reportFixedTable" cellpadding="0" cellspacing="0" border="0"> 
@@ -44,12 +46,14 @@ export function printReport(data) {
      <td class="left"   style="width:250px">${list.employee_name}</td>
      <td class="center" >${list.sub_department_name}</td>
      <td class="center" >${list.nationality}</td>
+     <td class="center" >${list.hospital_name}</td>
+     <td class="right" >${list.advance_due} </td>
+     <td class="right"  style="width:135px">${list.loan_due_amount} </td>
      <td class="right" >${list.total_earnings} </td>
      <td class="right"   style="width:135px">${list.total_deductions} </td>
      <td class="right"  style="width:150px">${list.total_contributions} </td>
      <td class="right" >${list.net_salary} </td>
-     <td class="right" >${list.advance_due} </td>
-     <td class="right"  style="width:135px">${list.loan_due_amount} </td>
+     
 
   
 </tr>
