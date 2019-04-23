5dc0eb63c55e933d02c2a1616ad238b6512711e4
Aboobacker Sidhiqe <>
2019-04-23 12:52:06 +0530

    Employee Department issues + New Add to Mobile UI and Favicon for Mobile App

--- src/EHR/model/doctorsWorkBench.js
+++ src/EHR/model/doctorsWorkBench.js
@@ -761,11 +761,12 @@ let getMyDay = (req, res, next) => {
     _mysql
       .executeQuery({
         query:
-          "select  E.hims_f_patient_encounter_id,P.patient_code,P.full_name,E.patient_id ,V.appointment_patient,E.provider_id,E.`status`,E.nurse_examine,E.checked_in,\
+          "select  E.hims_f_patient_encounter_id,P.patient_code,P.full_name,P.gender,P.age,E.patient_id ,V.appointment_patient,V.new_visit_patient,E.provider_id,E.`status`,E.nurse_examine,E.checked_in,\
          E.payment_type,E.episode_id,E.encounter_id,E.`source`,E.updated_date as encountered_date,E.visit_id ,sub_department_id from hims_f_patient_encounter E\
          INNER JOIN hims_f_patient P ON E.patient_id=P.hims_d_patient_id \
             inner join hims_f_patient_visit V on E.visit_id=V.hims_f_patient_visit_id  where E.record_status='A' AND  V.record_status='A' AND " +
-          _query
+          _query,
+        printQuery: true
       })
       .then(result => {
         _mysql.releaseConnection();
