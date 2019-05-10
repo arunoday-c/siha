a8706026990deabcce546c721992dd269a210043
unknown <irfan.algaeh@gmail.com>
2019-05-10 14:40:24 +0530

    added api getGlassPrescription

--- src/EHR/model/opthometry.js
+++ src/EHR/model/opthometry.js
@@ -144,6 +144,50 @@ let addGlassPrescription = (req, res, next) => {
   }
 };
 
+let getGlassPrescription = (req, res, next) => {
+  const _mysql = new algaehMysql({ path: keyPath });
+
+  let str_qry = "";
+
+  if (req.query.hims_f_glass_prescription_id > 0) {
+    str_qry = `where hims_f_glass_prescription_id=${
+      req.query.hims_f_glass_prescription_id
+    }`;
+  }
+  try {
+    _mysql
+      .executeQuery({
+        query: `select hims_f_glass_prescription_id,patient_id,encounter_id,provider_id,episode_id,prescription_date,pgp_power_right_odsph,\
+            pgp_power_right_odcyl,pgp_power_right_odaxis,pgp_power_right_odadd,pgp_power_right_ossph,pgp_power_right_oscyl,\
+            pgp_power_right_osaxis,pgp_power_right_osadd,pgp_power_left_odsph,pgp_power_left_odcyl,pgp_power_left_odaxis,\
+            pgp_power_left_odadd,pgp_power_left_ossph,pgp_power_left_oscyl,pgp_power_left_osaxis,pgp_power_left_osadd,\
+            cva_specs,cva_cl,cva_dv_right,cva_dv_left,cva_nv_right,cva_nv_left,auto_ref_right_sch,auto_ref_right_cyl,\
+            auto_ref_right_axis,auto_ref_left_sch,auto_ref_left_cyl,auto_ref_left_axis,bcva_dv_right_sch,bcva_dv_right_cyl,\
+            bcva_dv_right_axis,bcva_dv_right_vision,bcva_nv_right_sch,bcva_nv_right_cyl,bcva_nv_right_axis,bcva_nv_right_vision,\
+            bcva_dv_left_sch,bcva_dv_left_cyl,bcva_dv_left_axis,bcva_dv_left_vision,bcva_nv_left_sch,bcva_nv_left_cyl,bcva_nv_left_axis,\
+            bcva_nv_left_vision,k1_right,k2_right,axis_right,k1_left,k2_left,axis_left,bcva_dv_right_prism,bcva_dv_right_bc,bcva_dv_right_dia,\
+            bcva_nv_right_prism,bcva_nv_right_bc,bcva_nv_right_dia,bcva_dv_left_prism,bcva_dv_left_bc,bcva_dv_left_dia,bcva_nv_left_prism,\
+            bcva_nv_left_bc,bcva_nv_left_dia,pachy_right,pachy_left,w_wcs_right,w_wcs_left,ac_depth_right,ac_depth_left,ipd_right,\
+            ipd_left,color_vision_wnl_right,color_vision_wnl_left,confrontation_fields_full_right,confrontation_fields_full_left,\
+            pupils_errl_right,pupils_errl_left,cover_test_ortho_right,cover_test_ortho_left,covergence,safe_fesa,glass,polycarbonate,\
+            cr_39,unifocal,d_bifocal,kryptok,progressive,office_lense,tint,photochromic,advice_arc,advice_src,polarised,advice_csl,cl_type,remarks from hims_f_glass_prescription ${str_qry} `
+      })
+      .then(result => {
+        _mysql.releaseConnection();
+        req.records = result;
+        next();
+      })
+      .catch(error => {
+        _mysql.releaseConnection();
+        next(error);
+      });
+  } catch (e) {
+    _mysql.releaseConnection();
+    next(e);
+  }
+};
+
 module.exports = {
-  addGlassPrescription
+  addGlassPrescription,
+  getGlassPrescription
 };
