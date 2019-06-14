a427ab43850fc6a908119d2ef7179de033144333
Syed Noor <noor@Syeds-MacBook-Air.local>
2019-06-14 18:05:46 +0300

    time fix in wrappers

--- client/src/Components/Wrapper/datePicker.js
+++ client/src/Components/Wrapper/datePicker.js
@@ -1,156 +1,178 @@
-import React, { PureComponent } from 'react';
-import Label from '../Wrapper/label';
-import '../Wrapper/wrapper.css';
-import { getCookie } from '../../utils/algaehApiCall.js';
-import moment from 'moment';
-import config from '../../utils/config.json';
-import { checkSecurity } from '../../utils/GlobalFunctions';
+import React, { PureComponent } from "react";
+import Label from "../Wrapper/label";
+import "../Wrapper/wrapper.css";
+import { getCookie } from "../../utils/algaehApiCall.js";
+import moment from "moment";
+import config from "../../utils/config.json";
+import { checkSecurity } from "../../utils/GlobalFunctions";
 // import "../../../node_modules/hijri-date-picker/build/css/index.css";
 export default class DateHandler extends PureComponent {
-	generateLabel = () => {
-		if (this.props.label != null) {
-			return <Label label={this.props.label} />;
-		}
-	};
+  generateLabel = () => {
+    if (this.props.label != null) {
+      return <Label label={this.props.label} />;
+    }
+  };
 
-	constructor(props) {
-		super(props);
-		let momentDate = props.value ? moment(props.value) : null;
-		this.state = {
-			language: 'en',
-			value: momentDate,
-			hasSecurity: false
-		};
-	}
+  constructor(props) {
+    super(props);
+    let momentDate = props.value ? moment(props.value) : null;
+    this.state = {
+      language: "en",
+      value: momentDate,
+      hasSecurity: false
+    };
+  }
 
-	componentWillReceiveProps(nextProps) {
-		if (this.state.hasSecurity) return;
-		let momentDate = nextProps.value ? moment(nextProps.value) : null;
-		let lang = getCookie('Language');
-		this.setState({
-			language: lang === null ? lang : 'en',
-			value: momentDate ? (momentDate.isValid() ? momentDate._d : null) : null
-		});
-	}
+  componentWillReceiveProps(nextProps) {
+    if (this.state.hasSecurity) return;
+    let momentDate = nextProps.value ? moment(nextProps.value) : null;
+    let lang = getCookie("Language");
+    this.setState({
+      language: lang === null ? lang : "en",
+      value: momentDate ? (momentDate.isValid() ? momentDate._d : null) : null
+    });
+  }
 
-	componentDidMount() {
-		const _hasSecurity = this.getSecurityCheck();
-		if (_hasSecurity) {
-			this.setState({ hasSecurity: true });
-			return;
-		}
-	}
+  componentDidMount() {
+    const _hasSecurity = this.getSecurityCheck();
+    if (_hasSecurity) {
+      this.setState({ hasSecurity: true });
+      return;
+    }
+  }
 
-	getSecurityCheck() {
-		let hasSecurity = false;
-		if (this.props.textBox.security !== undefined) {
-			const _security = this.props.textBox.security;
+  getSecurityCheck() {
+    let hasSecurity = false;
+    if (this.props.textBox.security !== undefined) {
+      const _security = this.props.textBox.security;
 
-			checkSecurity({
-				securityType: 'element',
-				component_code: _security.component_code,
-				module_code: _security.module_code,
-				screen_code: _security.screen_code,
-				screen_element_code: _security.screen_element_code,
-				hasSecurity: () => {
-					hasSecurity = true;
-				}
-			});
-		}
-		return hasSecurity;
-	}
+      checkSecurity({
+        securityType: "element",
+        component_code: _security.component_code,
+        module_code: _security.module_code,
+        screen_code: _security.screen_code,
+        screen_element_code: _security.screen_element_code,
+        hasSecurity: () => {
+          hasSecurity = true;
+        }
+      });
+    }
+    return hasSecurity;
+  }
 
-	onDayChange = (e) => {
-		if (this.props.singleOutput === undefined) {
-			if (this.props.events !== undefined && typeof this.props.events.onChange === 'function') {
-				this.props.events.onChange(moment(e.target.value, config.formators.date)._d, e.target.name);
-			}
-		} else {
-			if (this.props.events !== undefined && typeof this.props.events.onChange === 'function') {
-				this.props.events.onChange({
-					value: moment(e.target.value, config.formators.date)._d,
-					name: e.target.name
-				});
-			}
-		}
-	};
+  onDayChange = e => {
+    if (this.props.singleOutput === undefined) {
+      if (
+        this.props.events !== undefined &&
+        typeof this.props.events.onChange === "function"
+      ) {
+        const configType =
+          this.props.type !== undefined && this.props.type === "time"
+            ? config.formators.time
+            : config.formators.date;
+        this.props.events.onChange(
+          moment(e.target.value, configType)._d,
+          e.target.name
+        );
+      }
+    } else {
+      if (
+        this.props.events !== undefined &&
+        typeof this.props.events.onChange === "function"
+      ) {
+        const configType =
+          this.props.type !== undefined && this.props.type === "time"
+            ? config.formators.time
+            : config.formators.date;
+        this.props.events.onChange({
+          value: moment(e.target.value, configType)._d,
+          name: e.target.name
+        });
+      }
+    }
+  };
 
-	renderDatePicker = () => {
-		const minDate =
-			this.props.minDate !== undefined
-				? {
-						min: moment(this.props.minDate).format(
-							this.props.type !== undefined && this.props.type === 'time'
-								? config.formators.time
-								: config.formators.date
-						)
-					}
-				: {};
-		const maxDate =
-			this.props.maxDate !== undefined
-				? {
-						max: moment(this.props.maxDate).format(
-							this.props.type !== undefined && this.props.type === 'time'
-								? config.formators.time
-								: config.formators.date
-						)
-					}
-				: {};
-		const value =
-			this.state.value !== undefined
-				? this.state.value !== null
-					? moment(this.state.value).format(
-							this.props.type !== undefined && this.props.type === 'time'
-								? config.formators.time
-								: config.formators.date
-						)
-					: ''
-				: '';
-		const name = this.props.textBox.name !== undefined ? { name: this.props.textBox.name } : {};
-		const _required =
-			this.props.label !== undefined
-				? this.props.label.isImp !== undefined ? { required: this.props.label.isImp } : {}
-				: {};
-		const _isHijri = this.props.textBox.hijri !== undefined ? true : false;
-		const _type = this.props.type !== undefined ? this.props.type : 'date';
-		return (
-			<div className="algaeh-datePicker">
-				{!_isHijri ? (
-					<input
-						data_role="datepicker"
-						type={_type}
-						value={value}
-						{...name}
-						{...maxDate}
-						{...minDate}
-						disabled={this.props.disabled}
-						onChange={this.onDayChange.bind(this)}
-						{...this.props.textBox.others}
-						{..._required}
-					/>
-				) : null}
-			</div>
-		);
-	};
+  renderDatePicker = () => {
+    const minDate =
+      this.props.minDate !== undefined
+        ? {
+            min: moment(this.props.minDate).format(
+              this.props.type !== undefined && this.props.type === "time"
+                ? config.formators.time
+                : config.formators.date
+            )
+          }
+        : {};
+    const maxDate =
+      this.props.maxDate !== undefined
+        ? {
+            max: moment(this.props.maxDate).format(
+              this.props.type !== undefined && this.props.type === "time"
+                ? config.formators.time
+                : config.formators.date
+            )
+          }
+        : {};
+    const value =
+      this.state.value !== undefined
+        ? this.state.value !== null
+          ? moment(this.state.value).format(
+              this.props.type !== undefined && this.props.type === "time"
+                ? config.formators.time
+                : config.formators.date
+            )
+          : ""
+        : "";
+    const name =
+      this.props.textBox.name !== undefined
+        ? { name: this.props.textBox.name }
+        : {};
+    const _required =
+      this.props.label !== undefined
+        ? this.props.label.isImp !== undefined
+          ? { required: this.props.label.isImp }
+          : {}
+        : {};
+    const _isHijri = this.props.textBox.hijri !== undefined ? true : false;
+    const _type = this.props.type !== undefined ? this.props.type : "date";
+    return (
+      <div className="algaeh-datePicker">
+        {!_isHijri ? (
+          <input
+            data_role="datepicker"
+            type={_type}
+            value={value}
+            {...name}
+            {...maxDate}
+            {...minDate}
+            disabled={this.props.disabled}
+            onChange={this.onDayChange.bind(this)}
+            {...this.props.textBox.others}
+            {..._required}
+          />
+        ) : null}
+      </div>
+    );
+  };
 
-	render() {
-		if (this.state.hasSecurity) return null;
-		else {
-			if (this.props.div != null) {
-				return (
-					<div className={this.props.div.className} {...this.props.div.others}>
-						{this.generateLabel()}
-						{this.renderDatePicker()}
-					</div>
-				);
-			} else {
-				return (
-					<React.Fragment>
-						{this.generateLabel()}
-						{this.renderDatePicker()}
-					</React.Fragment>
-				);
-			}
-		}
-	}
+  render() {
+    if (this.state.hasSecurity) return null;
+    else {
+      if (this.props.div != null) {
+        return (
+          <div className={this.props.div.className} {...this.props.div.others}>
+            {this.generateLabel()}
+            {this.renderDatePicker()}
+          </div>
+        );
+      } else {
+        return (
+          <React.Fragment>
+            {this.generateLabel()}
+            {this.renderDatePicker()}
+          </React.Fragment>
+        );
+      }
+    }
+  }
 }
