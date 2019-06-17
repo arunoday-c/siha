fa749f7f414584e8a76a20a5fbca152bc4b6f60a
Aboobacker Sidhiqe <>
2019-06-17 19:57:07 +0530

    Convert to New Report Department wise Income + Added Item Expiry Report + Remove Const Algaeh Utilites from Report

--- AlgaehReporting/algaeh_report_tool/templates/appointmentList.js
+++ AlgaehReporting/algaeh_report_tool/templates/appointmentList.js
@@ -1,27 +1,25 @@
-import algaehUtilities from "algaeh-utilities/utilities";
 const executePDF = function executePDFMethod(options) {
-  const _ = options.loadash;
-  return new Promise(function(resolve, reject) {
-    const utilities = new algaehUtilities();
-    try {
-      let str = "";
-      let input = {};
-      let params = options.args.reportParams;
+	const _ = options.loadash;
+	return new Promise(function(resolve, reject) {
+		try {
+			let str = '';
+			let input = {};
+			let params = options.args.reportParams;
 
-      params.forEach(para => {
-        input[para["name"]] = para["value"];
-      });
+			params.forEach((para) => {
+				input[para['name']] = para['value'];
+			});
 
-      if (input.provider_id > 0) {
-        str += ` and A.provider_id= ${input.provider_id}`;
-      }
+			if (input.provider_id > 0) {
+				str += ` and A.provider_id= ${input.provider_id}`;
+			}
 
-      if (input.status_id > 0) {
-        str += ` and A.appointment_status_id= ${input.status_id}`;
-      }
-      options.mysql
-        .executeQuery({
-          query: `select hims_f_patient_appointment_id,appointment_date,appointment_from_time,appointment_to_time,
+			if (input.status_id > 0) {
+				str += ` and A.appointment_status_id= ${input.status_id}`;
+			}
+			options.mysql
+				.executeQuery({
+					query: `select hims_f_patient_appointment_id,appointment_date,appointment_from_time,appointment_to_time,
             s.description as app_status,s.default_status, patient_name,patient_code,A.age,contact_number,cancelled,appointment_remarks,
             E.full_name as doctor_name,E.employee_code as doctor_code ,cancel_reason,concat(D.full_name,'-' ,D.employee_code)as updated_by from 
             hims_f_patient_appointment A inner join hims_d_employee E on A.provider_id=E.hims_d_employee_id
@@ -30,50 +28,42 @@ const executePDF = function executePDFMethod(options) {
             left join  hims_d_employee D on  U.employee_id=D.hims_d_employee_id
             where A.sub_department_id=? ${str} and A.hospital_id=? and  appointment_date between date(?) and date(?) 
             order by appointment_date`,
-          values: [
-            input.sub_department_id,
-            options.args.hospital_id,
-            input.from_date,
-            input.to_date
-          ],
-          printQuery: true
-        })
-        .then(result => {
-          const data = _.chain(result)
-            .groupBy(g => g.doctor_code)
-            .map(function(detail, key) {
-              let cancel = detail.filter(item => {
-                return item.default_status == "CAN";
-              }).length;
-              let checked_in = detail.filter(item => {
-                return item.default_status == "C";
-              }).length;
-              let no_show = detail.filter(item => {
-                return item.default_status == "NS";
-              }).length;
+					values: [ input.sub_department_id, options.args.hospital_id, input.from_date, input.to_date ],
+					printQuery: true
+				})
+				.then((result) => {
+					const data = _.chain(result)
+						.groupBy((g) => g.doctor_code)
+						.map(function(detail, key) {
+							let cancel = detail.filter((item) => {
+								return item.default_status == 'CAN';
+							}).length;
+							let checked_in = detail.filter((item) => {
+								return item.default_status == 'C';
+							}).length;
+							let no_show = detail.filter((item) => {
+								return item.default_status == 'NS';
+							}).length;
 
-              return {
-                doctor_code: key,
-                doctor_name: detail[0]["doctor_name"],
-                checked_in: checked_in,
-                no_show: no_show,
-                cancel: cancel,
-                details: detail
-              };
-            })
-            .value();
+							return {
+								doctor_code: key,
+								doctor_name: detail[0]['doctor_name'],
+								checked_in: checked_in,
+								no_show: no_show,
+								cancel: cancel,
+								details: detail
+							};
+						})
+						.value();
 
-          utilities.logger().log("datazz: ", data);
-          resolve({ groupdetails: data });
-        })
-        .catch(error => {
-          options.mysql.releaseConnection();
-
-          console.log("error", error);
-        });
-    } catch (e) {
-      reject(e);
-    }
-  });
+					resolve({ groupdetails: data });
+				})
+				.catch((error) => {
+					options.mysql.releaseConnection();
+				});
+		} catch (e) {
+			reject(e);
+		}
+	});
 };
 module.exports = { executePDF };
--- AlgaehReporting/algaeh_report_tool/templates/dailyCollectionPharmacy.js
+++ AlgaehReporting/algaeh_report_tool/templates/dailyCollectionPharmacy.js
@@ -1,111 +1,47 @@
-import algaehUtilities from "algaeh-utilities/utilities";
 const executePDF = function executePDFMethod(options) {
-  const _ = options.loadash;
-  return new Promise(function(resolve, reject) {
-    const utilities = new algaehUtilities();
-    try {
-      utilities.logger().log("result: ", options.result);
-      let cash_collected = options.result[0];
-      let cash_retuned = options.result[1];
-      let total_returned = _.sumBy(cash_retuned, s =>
-        parseFloat(s.return_amout)
-      ).toFixed(3);
+	const _ = options.loadash;
+	return new Promise(function(resolve, reject) {
+		try {
+			let cash_collected = options.result[0];
+			let cash_retuned = options.result[1];
+			let total_returned = _.sumBy(cash_retuned, (s) => parseFloat(s.return_amout)).toFixed(3);
 
-      // let result = [];
+			const total_cash = _.chain(cash_collected)
+				.filter((f) => f.pay_type == 'CA')
+				.sumBy((s) => parseFloat(s.amount))
+				.value()
+				.toFixed(3);
 
-      //   if (
-      //     input.pay_type == "CD" ||
-      //     input.pay_type == "CH" ||
-      //     input.pay_type == "CA"
-      //   ) {
-      //     let ids = _.chain(cash_collected)
-      //       .filter(f => f.pay_type == input.pay_type)
-      //       .map(obj => obj.hims_f_receipt_header_id)
-      //       .value();
+			const total_card = _.chain(cash_collected)
+				.filter((f) => f.pay_type == 'CD')
+				.sumBy((s) => parseFloat(s.amount))
+				.value()
+				.toFixed(3);
 
-      //     ids.map(val => {
-      //       result.push(
-      //         ..._.filter(cash_collected, f => f.hims_f_receipt_header_id == val)
-      //       );
-      //     });
-      //   } else {
-      //     result = cash_collected;
-      //   }
+			const total_check = _.chain(cash_collected)
+				.filter((f) => f.pay_type == 'CH')
+				.sumBy((s) => parseFloat(s.amount))
+				.value()
+				.toFixed(3);
+			const total_collection = parseFloat(
+				parseFloat(total_cash) + parseFloat(total_card) + parseFloat(total_check)
+			).toFixed(3);
 
-      //   const data = _.chain(result)
-      //     .groupBy(g => g.hims_f_receipt_header_id)
-      //     .map(function(item, key) {
-      //       const cash = _.chain(item)
-      //         .filter(f => f.pay_type == "CA")
-      //         .sumBy(s => parseFloat(s.amount))
-      //         .value()
-      //         .toFixed(3);
+			const income = parseFloat(total_collection - total_returned).toFixed(3);
 
-      //       const card = _.chain(item)
-      //         .filter(f => f.pay_type == "CD")
-      //         .sumBy(s => parseFloat(s.amount))
-      //         .value()
-      //         .toFixed(3);
-      //       const check = _.chain(item)
-      //         .filter(f => f.pay_type == "CH")
-      //         .sumBy(s => parseFloat(s.amount))
-      //         .value()
-      //         .toFixed(3);
-      //       return {
-      //         receipt_number: item[0]["receipt_number"],
-      //         pos_number: item[0]["pos_number"],
-      //         receipt_date: item[0]["receipt_date"],
-      //         patient_full_name: item[0]["patient_full_name"],
-      //         mrn_no: item[0]["mrn_no"],
-      //         total_amount: item[0]["total_amount"],
-      //         cash: cash,
-      //         card: card,
-      //         check: check,
-      //         cashier: item[0]["cashier"],
-      //         collected_time: item[0]["collected_time"]
-      //       };
-      //     })
-      //     .value();
-
-      const total_cash = _.chain(cash_collected)
-        .filter(f => f.pay_type == "CA")
-        .sumBy(s => parseFloat(s.amount))
-        .value()
-        .toFixed(3);
-
-      const total_card = _.chain(cash_collected)
-        .filter(f => f.pay_type == "CD")
-        .sumBy(s => parseFloat(s.amount))
-        .value()
-        .toFixed(3);
-
-      const total_check = _.chain(cash_collected)
-        .filter(f => f.pay_type == "CH")
-        .sumBy(s => parseFloat(s.amount))
-        .value()
-        .toFixed(3);
-      const total_collection = parseFloat(
-        parseFloat(total_cash) +
-          parseFloat(total_card) +
-          parseFloat(total_check)
-      ).toFixed(3);
-
-      const income = parseFloat(total_collection - total_returned).toFixed(3);
-
-      const output = {
-        // details: data,
-        total_cash: total_cash,
-        total_card: total_card,
-        total_check: total_check,
-        total_collection: total_collection,
-        total_returned: total_returned,
-        income: income
-      };
-      utilities.logger().log("output: ", output);
-      resolve(output);
-    } catch (e) {
-      reject(e);
-    }
-  });
+			const output = {
+				// details: data,
+				total_cash: total_cash,
+				total_card: total_card,
+				total_check: total_check,
+				total_collection: total_collection,
+				total_returned: total_returned,
+				income: income
+			};
+			resolve(output);
+		} catch (e) {
+			reject(e);
+		}
+	});
 };
 module.exports = { executePDF };
