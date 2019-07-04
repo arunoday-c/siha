0ec9bf995f63f9fbd72b7e2f1f8454a68b57b010
Aboobacker Sidhiqe <>
2019-07-04 15:58:41 +0530

    Added Receipt Entry

---
+++ AlgaehReporting/algaeh_report_tool/templates/receiptEntryInventory.js
@@ -0,0 +1,30 @@
+const algaehUtilities = require("algaeh-utilities/utilities");
+const executePDF = function executePDFMethod(options) {
+  return new Promise(function(resolve, reject) {
+    try {
+      // resolve(options.result[0]);
+
+      const utilities = new algaehUtilities();
+
+      utilities.logger().log("outpoy: ", options.result);
+      if (options.result.length > 0) {
+        resolve({
+          net_payable: options.result[0]["net_payable"],
+          purchase_number: options.result[0]["purchase_number"],
+          po_date: options.result[0]["po_date"],
+          location_description: options.result[0]["location_description"],
+          vendor_name: options.result[0]["vendor_name"],
+          sub_department_name: options.result[0]["sub_department_name"],
+          employee_code: options.result[0]["employee_code"],
+          full_name: options.result[0]["full_name"],
+          detailList: options.result
+        });
+      } else {
+        resolve({ detailList: options.result });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+module.exports = { executePDF };
