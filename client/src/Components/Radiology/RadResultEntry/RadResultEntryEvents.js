05448a75ee208aefe7d6227e73832549f8977be6
MsP <msp.algaeh@gmail.com>
2019-07-16 19:29:54 +0530

    fixed some bugs in work list

--- client/src/Components/Radiology/RadResultEntry/RadResultEntryEvents.js
+++ client/src/Components/Radiology/RadResultEntry/RadResultEntryEvents.js
@@ -11,47 +11,6 @@ const texthandle = ($this, e) => {
   });
 };
 
-const examhandle = ($this, e) => {
-  let name = e.name || e.target.name;
-  let value = e.value || e.target.value;
-
-  if ($this.state.pre_exam_status === "CO") {
-    $this.setState({
-      [name]: $this.state.exam_status
-    });
-    swalMessage({
-      title: "After Complete cant do any changes.",
-      type: "warning"
-    });
-  } else {
-    let exam_start_date_time = null;
-    let exam_end_date_time = null;
-    let report_type = "NS";
-
-    if (value === "ST") {
-      exam_start_date_time = moment(new Date())._d;
-    } else if (value === "CO") {
-      exam_start_date_time =
-        $this.state.exam_start_date_time || moment(new Date())._d;
-      exam_end_date_time = moment(new Date())._d;
-      report_type = "PR";
-    }
-
-    $this.setState(
-      {
-        [name]: value,
-        exam_start_date_time: exam_start_date_time,
-        exam_end_date_time: exam_end_date_time,
-        report_type: report_type,
-        changesDone: true
-      },
-      () => {
-        UpdateRadOrder($this, "Exam");
-      }
-    );
-  }
-};
-
 const templatehandle = ($this, e) => {
   let name = e.name || e.target.name;
   let value = e.value || e.target.value;
@@ -66,42 +25,109 @@ const rtehandle = ($this, result_html) => {
   $this.setState({ result_html });
 };
 
+const handleExamStatus = ($this, value) => {
+  let append_message = "";
+  let functionToCall;
+  if (value === "start") {
+    append_message = "Start ";
+    functionToCall = startTest;
+  } else if (value === "cancel") {
+    append_message = "Are you sure, you want to cancel ";
+    functionToCall = cancelTest;
+  } else if (value === "completed") {
+    append_message = "Is it completed ";
+    functionToCall = completeTest;
+  } else if (value === "validate") {
+    append_message = " Validate";
+    functionToCall = validateTest;
+  }
+  swal({
+    title:
+      append_message +
+      $this.state.full_name +
+      " procedure " +
+      $this.state.service_name,
+    type: "warning",
+    showCancelButton: true,
+    confirmButtonText: "Yes",
+    confirmButtonColor: "#",
+    cancelButtonColor: "#d33",
+    cancelButtonText: "No"
+  }).then(willProceed => {
+    if (willProceed.value) {
+      if (typeof functionToCall === "function") {
+        functionToCall($this);
+      }
+    }
+  });
+};
+
+const startTest = $this => {
+  $this.setState(
+    {
+      exam_status: "ST",
+      exam_start_date_time: moment(new Date())._d,
+      report_type: "PR",
+      status: "UP",
+      changesDone: true
+    },
+    () => UpdateRadOrder($this, "start")
+  );
+};
+
+const cancelTest = $this => {
+  $this.setState(
+    {
+      exam_status: "CN",
+      status: "CN",
+      report_type: "PR",
+      changesDone: true
+    },
+    () => UpdateRadOrder($this, "cancel")
+  );
+};
+
+const completeTest = $this => {
+  $this.setState(
+    {
+      exam_status: "CO",
+      exam_end_date_time: moment(new Date())._d,
+      status: "RC",
+      report_type: "FL",
+      changesDone: true
+    },
+    () => UpdateRadOrder($this, "complete")
+  );
+};
+
+const validateTest = $this => {
+  if ($this.state.result_html === null) {
+    swalMessage({
+      title: "Please select the template for result.",
+      type: "warning"
+    });
+    return;
+  } else {
+    $this.setState(
+      {
+        status: "RA"
+      },
+      () => UpdateRadOrder($this, "validate")
+    );
+  }
+};
+
 const UpdateRadOrder = ($this, value) => {
-  if ($this.state.template_id === null && value === "Validate") {
+  if ($this.state.template_id === null && value === "validate") {
     swalMessage({
       title: "Please Select Template",
       type: "warning"
     });
   } else {
     let inputobj = {};
-    let status = "",
-      report_type = "",
-      append_message = "";
-    if ($this.state.exam_status === "AW") {
-      status = "UP";
-      append_message = "Awaiting to start ";
-      report_type = "PR";
-    } else if ($this.state.exam_status === "ST") {
-      status = "UP";
-      append_message = "Start ";
-      report_type = "PR";
-    } else if ($this.state.exam_status === "CN") {
-      status = "CN";
-      append_message = "Are you sure, you want to cancel ";
-      report_type = "PR";
-    } else if ($this.state.exam_status === "CO") {
-      status = "RC";
-      append_message = "Is it completed ";
-      report_type = "FL";
-    }
-
-    if (value === "Validate") {
-      status = "RA";
-      append_message = " Validate";
-    }
     inputobj = {
       hims_f_rad_order_id: $this.state.hims_f_rad_order_id,
-      status: status,
+      status: $this.state.status,
       cancelled: $this.state.cancelled,
       scheduled_date_time: moment(new Date())._d,
       scheduled_by: $this.state.scheduled_by,
@@ -117,76 +143,47 @@ const UpdateRadOrder = ($this, value) => {
       exam_start_date_time: $this.state.exam_start_date_time,
       exam_end_date_time: $this.state.exam_end_date_time,
       exam_status: $this.state.exam_status,
-      report_type: report_type,
+      report_type: $this.state.report_type,
       result_html: $this.state.result_html,
       comments: $this.state.comments
     };
-
-    swal({
-      title:
-        append_message +
-        $this.state.full_name +
-        " procedure " +
-        $this.state.service_name,
-      type: "warning",
-      showCancelButton: true,
-      confirmButtonText: "Yes",
-      confirmButtonColor: "#",
-      cancelButtonColor: "#d33",
-      cancelButtonText: "No"
-    }).then(willProceed => {
-      if (willProceed.value) {
-        algaehApiCall({
-          uri: "/radiology/updateRadOrderedServices",
-          module: "radiology",
-          data: inputobj,
-          method: "PUT",
-          onSuccess: response => {
-            if (response.data.success === true) {
-              swalMessage({
-                title: "Record updated successfully . .",
-                type: "success"
-              });
-              $this.props.getRadiologyTestList({
-                uri: "/radiology/getRadOrderedServices",
-                module: "radiology",
-                method: "GET",
-                data: inputobj,
-                redux: {
-                  type: "RAD_LIST_GET_DATA",
-                  mappingName: "radschlist"
-                },
-                afterSuccess: data => {
-                  $this.setState({
-                    isOpen: !$this.state.isOpen
-                    // status: "RA"
-                  });
-                }
+    algaehApiCall({
+      uri: "/radiology/updateRadOrderedServices",
+      module: "radiology",
+      data: inputobj,
+      method: "PUT",
+      onSuccess: response => {
+        if (response.data.success === true) {
+          swalMessage({
+            title: "Record updated successfully . .",
+            type: "success"
+          });
+          $this.props.getRadiologyTestList({
+            uri: "/radiology/getRadOrderedServices",
+            module: "radiology",
+            method: "GET",
+            data: inputobj,
+            redux: {
+              type: "RAD_LIST_GET_DATA",
+              mappingName: "radschlist"
+            },
+            afterSuccess: data => {
+              $this.setState({
+                isOpen: !$this.state.isOpen
+                // status: "RA"
               });
             }
-          },
-          onFailure: error => {
-            swalMessage({
-              title: error.message,
-              type: "error"
-            });
-          }
+          });
+        }
+      },
+      onFailure: error => {
+        swalMessage({
+          title: error.message,
+          type: "error"
         });
       }
     });
   }
 };
 
-const onvalidate = $this => {
-  if ($this.state.result_html === null) {
-    swalMessage({
-      title: "Please select the template for result.",
-      type: "warning"
-    });
-    return;
-  } else {
-    UpdateRadOrder($this, "Validate");
-  }
-};
-
-export { texthandle, examhandle, templatehandle, rtehandle, onvalidate };
+export { texthandle, templatehandle, rtehandle, handleExamStatus };
