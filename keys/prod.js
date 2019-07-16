6276f1c3710d80b5e2aae0f7a4f8ab2e6a4ee0b6
Syed Noor <noor@Syeds-MacBook-Air.local>
2019-07-16 15:38:14 +0300

    prod.js removed comments and packagae.jon remove core infront of start

--- keys/prod.js
+++ keys/prod.js
@@ -1,14 +1,11 @@
 exports.default = {
-  // API token key
   SECRETKey: "ALGAEH Technologies PVT Ltd HIMSv1",
-  TOKENTIME: 2592000, // 60 * 60 * 30 ,
-  // Server port and request body limit
+  TOKENTIME: 2592000,
   port: 3002,
   bodyLimit: "900kb",
 
   mysqlDb: {
     connectionLimit: 20,
-    // host: "166.62.10.184",
     host: "49.206.18.38",
     port: 3306,
     user: "algaeh_root",
@@ -18,26 +15,21 @@ exports.default = {
     acquireTimeout: 20000,
     dateStrings: true,
     insecureAuth: true,
-    waitForConnections: true, // Default value.
-    queueLimit: 0, // Unlimited - default value.
+    waitForConnections: true,
+    queueLimit: 0,
     supportBigNumbers: true,
     bigNumberStrings: true
   },
-
   mongoDb: {
-    connectionURI:
-      "mongodb://algaeh_root:medteam2013@49.206.18.38:27017/algaeh_hims_mongo_db"
+    connectionURI: ""
   },
-
-  //Db Date format
   dbFormat: {
     date: "YYYY-MM-DD"
   },
   useSSL: false,
-  chromePuppeteer:{
+  chromePuppeteer: {
     headless: true,
-      executablePath:
-        "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
-    
+    executablePath:
+      "/Applications/Google Chrome.app/Contents/MacOS/Google Chrome"
   }
 };
