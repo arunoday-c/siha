892871e07ecb3bfcec9de3427610f5acb18fa6a6
unknown <irfan.algaeh@gmail.com>
2019-07-19 17:15:50 +0530

    added new  billing functionality

--- Billing/src/controllers/billing.js
+++ Billing/src/controllers/billing.js
@@ -47,17 +47,17 @@ export default () => {
     }
   );
 
-  api.post(
-    "/getBillDetails",
-    getBillDetails,
+  // api.post(
+  //   "/getBillDetails",
+  //   getBillDetails,
 
-    (req, res, next) => {
-      res.status(utlities.AlgaehUtilities().httpStatus().ok).json({
-        success: true,
-        records: req.records
-      });
-    }
-  );
+  //   (req, res, next) => {
+  //     res.status(utlities.AlgaehUtilities().httpStatus().ok).json({
+  //       success: true,
+  //       records: req.records
+  //     });
+  //   }
+  // );
 
   api.get(
     "/getPakageDetails",
@@ -70,6 +70,20 @@ export default () => {
       });
     }
   );
+
+  api.post("/getBillDetails", getBillDetails, (req, res, next) => {
+    if (req.records.invalid_input == true) {
+      res.status(utlities.AlgaehUtilities().httpStatus().ok).json({
+        success: false,
+        records: req.records
+      });
+    } else {
+      res.status(utlities.AlgaehUtilities().httpStatus().ok).json({
+        success: true,
+        records: req.records
+      });
+    }
+  });
 
   return api;
 };
