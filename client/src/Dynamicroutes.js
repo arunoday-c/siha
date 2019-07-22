79999aada05b062859f4c07b0eaf214172336e88
Aboobacker Sidhiqe <>
2019-07-23 00:18:15 +0530

    Updated Shasky Dashboard as quick solution

--- client/src/Dynamicroutes.js
+++ client/src/Dynamicroutes.js
@@ -23,25 +23,24 @@ import CommonDashboard from "./Components/Dashboard/Dashboard";
 
 const PageToPlot = {
   Dashboard: CommonDashboard,
-  // 7: React.lazy(() =>
-  //   retry(() => import("./Components/Dashboard/frontdesk-dashboard"))
-  // ),
-  // 8: React.lazy(() =>
-  //   retry(() => import("./Components/Dashboard/doctor-dashboard"))
-  // ),
-  // 11: React.lazy(() =>
-  //   retry(() => import("./Components/Dashboard/lab-dashboard"))
-  // ),
-  // 12: React.lazy(() =>
-  //   retry(() => import("./Components/Dashboard/hr-dashboard"))
-  // ),
-  // 13: React.lazy(() =>
-  //   retry(() => import("./Components/Dashboard/pharmacy-dashboard"))
-  // ),
-  // 14: React.lazy(() =>
-  //   retry(() => import("./Components/Dashboard/inventory-dashboard"))
-  // ),
-
+  7: React.lazy(() =>
+    retry(() => import("./Components/Dashboard/frontdesk-dashboard"))
+  ),
+  8: React.lazy(() =>
+    retry(() => import("./Components/Dashboard/doctor-dashboard"))
+  ),
+  11: React.lazy(() =>
+    retry(() => import("./Components/Dashboard/lab-dashboard"))
+  ),
+  12: React.lazy(() =>
+    retry(() => import("./Components/Dashboard/hr-dashboard"))
+  ),
+  13: React.lazy(() =>
+    retry(() => import("./Components/Dashboard/pharmacy-dashboard"))
+  ),
+  14: React.lazy(() =>
+    retry(() => import("./Components/Dashboard/inventory-dashboard"))
+  ),
   FrontDesk: React.lazy(() =>
     retry(() => import("./Components/RegistrationPatient/RegistrationPatient"))
   ),
--- keys/dev.js
+++ keys/dev.js
@@ -20,10 +20,9 @@ exports.default = {
     database: "hims_test_db",
     // database: "twareat_live_db",
     //database: "awdesh_db",
-    //database: "hrms_shaksy_db",
+    // database: "hrms_shaksy_db",
     // database: "seco",
     // database: "algaeh_hrms_oman_db",
-
     //database: "medical_db",
 
     multipleStatements: true,
