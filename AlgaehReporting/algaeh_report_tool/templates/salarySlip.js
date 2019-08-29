b68e3083df04d936161d099c83bf6252efca5cd3
unknown <irfan.algaeh@gmail.com>
2019-08-29 19:10:19 +0530

    added salary slip

--- AlgaehReporting/algaeh_report_tool/templates/salarySlip.js
+++ AlgaehReporting/algaeh_report_tool/templates/salarySlip.js
@@ -1,19 +1,15 @@
 const algaehUtilities = require("algaeh-utilities/utilities");
-
+var writtenForm = require("written-number");
 const executePDF = function executePDFMethod(options) {
   return new Promise(function(resolve, reject) {
     try {
       const _ = options.loadash;
-
       const utilities = new algaehUtilities();
-
       let input = options.args.reportParams;
-
-      console.log("input:", input);
       options.mysql
         .executeQuery({
           query: `select hims_f_salary_id ,S.employee_id, S.year,S.month,ED.hims_d_earning_deduction_id as earning_id,
-          ED.earning_deduction_description as earning_description , 
+          ED.earning_deduction_description as earning_description , S.net_salary,
           SE.amount as earning_amount,EDD.hims_d_earning_deduction_id as deduction_id,
           EDD.earning_deduction_description as deduction_description,SD.amount as deduction_amount,
           S.total_earnings,S.total_deductions,SDP.sub_department_name, D.department_name,
@@ -32,7 +28,6 @@ const executePDF = function executePDFMethod(options) {
           printQuery: true
         })
         .then(result => {
-          utilities.logger().log("result: ", result);
           const outputArray = [];
           if (result.length > 0) {
             const employees = _.chain(result)
@@ -41,29 +36,40 @@ const executePDF = function executePDFMethod(options) {
               .value();
 
             employees.forEach(employe => {
-              const emp_earnings = _.chain(employe)
-                .groupBy(g => g.earning_id)
-                .map(m => {
-                  return {
+              const emp_earnings = [];
+              employe.forEach(m => {
+                const exist = emp_earnings.find(val => {
+                  return val.earning_id == m.earning_id;
+                });
+
+                if (!exist) {
+                  emp_earnings.push({
                     earning_id: m.earning_id,
                     earning_description: m.earning_description,
                     earning_amount: m.earning_amount
-                  };
-                })
-                .value();
+                  });
+                }
+              });
 
-              const emp_deductions = _.chain(employe)
-                .groupBy(g => g.deduction_id)
-                .map(m => {
-                  return {
+              const emp_deductions = [];
+              employe.forEach(m => {
+                const exist = emp_deductions.find(val => {
+                  return val.deduction_id == m.deduction_id;
+                });
+
+                if (!exist) {
+                  emp_deductions.push({
                     deduction_id: m.deduction_id,
                     deduction_description: m.deduction_description,
                     deduction_amount: m.deduction_amount
-                  };
-                })
-                .value();
-
+                  });
+                }
+              });
               outputArray.push({
+                year: employe[0].year,
+                month: employe[0].month,
+                net_salary: employe[0].net_salary,
+                salary_in_words: writtenForm(employe[0].net_salary) + " Only",
                 total_earnings: employe[0].total_earnings,
                 total_deductions: employe[0].total_deductions,
                 sub_department_name: employe[0].sub_department_name,
@@ -72,13 +78,12 @@ const executePDF = function executePDFMethod(options) {
                 full_name: employe[0].full_name,
                 designation: employe[0].designation,
                 hospital_name: employe[0].hospital_name,
-
                 emp_earnings: emp_earnings,
                 emp_deductions: emp_deductions
               });
             });
 
-            utilities.logger().log("outputArray: ", outputArray);
+            // utilities.logger().log("outputArray: ", outputArray);
             resolve({
               result: outputArray
             });
