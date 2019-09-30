9d807dc1f0f46a6abe4ab720b9ef2bd6cc98e192
mspriyan <mspriyang3@gmail.com>
2019-09-30 10:43:48 +0530

    merged with upstream

--- src/controller/email.js
+++
@@ -1,5 +0,0 @@
-import { Router } from "express";
-import httpStatus from "../utils/httpStatus";
-export default () => {
-  let api;
-};
--- src/db.js
+++
@@ -1,7 +0,0 @@
-import mysql from "mysql";
-import algaehKeys from "algaeh-keys"; //"./keys/keys";
-const config = algaehKeys.default;
-export default callback => {
-  let db = mysql.createPool(config.mysqlDb);
-  callback(db);
-};
