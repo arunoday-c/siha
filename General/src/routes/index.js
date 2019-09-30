a8fc203a96d822660eae11b9f36d58f62eb94234
Syed Noor <syednoor.algaeh@gmail.com>
2019-09-30 12:38:45 +0530

    development cleanup

--- General/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-import express from "express";
-import test from "../controllers/test";
-
-const router = express();
-router.use("/test", test());
-
-export default router;
--- test/cleaner-config.js
+++
@@ -1,8 +0,0 @@
-
-
-module.exports = {
-  mysql: {
-    strategy: "truncation",
-    skipTables: []
-  }
-};
