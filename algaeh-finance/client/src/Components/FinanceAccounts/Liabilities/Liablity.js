b930795c997db5456c91b02ac53601774ac5da85
Aboobacker Sidhiqe <>
2019-10-03 14:11:33 +0530

    liabilities

--- algaeh-finance/client/src/Components/FinanceAccounts/Liabilities/Liablity.js
+++ algaeh-finance/client/src/Components/FinanceAccounts/Liabilities/Liablity.js
@@ -21,8 +21,8 @@ export default function Liablity() {
 
   const treeData = [
     {
-      title: "`title`",
-      subtitle: "`subtitle`",
+      title: "Current Liabilities",
+      //subtitle: "`subtitle`",
       expanded: true,
       children: [
         {
@@ -30,112 +30,53 @@ export default function Liablity() {
           subtitle: "Defined in `children` array belonging to parent"
         },
         {
-          title: "Nested structure is rendered virtually",
-          subtitle: (
-            <span>
-              The tree uses&nbsp;
-              <a href="https://github.com/bvaughn/react-virtualized">
-                react-virtualized
-              </a>
-              &nbsp;and the relationship lines are more of a visual trick.
-            </span>
-          )
+          title: "Child Node",
+          subtitle: "Defined in `children` array belonging to parent"
         }
       ]
     },
     {
+      title: "Accounts Payable",
+      //subtitle: "`subtitle`",
       expanded: true,
-      title: "Any node can be the parent or child of any other node",
       children: [
         {
-          expanded: true,
-          title: "Chicken",
-          children: [{ title: "Egg" }]
+          title: "Child Node",
+          subtitle: "Defined in `children` array belonging to parent"
+        },
+        {
+          title: "Child Node",
+          subtitle: "Defined in `children` array belonging to parent"
         }
       ]
     },
     {
-      title: "Button(s) can be added to the node",
-      subtitle:
-        "Node info is passed when generating so you can use it in your onClick handler"
-    },
-    {
+      title: "Cheque Issued",
+      //subtitle: "`subtitle`",
       expanded: true,
-      title: "Show node children by setting `expanded`",
-      subtitle: ({ node }) => `expanded: ${node.expanded ? "true" : "false"}`,
       children: [
         {
-          title: "Bruce",
-          subtitle: ({ node }) =>
-            `expanded: ${node.expanded ? "true" : "false"}`,
-          children: [{ title: "Bruce Jr." }, { title: "Brucette" }]
+          title: "Child Node",
+          subtitle: "Defined in `children` array belonging to parent"
+        },
+        {
+          title: "Child Node",
+          subtitle: "Defined in `children` array belonging to parent"
         }
       ]
     },
     {
+      title: "Deferred Income",
+      //subtitle: "`subtitle`",
       expanded: true,
-      title: "Advanced",
-      subtitle: "Settings, behavior, etc.",
       children: [
         {
-          title: (
-            <div>
-              <div
-                style={{
-                  backgroundColor: "gray",
-                  display: "inline-block",
-                  borderRadius: 10,
-                  color: "#FFF",
-                  padding: "0 5px"
-                }}
-              >
-                Any Component
-              </div>
-              &nbsp;can be used for `title`
-            </div>
-          )
+          title: "Child Node",
+          subtitle: "Defined in `children` array belonging to parent"
         },
         {
-          expanded: true,
-          title: "Limit nesting with `maxDepth`",
-          subtitle: `It's set to ${maxDepth} for this example`,
-          children: [
-            {
-              expanded: true,
-              title: renderDepthTitle,
-              children: [
-                {
-                  expanded: true,
-                  title: renderDepthTitle,
-                  children: [
-                    { title: renderDepthTitle },
-                    {
-                      title: ({ path }) =>
-                        path.length >= maxDepth
-                          ? "This cannot be dragged deeper"
-                          : "This can be dragged deeper"
-                    }
-                  ]
-                }
-              ]
-            }
-          ]
-        },
-        {
-          title: "Disable dragging on a per-node basis with the `canDrag` prop",
-          subtitle: "Or set it to false to disable all dragging.",
-          noDragging: true
-        },
-        {
-          title: "You cannot give this children",
-          subtitle:
-            "Dropping is prevented via the `canDrop` API using `nextParent`",
-          noChildren: true
-        },
-        {
-          title:
-            "When node contents are really long, it will cause a horizontal scrollbar" +
-            " to appear. Deeply nested elements will also trigger the scrollbar."
+          title: "Child Node",
+          subtitle: "Defined in `children` array belonging to parent"
         }
       ]
     }
