71fe03ce567275539388d0e4b4bd2830f5ca755d
irfan.algaeh <irfan.algaeh@gmail.com>
2019-10-23 18:00:50 +0530

    added finance addAccountHeads api

--- algaeh-finance/src/controllers/finance.js
+++ algaeh-finance/src/controllers/finance.js
@@ -2,12 +2,25 @@ import { Router } from "express";
 import utlities from "algaeh-utilities";
 import finance from "../models/finance";
 
-const { getAccountHeads } = finance;
+const { getAccountHeads, addAccountHeads } = finance;
 
 export default () => {
   const api = Router();
 
   api.get("/getAccountHeads", getAccountHeads, (req, res, next) => {
+    if (req.records.invalid_input == true) {
+      res.status(utlities.AlgaehUtilities().httpStatus().ok).json({
+        success: false,
+        result: req.records
+      });
+    } else {
+      res.status(utlities.AlgaehUtilities().httpStatus().ok).json({
+        success: true,
+        result: req.records
+      });
+    }
+  });
+  api.post("/addAccountHeads", addAccountHeads, (req, res, next) => {
     if (req.records.invalid_input == true) {
       res.status(utlities.AlgaehUtilities().httpStatus().ok).json({
         success: false,
