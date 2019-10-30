aec00beab1a726c03ea1eacd24a9e5a8790302dd
irfan.algaeh <irfan.algaeh@gmail.com>
2019-10-30 16:11:58 +0530

    added api's getFinanceAccountsMaping,addFinanceAccountsMaping

--- algaeh-finance/src/controllers/finance.js
+++ algaeh-finance/src/controllers/finance.js
@@ -2,37 +2,100 @@ import { Router } from "express";
 import utlities from "algaeh-utilities";
 import finance from "../models/finance";
 
-const { getAccountHeads, addAccountHeads } = finance;
+const {
+  getAccountHeads,
+  addAccountHeads,
+  getFinanceAccountsMaping,
+  addFinanceAccountsMaping
+} = finance;
 
 export default () => {
   const api = Router();
 
   api.get("/getAccountHeads", getAccountHeads, (req, res, next) => {
     if (req.records.invalid_input == true) {
-      res.status(utlities.AlgaehUtilities().httpStatus().ok).json({
-        success: false,
-        result: req.records
-      });
+      res
+        .status(utlities.AlgaehUtilities().httpStatus().ok)
+        .json({
+          success: false,
+          result: req.records
+        })
+        .end();
     } else {
-      res.status(utlities.AlgaehUtilities().httpStatus().ok).json({
-        success: true,
-        result: req.records
-      });
+      res
+        .status(utlities.AlgaehUtilities().httpStatus().ok)
+        .json({
+          success: true,
+          result: req.records
+        })
+        .end();
     }
   });
   api.post("/addAccountHeads", addAccountHeads, (req, res, next) => {
     if (req.records.invalid_input == true) {
-      res.status(utlities.AlgaehUtilities().httpStatus().ok).json({
-        success: false,
-        result: req.records
-      });
+      res
+        .status(utlities.AlgaehUtilities().httpStatus().ok)
+        .json({
+          success: false,
+          result: req.records
+        })
+        .end();
     } else {
-      res.status(utlities.AlgaehUtilities().httpStatus().ok).json({
-        success: true,
-        result: req.records
-      });
+      res
+        .status(utlities.AlgaehUtilities().httpStatus().ok)
+        .json({
+          success: true,
+          result: req.records
+        })
+        .end();
     }
   });
+  api.get(
+    "/getFinanceAccountsMaping",
+    getFinanceAccountsMaping,
+    (req, res, next) => {
+      if (req.records.invalid_input == true) {
+        res
+          .status(utlities.AlgaehUtilities().httpStatus().ok)
+          .json({
+            success: false,
+            result: req.records
+          })
+          .end();
+      } else {
+        res
+          .status(utlities.AlgaehUtilities().httpStatus().ok)
+          .json({
+            success: true,
+            result: req.records
+          })
+          .end();
+      }
+    }
+  );
+  api.post(
+    "/addFinanceAccountsMaping",
+    addFinanceAccountsMaping,
+    (req, res, next) => {
+      if (req.records.invalid_input == true) {
+        res
+          .status(utlities.AlgaehUtilities().httpStatus().ok)
+          .json({
+            success: false,
+            result: req.records
+          })
+          .end();
+      } else {
+        res
+          .status(utlities.AlgaehUtilities().httpStatus().ok)
+          .json({
+            success: true,
+            result: req.records
+          })
+          .end();
+      }
+    }
+  );
 
   return api;
 };
