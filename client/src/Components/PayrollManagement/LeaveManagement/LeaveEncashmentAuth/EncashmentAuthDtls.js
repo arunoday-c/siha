cdc93bfb8eaca9d8366b45b8fe25b8ca1e82c272
Nowshad <nowshad@Nowshads-MacBook-Air.local>
2019-11-14 13:10:54 +0530

    Encashment authprozation done

--- client/src/Components/PayrollManagement/LeaveManagement/LeaveEncashmentAuth/EncashmentAuthDtls.js
+++ client/src/Components/PayrollManagement/LeaveManagement/LeaveEncashmentAuth/EncashmentAuthDtls.js
@@ -1,45 +1,40 @@
 import React, { Component } from "react";
-import { withRouter } from "react-router-dom";
-import { connect } from "react-redux";
-import { bindActionCreators } from "redux";
+
 
 import "./LeaveEncashmentAuth.scss";
 import { AuthorizeLEaveEncash } from "./LeaveEncashmentAuthEvents.js";
 
 import {
   AlgaehLabel,
-  AlgaehDataGrid,
   AlgaehModalPopUp
 } from "../../../Wrapper/algaehWrapper";
-import { AlgaehActions } from "../../../../actions/algaehActions";
 import {
   getAmountFormart,
   AlgaehOpenContainer
 } from "../../../../utils/GlobalFunctions";
 
-class EncashmentAuthDtls extends Component {
+export default class EncashmentAuthDtls extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      EncashDetailPer: [],
-      emp_name: null,
+      encashment_date: null,
+      encashment_number: null,
+      employee_code: null,
+      full_name: null,
+      leave_description: null,
+      leave_days: null,
+      total_amount: null,
       leave_encash_level: JSON.parse(
         AlgaehOpenContainer(sessionStorage.getItem("hrOptions"))
       ).leave_encash_level,
-      encash_authorized: null
+      hims_f_leave_encash_header_id: null
     };
   }
 
   componentWillReceiveProps(newProps) {
     debugger;
-    if (newProps.EncashDetailPer.length > 0) {
-      this.setState({
-        EncashDetailPer: newProps.EncashDetailPer,
-        emp_name: newProps.emp_name,
-        auth_level: newProps.auth_level,
-        encash_authorized: newProps.encash_authorized
-      });
-    }
+    this.setState({ ...newProps.EncashDetailPer });
+
   }
 
   render() {
@@ -54,22 +49,22 @@ class EncashmentAuthDtls extends Component {
         <div className="popupInner LeaveAuthPopup">
           <div className="popRightDiv">
             <div className="leave_en_auth row">
-              <div className="col-2 form-group">
+              <div className="col-3 form-group">
                 <AlgaehLabel
                   label={{
                     forceLabel: "Encashment Date"
                   }}
                 />
                 <h6>{this.state.encashment_date}</h6>
-              </div>{" "}
-              <div className="col-2 form-group">
+              </div>
+              <div className="col-3 form-group">
                 <AlgaehLabel
                   label={{
                     forceLabel: "Encashment No."
                   }}
                 />
                 <h6>{this.state.encashment_number}</h6>
-              </div>{" "}
+              </div>
               <div className="col-2 form-group">
                 <AlgaehLabel
                   label={{
@@ -77,92 +72,57 @@ class EncashmentAuthDtls extends Component {
                   }}
                 />
                 <h6>{this.state.employee_code}</h6>
-              </div>{" "}
-              <div className="col-2 form-group">
+              </div>
+              <div className="col-4 form-group">
                 <AlgaehLabel
                   label={{
                     forceLabel: "Employee Name"
                   }}
                 />
                 <h6>{this.state.full_name}</h6>
-              </div>{" "}
-              <div className="col-2 form-group">
+              </div>
+              <div className="col-3 form-group">
                 <AlgaehLabel
                   label={{
                     forceLabel: "Leave Description"
                   }}
                 />
                 <h6>{this.state.leave_description}</h6>
-              </div>{" "}
-              <div className="col-2 form-group">
+              </div>
+              <div className="col-3 form-group">
                 <AlgaehLabel
                   label={{
                     forceLabel: "Applied Days"
                   }}
                 />
-                <h6>{this.state.leave_description}</h6>
-              </div>{" "}
-              <div className="col-2 form-group">
-                <AlgaehLabel
-                  label={{
-                    forceLabel: "Balance Days"
-                  }}
-                />
-                <h6>{this.state.leave_description}</h6>
-              </div>{" "}
-              <div className="col-2 form-group">
+                <h6>{this.state.leave_days}</h6>
+              </div>
+              <div className="col-3 form-group">
                 <AlgaehLabel
                   label={{
                     forceLabel: "Encashment Amount"
                   }}
                 />
-                <h6>{this.state.leave_description}</h6>
+                <h6>{getAmountFormart(this.state.total_amount)}</h6>
               </div>
             </div>
           </div>
         </div>
         <div className="popupFooter">
           <div className="col-12">
-            {" "}
-            <button onClick={this.props.onClose} className="btn btn-primary">
+
+            <button onClick={AuthorizeLEaveEncash.bind(this, this, "APR")} className="btn btn-primary">
               Authorize
             </button>
-            <button onClick={this.props.onClose} className="btn btn-default">
+            <button onClick={AuthorizeLEaveEncash.bind(this, this, "REJ")} className="btn btn-default">
+              Reject
+            </button>
+            <button onClick={AuthorizeLEaveEncash.bind(this, this, "CAN")} className="btn btn-default">
               Cancel
-            </button>{" "}
+            </button>
           </div>
         </div>
       </AlgaehModalPopUp>
     );
   }
 }
-
-function mapStateToProps(state) {
-  return {
-    all_employees: state.all_employees,
-    leaveMaster: state.leaveMaster,
-    encashAuth: state.encashAuth,
-    organizations: state.organizations,
-    all_departments: state.all_departments
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return bindActionCreators(
-    {
-      getEmployees: AlgaehActions,
-      getLeaveMaster: AlgaehActions,
-      getOrganizations: AlgaehActions,
-      getLeaveEncashLevels: AlgaehActions,
-      getDepartments: AlgaehActions
-    },
-    dispatch
-  );
-}
-
-export default withRouter(
-  connect(
-    mapStateToProps,
-    mapDispatchToProps
-  )(EncashmentAuthDtls)
-);
