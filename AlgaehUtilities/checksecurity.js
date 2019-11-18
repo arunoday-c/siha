0361f4855ddc16c2d21c7c11c41221217f8e5647
mspriyan <mspriyang3@gmail.com>
2019-11-18 11:15:58 +0530

    changes for client in docker stack

--- AlgaehUtilities/checksecurity.js
+++ AlgaehUtilities/checksecurity.js
@@ -3,7 +3,7 @@ const crypto = require("crypto");
 const Redis = require("ioredis");
 let redis;
 if (process.env.REDIS_HOST) {
-  redis = new Redis(6379, process.env.REDIS_HOST);
+  redis = new Redis(process.env.REDIS_HOST);
 } else {
   redis = new Redis(6379, "127.0.0.1");
 }
