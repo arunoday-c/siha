7cc0fadd1785182b2ee192931116b2b335b14db0
Aboobacker Sidhiqe <>
2019-11-18 18:29:03 +0530

    finance transaction UI

--- algaeh-finance/client/src/Components/Transactions/Transactions.js
+++ algaeh-finance/client/src/Components/Transactions/Transactions.js
@@ -1,81 +1,45 @@
 import React, { useState } from "react";
-import { AlgaehLabel } from "../../Wrappers";
+// import { AlgaehLabel } from "../../Wrappers";
 import Expense from "./Expense";
 import Income from "./Income";
 import Transfer from "./Transfer";
+import { AlgaehTabs, AlgaehLabel } from "algaeh-react-components";
 
-export default function Transactions(props) {
-  const [activeTab, setActiveTab] = useState("Expense");
-
-  function openTab(e) {
-    var element = document.querySelectorAll("[algaehtabs]");
-    let i;
-    for (i = 0; i < element.length; i++) {
-      element[i].classList.remove("active");
-    }
-    e.currentTarget.classList.add("active");
-    const specified = e.currentTarget.getAttribute("algaehtabs");
-    setActiveTab(specified);
-  }
-
+export default function Transactions() {
   return (
-    <div className="">
-      <div className="col">
-        <div className="row">
-          <div className="tabMaster toggle-section">
-            <ul className="nav">
-              <li
-                algaehtabs={"Expense"}
-                className={"nav-item tab-button active"}
-                onClick={openTab}
-              >
-                {
-                  <AlgaehLabel
-                    label={{
-                      fieldName: "Expense"
-                    }}
-                  />
-                }
-              </li>
-              <li
-                algaehtabs={"Income"}
-                className={"nav-item tab-button "}
-                onClick={openTab}
-              >
-                {
-                  <AlgaehLabel
-                    label={{
-                      fieldName: "Income"
-                    }}
-                  />
-                }
-              </li>
-              <li
-                algaehtabs={"Transfer"}
-                className={"nav-item tab-button"}
-                onClick={openTab}
-              >
-                {
-                  <AlgaehLabel
-                    label={{
-                      fieldName: "Transfer"
-                    }}
-                  />
-                }
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div className="row common-section">
-          {activeTab === "Expense" ? (
-            <Expense />
-          ) : activeTab === "Income" ? (
-            <Income />
-          ) : activeTab === "Transfer" ? (
-            <Transfer />
-          ) : null}
-        </div>
-      </div>
-    </div>
+    <AlgaehTabs
+      content={[
+        {
+          title: (
+            <AlgaehLabel
+              label={{
+                forceLabel: "Income"
+              }}
+            />
+          ),
+          children: <Income />
+        },
+        {
+          title: (
+            <AlgaehLabel
+              label={{
+                forceLabel: "Expense"
+              }}
+            />
+          ),
+          children: <Expense />
+        },
+        {
+          title: (
+            <AlgaehLabel
+              label={{
+                forceLabel: "Transfer"
+              }}
+            />
+          ),
+          children: <Transfer />
+        }
+      ]}
+    />
   );
 }
