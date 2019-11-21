58e55f13255746b8f8030a11aad0f75e543b36a3
irfan.algaeh <irfan.algaeh@gmail.com>
2019-11-21 19:43:24 +0530

    changes in report ledgerReport

--- AlgaehReporting/algaeh_report_tool/templates/ledgerReport.js
+++ AlgaehReporting/algaeh_report_tool/templates/ledgerReport.js
@@ -8,7 +8,7 @@ const executePDF = function executePDFMethod(options) {
 
       const _ = options.loadash;
 
-     const  moment=options.moment;
+      const moment = options.moment;
       let input = {};
 
       const params = options.args.reportParams;
@@ -18,8 +18,7 @@ const executePDF = function executePDFMethod(options) {
         input[para["name"]] = para["value"];
       });
 
-
-      console.log("input:",input)
+      console.log("input:", input);
 
       let strQry = "";
 
@@ -37,32 +36,50 @@ const executePDF = function executePDFMethod(options) {
       }
 
       if (input.leafnode == "Y") {
-
         console.log("leaf:");
 
         options.mysql
           .executeQuery({
             query: `SELECT finance_voucher_id,payment_date ,credit_amount, debit_amount,
-            coalesce(credit_amount-debit_amount,0)as balance_amount,
+            coalesce(debit_amount-credit_amount,0)as debit_minus_credit ,
+            coalesce(credit_amount-debit_amount,0)as credit_minus_debit ,
             case payment_type when 'CR' then 'Credit' else 'Debit' end as payment_type,
-            narration,concat(H.account_name,' -> ',C.child_name) as account_details FROM finance_voucher_details VD inner join finance_account_head H on
+            narration,concat(H.account_name,' -> ',C.child_name) as account_details FROM 
+            finance_voucher_details VD inner join finance_account_head H on
              VD.head_id=H.finance_account_head_id inner join finance_account_child C on 
              VD.child_id=C.finance_account_child_id where 
-            head_id=? and child_id=? ${strQry} ${group_str};`,
+            head_id=? and child_id=? ${strQry} ;`,
             values: [input.head_id, input.child_id],
             printQuery: true
           })
           .then(result => {
-            console.log("result:",result)
-            resolve({ details: result });
+            console.log("result:", result);
+
+            let outputArray = [];
+            if (input.parent_id == 1 || input.parent_id == 5) {
+              outputArray = result.map(m => {
+                return {
+                  ...m,
+                  balance_amount: m.debit_minus_credit
+                };
+              });
+            } else {
+              outputArray = result.map(m => {
+                return {
+                  ...m,
+                  balance_amount: m.credit_minus_debit
+                };
+              });
+            }
+            console.log("outputArray:", outputArray);
+            resolve({ details: outputArray });
           })
           .catch(e => {
             options.mysql.releaseConnection();
             reject(e);
           });
       } else {
-
-        console.log("NON LEAF:")
+        console.log("NON LEAF:");
         options.mysql
           .executeQuery({
             query: ` with recursive cte  as (          
@@ -78,10 +95,6 @@ const executePDF = function executePDFMethod(options) {
             printQuery: true
           })
           .then(result => {
-            // _mysql.releaseConnection();
-            // req.records = result;
-            // next();
-            console.log("result:",result)
             const head_ids = result.map(m => m.finance_account_head_id);
             const child_ids = result
               .filter(f => {
@@ -91,24 +104,54 @@ const executePDF = function executePDFMethod(options) {
 
             options.mysql
               .executeQuery({
-                query: `   SELECT finance_voucher_id,payment_date ,
-                credit_amount,debit_amount,
-                coalesce( credit_amount-debit_amount,0)as balance_amount,
+                query: `   SELECT finance_voucher_id,payment_date ,head_id,child_id,
+                sum(credit_amount) as credit_amount,sum(debit_amount) as debit_amount,
+                coalesce( sum(credit_amount)-sum(debit_amount),0)as credit_minus_debit,               
+                coalesce(sum(debit_amount)- sum(credit_amount),0)as debit_minus_credit,               
                 case payment_type when 'CR' then 'Credit' else 'Debit' end as payment_type,
                 narration,concat(H.account_name,' -> ',C.child_name) as account_details
                  FROM finance_voucher_details VD 
                 inner join finance_account_head H on
                 VD.head_id=H.finance_account_head_id inner join finance_account_child C on 
                 VD.child_id=C.finance_account_child_id where 
-                head_id in (?) and child_id in (?) ${strQry} ${group_str}`,
+                head_id in (?) and child_id in (?) ${strQry} group by head_id,child_id with rollup`,
                 values: [head_ids, child_ids],
                 printQuery: true
               })
               .then(final_result => {
-                // _mysql.releaseConnection();
-                // req.records = result;
-                // next();
-                resolve({ details: final_result });
+                let outputArray = [];
+
+                const records = [];
+                const totals = [];
+
+                let total_amount = 0;
+                final_result.forEach(item => {
+                  if (item.head_id == null && item.child_id == null) {
+                    totals.push(item);
+                  } else if (item.head_id > 0 && item.child_id > 0) {
+                    records.push(item);
+                  }
+                });
+
+                if (input.parent_id == 1 || input.parent_id == 5) {
+                  total_amount = totals[0]["debit_minus_credit"];
+                  outputArray = records.map(m => {
+                    return {
+                      ...m,
+                      balance_amount: m.debit_minus_credit
+                    };
+                  });
+                } else {
+                  total_amount = totals[0]["credit_minus_debit"];
+                  outputArray = records.map(m => {
+                    return {
+                      ...m,
+                      balance_amount: m.credit_minus_debit
+                    };
+                  });
+                }
+                console.log("totals:", totals);
+                resolve({ details: outputArray, total_amount: total_amount });
               })
               .catch(e => {
                 _mysql.releaseConnection();
