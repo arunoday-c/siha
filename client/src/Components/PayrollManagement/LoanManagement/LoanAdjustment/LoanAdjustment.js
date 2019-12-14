075fec84eaa6a3a3f63b4de096b098e68df867e6
Nowshad <nowshad.algaeh@gmail.com>
2019-12-14 12:03:42 +0530

    loan adjustment removed employee serach and added branch and data will load as per branch

--- client/src/Components/PayrollManagement/LoanManagement/LoanAdjustment/LoanAdjustment.js
+++ client/src/Components/PayrollManagement/LoanManagement/LoanAdjustment/LoanAdjustment.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+import { connect } from "react-redux";
+import { bindActionCreators } from "redux";
+import { AlgaehActions } from "../../../../actions/algaehActions";
+
 import "./loan-adjustment.scss";
 
 import {
   AlagehFormGroup,
+  AlagehAutoComplete,
   AlgaehLabel,
   AlgaehDataGrid
 } from "../../../Wrapper/algaehWrapper";
@@ -10,16 +16,36 @@ import AlgaehSearch from "../../../Wrapper/globalSearch";
 import spotlightSearch from "../../../../Search/spotlightSearch.json";
 import { algaehApiCall, swalMessage } from "../../../../utils/algaehApiCall";
 import moment from "moment";
+import { AlgaehOpenContainer } from "../../../../utils/GlobalFunctions";
 
 class LoanAdjustment extends Component {
   constructor(props) {
     super(props);
     this.state = {
       employee_loans: [],
-      loading: false
+      loading: false,
+
+      hospital_id: JSON.parse(
+        AlgaehOpenContainer(sessionStorage.getItem("CurrencyDetail"))
+      ).hims_d_hospital_id,
     };
   }
 
+  componentDidMount() {
+    if (
+      this.props.organizations === undefined ||
+      this.props.organizations.length === 0
+    ) {
+      this.props.getOrganizations({
+        uri: "/organization/getOrganization",
+        method: "GET",
+        redux: {
+          type: "ORGS_GET_DATA",
+          mappingName: "organizations"
+        }
+      });
+    }
+  }
   employeeSearch() {
     AlgaehSearch({
       searchGrid: {
@@ -83,48 +109,49 @@ class LoanAdjustment extends Component {
   }
 
   getEmployeeLoans() {
-    if (
-      this.state.hims_d_employee_id === null ||
-      this.state.hims_d_employee_id === undefined
-    ) {
-      swalMessage({
-        title: "Please Select an Employee",
-        type: "warning"
-      });
-    } else {
-      this.setState({
-        loading: true
-      });
-      algaehApiCall({
-        uri: "/loan/getLoanApplication",
-        module: "hrManagement",
-        method: "GET",
-        data: {
-          employee_id: this.state.hims_d_employee_id,
-          loan_issued: "Y",
-          loan_closed: "N",
-          hospital_id: this.state.hospital_id
-        },
-        onSuccess: res => {
-          if (res.data.success) {
-            this.setState({
-              employee_loans: res.data.records,
-              loading: false
-            });
-          }
-        },
-        onFailure: err => {
-          swalMessage({
-            title: err.message,
-            type: "error"
-          });
 
+    this.setState({
+      loading: true
+    });
+    algaehApiCall({
+      uri: "/loan/getLoanApplication",
+      module: "hrManagement",
+      method: "GET",
+      data: {
+        employee_id: this.state.hims_d_employee_id,
+        loan_issued: "Y",
+        loan_closed: "N",
+        hospital_id: this.state.hospital_id
+      },
+      onSuccess: res => {
+        if (res.data.success) {
           this.setState({
+            employee_loans: res.data.records,
             loading: false
           });
         }
-      });
-    }
+      },
+      onFailure: err => {
+        swalMessage({
+          title: err.message,
+          type: "error"
+        });
+
+        this.setState({
+          loading: false
+        });
+      }
+    });
+
+  }
+
+  eventHandaler(e) {
+    let name = e.name || e.target.name;
+    let value = e.value || e.target.value;
+
+    this.setState({
+      [name]: value
+    });
   }
 
   render() {
@@ -134,7 +161,30 @@ class LoanAdjustment extends Component {
           className="row inner-top-search"
           style={{ paddingTop: 10, paddingBottom: 10 }}
         >
-          <div className="col-3 globalSearchCntr">
+          <AlagehAutoComplete
+            div={{ className: "col-3 mandatory" }}
+            label={{
+              forceLabel: "Select a Branch.",
+              isImp: true
+            }}
+            selector={{
+              name: "hospital_id",
+              className: "select-fld",
+              value: this.state.hospital_id,
+              dataSource: {
+                textField: "hospital_name",
+                valueField: "hims_d_hospital_id",
+                data: this.props.organizations
+              },
+              onChange: this.eventHandaler.bind(this),
+              onClear: () => {
+                this.setState({
+                  hospital_id: null
+                });
+              }
+            }}
+          />
+          {/* <div className="col-3 globalSearchCntr">
             <AlgaehLabel label={{ forceLabel: "Search Employee" }} />
             <h6 onClick={this.employeeSearch.bind(this)}>
               {this.state.employee_name
@@ -142,7 +192,7 @@ class LoanAdjustment extends Component {
                 : "Search Employee"}
               <i className="fas fa-search fa-lg"></i>
             </h6>
-          </div>
+          </div> */}
 
           <div className="col-lg-2">
             <button
@@ -153,8 +203,8 @@ class LoanAdjustment extends Component {
               {!this.state.loading ? (
                 <span>Load</span>
               ) : (
-                <i className="fas fa-spinner fa-spin" />
-              )}
+                  <i className="fas fa-spinner fa-spin" />
+                )}
             </button>
             <button
               onClick={this.clearState.bind(this)}
@@ -345,13 +395,14 @@ class LoanAdjustment extends Component {
                       keyId="hims_f_loan_application_id"
                       dataSource={{ data: this.state.employee_loans }}
                       isEditable={true}
+                      filter={true}
                       actions={{
                         allowDelete: false
                       }}
                       paging={{ page: 0, rowsPerPage: 10 }}
                       loading={this.state.loading}
                       events={{
-                        onEdit: () => {},
+                        onEdit: () => { },
                         onDone: this.adjustLoan.bind(this)
                         //onDelete: () => { }
                       }}
@@ -368,4 +419,25 @@ class LoanAdjustment extends Component {
   }
 }
 
-export default LoanAdjustment;
+
+function mapStateToProps(state) {
+  return {
+    organizations: state.organizations
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      getOrganizations: AlgaehActions
+    },
+    dispatch
+  );
+}
+
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(LoanAdjustment)
+);
