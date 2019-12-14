413b92d3383bad3a498fcfc5a11201954a16d462
irfan.algaeh <irfan.algaeh@gmail.com>
2019-12-14 14:02:23 +0530

    added getBalanceSheet api

--- algaeh-finance/src/routes/index.js
+++ algaeh-finance/src/routes/index.js
@@ -2,9 +2,11 @@ import express from "express";
 
 import finance from "../controllers/finance";
 import voucher from "../controllers/voucher";
+import financeReports from "../controllers/financeReports";
 
 const router = express();
 router.use("/finance", finance());
 router.use("/voucher", voucher());
+router.use("/financeReports", financeReports());
 
 export default router;
