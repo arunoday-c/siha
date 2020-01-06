88a37e958dc2b8e0a500cad067e5b11d40123081
irfan.algaeh <irfan.algaeh@gmail.com>
2020-01-06 16:44:12 +0530

    added ledger month wise, date wise report

--- AlgaehReporting/algaeh_report_tool/templates/MonthWiseGroupNode.js
+++ AlgaehReporting/algaeh_report_tool/templates/MonthWiseGroupNode.js
@@ -1,6 +1,3 @@
-// const { MONTHS } = require("./GlobalVariables.json");
-// const algaehUtilities = require("algaeh-utilities/utilities");
-
 const executePDF = function executePDFMethod(options) {
   return new Promise(function(resolve, reject) {
     try {
@@ -22,244 +19,81 @@ const executePDF = function executePDFMethod(options) {
         moment(input.from_date, "YYYY-MM-DD").format("YYYYMMDD") > 0 &&
         moment(input.to_date, "YYYY-MM-DD").format("YYYYMMDD") > 0
       ) {
-        strQry = ` and payment_date between date('${input.from_date}') and date('${input.to_date}') `;
+        strQry += ` and VD.payment_date between date('${input.from_date}') and date('${input.to_date}') `;
       }
 
       if (input.leafnode == "N") {
         options.mysql
           .executeQuery({
-            query: ` with recursive cte  as (          
-      select  finance_account_head_id,PC.child_id
-      from finance_account_head P left join finance_head_m_child PC on P.finance_account_head_id
-      =PC.head_id where finance_account_head_id=?
-      union                  
-      select H.finance_account_head_id,PC.child_id
-      from finance_account_head  H inner join cte
-      on H.parent_acc_id = cte.finance_account_head_id   left join finance_head_m_child PC on H.finance_account_head_id
-      =PC.head_id )select * from cte;
-      SELECT cost_center_type  FROM finance_options limit 1;`,
+            query: `SELECT cost_center_type  FROM finance_options limit 1;`,
             values: [input.head_id],
             printQuery: true
           })
           .then(result => {
-            if (result[0].length > 0) {
-              const head_ids = result[0].map(m => m.finance_account_head_id);
-              const child_ids = result[0]
-                .filter(f => {
-                  return f.child_id > 0;
-                })
-                .map(m => m.child_id);
-
+            if (result.length > 0) {
               //ST-cost center
               if (
-                result[1][0]["cost_center_type"] == "P" &&
+                result[0]["cost_center_type"] == "P" &&
                 input.cost_center_id > 0
               ) {
                 strQry += ` and project_id=${input.cost_center_id} `;
               } else if (
-                result[1][0]["cost_center_type"] == "SD" &&
+                result[0]["cost_center_type"] == "SD" &&
                 input.cost_center_id > 0
               ) {
                 strQry += ` and sub_department_id=${input.cost_center_id} `;
               }
               //END-cost center
-
               options.mysql
                 .executeQuery({
-                  query: `  SELECT finance_voucher_id,month ,VD.head_id,child_id,
-                sum(credit_amount) as credit_amount,sum(debit_amount) as debit_amount,
-                coalesce( sum(credit_amount)-sum(debit_amount),0)as credit_minus_debit,               
-                coalesce(sum(debit_amount)- sum(credit_amount),0)as debit_minus_credit,
-                H.account_name,child_id,C.child_name,monthname(concat('1999-',month,'-01')) as month_name 
-                FROM finance_voucher_details VD 
-                inner join finance_account_head H on
-                VD.head_id=H.finance_account_head_id inner join finance_account_child C on 
-                VD.child_id=C.finance_account_child_id where   VD.auth_status='A' and 
-                VD.head_id in (?) and child_id in (?) ${strQry} group by month,head_id,child_id with rollup;;`,
-                  values: [head_ids, child_ids],
+                  query: `select finance_voucher_id,VD.head_id ,VD.child_id,monthname(concat('1999-',month,'-01')) as month_name, 
+                  ROUND(sum(debit_amount),${decimal_places}) as debit_amount,ROUND(sum(credit_amount),${decimal_places})  as credit_amount,C.child_name
+                  from finance_voucher_details VD inner join finance_account_child C on
+                  VD.child_id=C.finance_account_child_id where  VD.auth_status='A' and
+                  VD.head_id in (with recursive cte  as (
+                  select  finance_account_head_id  from finance_account_head H
+                  inner join finance_account_child where finance_account_head_id =7 union
+                  select  H.finance_account_head_id from finance_account_head H
+                  inner join cte on H.parent_acc_id = cte.finance_account_head_id
+                  )select * from cte ) ${strQry}   group by month,child_id ;`,
+
+                  values: [input.head_id],
                   printQuery: true
                 })
                 .then(final_result => {
-                  options.mysql.releaseConnection();
+                  let total_debit = parseFloat(0).toFixed(decimal_places);
+                  let total_credit = parseFloat(0).toFixed(decimal_places);
                   if (final_result.length > 0) {
-                    let outputArray = [];
-                    let entriesArray = [];
-                    let grand_total = {};
-                    const records = [];
+                    //to get only balace amount
 
-                    const monthwiseTotals = [];
-
-                    const monthwiseHeadSum = [];
-
-                    //seprating data
+                    // if (input.parent_id == 1 || input.parent_id == 5) {
+                    //   //DR
+                    // }
                     final_result.forEach(item => {
-                      if (
-                        item.head_id == null &&
-                        item.child_id == null &&
-                        item.month == null
-                      ) {
-                        grand_total = item;
-                      } else if (
-                        item.month != null &&
-                        item.head_id == null &&
-                        item.child_id == null
-                      ) {
-                        monthwiseTotals.push(item);
-                      } else if (
-                        item.month != null &&
-                        item.head_id > 0 &&
-                        item.child_id == null
-                      ) {
-                        monthwiseHeadSum.push(item);
-                      } else if (
-                        item.month != null &&
-                        item.head_id > 0 &&
-                        item.child_id > 0
-                      ) {
-                        records.push(item);
-                      }
+                      total_credit = (
+                        parseFloat(total_credit) +
+                        parseFloat(item.credit_amount)
+                      ).toFixed(decimal_places);
+                      total_debit = (
+                        parseFloat(total_debit) + parseFloat(item.debit_amount)
+                      ).toFixed(decimal_places);
                     });
 
-                    //-------------------------------------------------------------------------
-                    //to get only balace amount
-                    if (input.parent_id == 1 || input.parent_id == 5) {
-                      entriesArray = records.map(m => {
-                        return {
-                          credit_amount: parseFloat(m.credit_amount).toFixed(
-                            decimal_places
-                          ),
-                          debit_amount: parseFloat(m.debit_amount).toFixed(
-                            decimal_places
-                          ),
-                          child_name: m.child_name,
-
-                          head_id: m.head_id,
-                          child_id: m.child_id,
-                          month: m.month,
-                          balance_amount: parseFloat(
-                            m.debit_minus_credit
-                          ).toFixed(decimal_places)
-                        };
-                      });
-                    } else {
-                      entriesArray = records.map(m => {
-                        return {
-                          credit_amount: parseFloat(m.credit_amount).toFixed(
-                            decimal_places
-                          ),
-                          debit_amount: parseFloat(m.debit_amount).toFixed(
-                            decimal_places
-                          ),
-                          child_name: m.child_name,
-
-                          head_id: m.head_id,
-                          child_id: m.child_id,
-                          month: m.month,
-                          balance_amount: parseFloat(
-                            m.credit_minus_debit
-                          ).toFixed(decimal_places)
-                        };
-                      });
-                    }
-
-                    const monthwiseEntries = _.chain(entriesArray)
-                      .groupBy(g => g.month)
+                    const monthWiseGroup = _.chain(final_result)
+                      .groupBy(g => g.month_name)
                       .value();
 
-                    if (input.parent_id == 1 || input.parent_id == 5) {
-                      monthwiseTotals.forEach(data => {
-                        const monthWiseheads = monthwiseHeadSum.filter(
-                          f => f.month == data.month
-                        );
-                        const monthWiseChilds = monthwiseEntries[data.month];
+                    const outputArray = [];
 
-                        const details = [];
-
-                        monthWiseheads.forEach(head => {
-                          const entries = monthWiseChilds.filter(
-                            child => head.head_id == child.head_id
-                          );
-
-                          details.push({
-                            head_account: head.account_name,
-                            total_of_head_account: head.debit_minus_credit,
-                            sub_accounts: entries
-                          });
-                        });
-
-                        outputArray.push({
-                          month_name: data.month_name,
-                          month_closing_bal: data.debit_minus_credit,
-                          head_accounts: details
-                        });
-                      });
-                    } else {
-                      monthwiseTotals.forEach(data => {
-                        const monthWiseheads = monthwiseHeadSum.filter(
-                          f => f.month == data.month
-                        );
-                        const monthWiseChilds = monthwiseEntries[data.month];
-
-                        const details = [];
-
-                        monthWiseheads.forEach(head => {
-                          const entries = monthWiseChilds.filter(
-                            child => head.head_id == child.head_id
-                          );
-
-                          details.push({
-                            head_account: head.account_name,
-                            total_of_head_account: head.credit_minus_debit,
-                            sub_accounts: entries
-                          });
-                        });
-
-                        outputArray.push({
-                          month_name: data.month_name,
-                          month_closing_bal: data.credit_minus_debit,
-                          head_accounts: details
-                        });
-                      });
-                    }
-
-                    let finalTotals = null;
-                    if (input.parent_id == 1 || input.parent_id == 5) {
-                      const symbol = " Dr";
-                      finalTotals = {
-                        final_credit_amount:
-                          parseFloat(grand_total.credit_amount).toFixed(
-                            decimal_places
-                          ) + symbol,
-                        final_debit_amount:
-                          parseFloat(grand_total.debit_amount).toFixed(
-                            decimal_places
-                          ) + symbol,
-                        final_balance:
-                          parseFloat(grand_total.debit_minus_credit).toFixed(
-                            decimal_places
-                          ) + symbol
-                      };
-                    } else {
-                      const symbol = " Cr";
-                      finalTotals = {
-                        final_credit_amount:
-                          parseFloat(grand_total.credit_amount).toFixed(
-                            decimal_places
-                          ) + symbol,
-                        final_debit_amount:
-                          parseFloat(grand_total.debit_amount).toFixed(
-                            decimal_places
-                          ) + symbol,
-                        final_balance:
-                          parseFloat(grand_total.credit_minus_debit).toFixed(
-                            decimal_places
-                          ) + symbol
-                      };
+                    for (let i in monthWiseGroup) {
+                      monthWiseGroup[i][0]["transaction_month"] = i;
+                      outputArray.push(...monthWiseGroup[i]);
                     }
 
                     resolve({
-                      months: outputArray,
-                      ...finalTotals
+                      details: outputArray,
+                      total_debit: total_debit,
+                      total_credit: total_credit
                     });
                   } else {
                     resolve({
@@ -268,6 +102,7 @@ const executePDF = function executePDFMethod(options) {
                   }
                 })
                 .catch(e => {
+                  console.log("IRFAN :", e);
                   options.mysql.releaseConnection();
                   next(e);
                 });
@@ -278,6 +113,7 @@ const executePDF = function executePDFMethod(options) {
             }
           })
           .catch(e => {
+            console.log("IRFAN :", e);
             options.mysql.releaseConnection();
             next(e);
           });
