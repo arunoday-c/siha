9c4d4d0b59afca291138a715a4af7ca30714d060
mspriyan <mspriyang3@gmail.com>
2020-01-08 16:44:09 +0530

    added modal to edit cost center

--- algaeh-finance/client/src/Components/CostCenterPage/CenterComponent.js
+++ algaeh-finance/client/src/Components/CostCenterPage/CenterComponent.js
@@ -1,65 +1,87 @@
-import React, { memo } from "react";
+import React, { memo, useState } from "react";
 import "./costcenter.scss";
-import { AlgaehDataGrid } from "algaeh-react-components";
+import {
+  AlgaehDataGrid,
+  AlgaehModal,
+  AlgaehButton
+} from "algaeh-react-components";
+import CostCenterComponent from "../costCenterComponent";
 
 export default memo(function CenterComponent({ data }) {
+  const [visible, setVisible] = useState(false);
+  const [current, setCurrent] = useState(null);
+
+  function openEdit(obj) {
+    setVisible(true);
+    setCurrent(obj);
+  }
+
+  function closeEdit() {
+    setVisible(false);
+    setCurrent(null);
+  }
+
+  function EditButton(...args) {
+    return (
+      <button className="btn btn-primary" onClick={() => openEdit(args[1])}>
+        Edit
+      </button>
+    );
+  }
+
   return (
     <div>
+      {visible && current ? (
+        <AlgaehModal
+          title="Edit Cost Center"
+          visible={visible}
+          mask={true}
+          maskClosable={false}
+          onCancel={closeEdit}
+          footer={null}
+        >
+          <CostCenterComponent
+            noborder={false}
+            orgUrl="/organization/getOrganization"
+            propBranchID={current.cost_center_id}
+            propCenterID={current.hospital_id}
+            render={values => (
+              <AlgaehButton type="primary" onClick={() => closeEdit(values)}>
+                Change
+              </AlgaehButton>
+            )}
+          />
+        </AlgaehModal>
+      ) : null}
       <div className="row">
         <div className="col-12">
           <div className="portlet portlet-bordered margin-bottom-15">
             <div className="portlet-title">
               <div className="caption">
                 <h3 className="caption-subject">List of Cost Center</h3>
-              </div>{" "}
+              </div>
               <div className="actions"></div>
             </div>
             <div className="portlet-body">
               <AlgaehDataGrid
                 columns={[
                   {
-                    key: "cost_center_id",
-                    title: "Center ID",
-                    sortable: true,
-                    filtered: false
+                    title: "Action",
+                    key: "key",
+                    align: "center",
+                    displayTemplate: EditButton
                   },
                   {
                     key: "hospital_name",
                     title: "Branch",
                     filtered: true,
-                    align: "left",
-                    editorTemplate: (text, records) => {
-                      return (
-                        <input
-                          type="text"
-                          value={text}
-                          onChange={e => {
-                            console.log("text", text);
-                            console.log("records", records);
-                            records["title"] = "Hello";
-                          }}
-                        />
-                      );
-                    }
+                    align: "center"
                   },
                   {
                     key: "cost_center",
                     title: "Cost Center",
                     filtered: true,
-                    align: "left",
-                    editorTemplate: (text, records) => {
-                      return (
-                        <input
-                          type="number"
-                          value={text}
-                          onChange={e => {
-                            console.log("text", text);
-                            console.log("records", records);
-                            records["title"] = "Hello";
-                          }}
-                        />
-                      );
-                    }
+                    align: "center"
                   }
                 ]}
                 loading={false}
@@ -68,9 +90,8 @@ export default memo(function CenterComponent({ data }) {
                 dataSource={{
                   data
                 }}
-                rowUnique="id"
+                rowUnique="cost_center_id"
                 xaxis={1500}
-                showCheckBox={{}}
               />
             </div>
           </div>
