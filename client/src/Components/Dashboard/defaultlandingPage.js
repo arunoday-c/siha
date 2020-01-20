d956e6627f0f0b2d371ffe8d79fc1bdad8d18179
Aboobacker Sidhiqe <>
2020-01-20 12:35:09 +0530

    Default Landing Page

---
+++ client/src/Components/Dashboard/defaultlandingPage.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "defaultlandingPage.scss";
+export default class defaultlandingPage extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Welcome, User Full Name</h1>
+        <p>More Details How to use application will define here.</p>
+      </div>
+    );
+  }
+}
