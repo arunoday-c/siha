76b5f98b6f859ad409e5d13bedcbbb26743b4948
Nowshad <nowshad.algaeh@gmail.com>
2020-01-20 13:21:20 +0530

    Clinical desk module plan issue done

--- client/src/Components/PatientProfile/PatientProfile.js
+++ client/src/Components/PatientProfile/PatientProfile.js
@@ -649,7 +649,7 @@ class PatientProfile extends Component {
   }
   render() {
     const module_plan = _.find(this.active_modules, f => {
-      return f.module_id === this.selected_module;
+      return parseInt(f.module_id) === parseInt(this.selected_module);
     });
 
     const _pat_profile =
