d50b10bbdc2119362c17430d531a99ed7ab71638
Aboobacker Sidhiqe <>
2020-01-31 16:47:34 +0530

    Global Function Temporary changes in AlgaehOpenContainer - Noor

--- algaeh-finance/client/src/utils/GlobalFunctions.js
+++ algaeh-finance/client/src/utils/GlobalFunctions.js
@@ -311,7 +311,7 @@ const loadJSON = (file, callback) => {
   var xobj = new XMLHttpRequest();
   xobj.overrideMimeType("application/json");
   xobj.open("GET", file, true);
-  xobj.onreadystatechange = function () {
+  xobj.onreadystatechange = function() {
     if (xobj.readyState === 4 && xobj.status === 200) {
       callback(JSON.parse(xobj.responseText));
     }
@@ -422,11 +422,11 @@ export function AlgaehValidation(options) {
           _title =
             _langua === "en"
               ? _lable
-                .replace("*", "")
-                .toLowerCase()
-                .replace(/^\w/, c => {
-                  return c.toUpperCase();
-                }) + "- Cannot be empty"
+                  .replace("*", "")
+                  .toLowerCase()
+                  .replace(/^\w/, c => {
+                    return c.toUpperCase();
+                  }) + "- Cannot be empty"
               : _lable.replace("*", "") + "- لا يمكن أن يكون فارغا";
         }
         swalMessage({
@@ -494,8 +494,8 @@ export function numberFormater(value, options, CurrencyDetail) {
   let s = prec
     ? toFixedFix(n, prec)
     : Math.round(n)
-      .toString()
-      .split(".");
+        .toString()
+        .split(".");
   if (s instanceof Array) {
     if (s[0].length > 3) {
       s[0] = s[0].replace(
@@ -604,17 +604,18 @@ export function AlgaehCloseContainer(string) {
   return crypted;
 }
 export function AlgaehOpenContainer(string) {
-  var decipher = crypto.createDecipher(algorithm, containerId);
-  var dec = decipher.update(string, "hex", "utf8");
-  dec += decipher.final("utf8");
-  return dec;
+  // var decipher = crypto.createDecipher(algorithm, containerId);
+  // var dec = decipher.update(string, "hex", "utf8");
+  // dec += decipher.final("utf8");
+  // return dec;
+  return {};
 }
 export function checkSecurity(options) {
   let currentSecurity =
     sessionStorage.getItem("AlgaehScreener") !== null
       ? JSON.parse(
-        AlgaehOpenContainer(sessionStorage.getItem("AlgaehScreener"))
-      )
+          AlgaehOpenContainer(sessionStorage.getItem("AlgaehScreener"))
+        )
       : undefined;
   if (currentSecurity !== undefined) {
     if (options.securityType === "componet") {
