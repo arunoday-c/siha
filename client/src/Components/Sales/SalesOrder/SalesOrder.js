7fc21317b3f5e8dc33a8dfca1d5b7cec5906ad54
Aboobacker Sidhiqe <>
2020-02-04 10:36:17 +0530

    Sales order after create will show now (pending)

--- client/src/Components/Sales/SalesOrder/SalesOrder.js
+++ client/src/Components/Sales/SalesOrder/SalesOrder.js
@@ -222,8 +222,8 @@ class SalesOrder extends Component {
                 <h6>
                   {this.state.sales_order_date
                     ? moment(this.state.sales_order_date).format(
-                      Options.dateFormat
-                    )
+                        Options.dateFormat
+                      )
                     : Options.dateFormat}
                 </h6>
               </div>
@@ -241,29 +241,29 @@ class SalesOrder extends Component {
                     ) : this.state.authorize1 === "Y" &&
                       this.state.authorize2 === "Y" &&
                       this.state.is_completed === "N" ? (
-                          <span className="badge badge-success">
-                            Authorized / Dispatch Pending
+                      <span className="badge badge-success">
+                        Authorized / Dispatch Pending
                       </span>
-                        ) : this.state.authorize1 === "Y" &&
-                          this.state.authorize2 === "N" ? (
-                            <span className="badge badge-danger">
-                              Authorized 2 Pending
+                    ) : this.state.authorize1 === "Y" &&
+                      this.state.authorize2 === "N" ? (
+                      <span className="badge badge-danger">
+                        Authorized 2 Pending
                       </span>
-                          ) : this.state.authorize1 === "N" &&
-                            this.state.authorize2 === "N" ? (
-                              <span className="badge badge-danger">Pending</span>
-                            ) : this.state.is_completed === "Y" &&
-                              this.state.invoice_generated === "N" ? (
-                                <span className="badge badge-danger">
-                                  Invoice Generation Pending
+                    ) : this.state.authorize1 === "N" &&
+                      this.state.authorize2 === "N" ? (
+                      <span className="badge badge-danger">Pending</span>
+                    ) : this.state.is_completed === "Y" &&
+                      this.state.invoice_generated === "N" ? (
+                      <span className="badge badge-danger">
+                        Invoice Generation Pending
                       </span>
-                              ) : this.state.invoice_generated === "Y" ? (
-                                <span className="badge badge-success">
-                                  Invoice Generated
+                    ) : this.state.invoice_generated === "Y" ? (
+                      <span className="badge badge-success">
+                        Invoice Generated
                       </span>
-                              ) : (
-                                  "-------"
-                                )}
+                    ) : (
+                      <span className="badge badge-danger">Pending</span>
+                    )}
                   </h6>
                 </div>
               ) : null}
@@ -272,17 +272,17 @@ class SalesOrder extends Component {
           printArea={
             this.state.sales_order_number !== null
               ? {
-                menuitems: [
-                  {
-                    label: "Sales Order Report",
-                    events: {
-                      onClick: () => {
-                        generateSalesOrderReport(this.state);
+                  menuitems: [
+                    {
+                      label: "Sales Order Report",
+                      events: {
+                        onClick: () => {
+                          generateSalesOrderReport(this.state);
+                        }
                       }
                     }
-                  }
-                ]
-              }
+                  ]
+                }
               : ""
           }
           selectedLang={this.state.selectedLang}
@@ -409,25 +409,25 @@ class SalesOrder extends Component {
                     </h6>
                   </div>
                 ) : (
-                    <AlagehFormGroup
-                      div={{ className: "col" }}
-                      label={{
-                        forceLabel: "Name of Sales Person",
-                        isImp: false
-                      }}
-                      textBox={{
-                        className: "txt-fld",
-                        name: "sales_man",
-                        value: this.state.sales_man,
-                        events: {
-                          onChange: texthandle.bind(this, this)
-                        },
-                        others: {
-                          disabled: this.state.dataExists
-                        }
-                      }}
-                    />
-                  )}
+                  <AlagehFormGroup
+                    div={{ className: "col" }}
+                    label={{
+                      forceLabel: "Name of Sales Person",
+                      isImp: false
+                    }}
+                    textBox={{
+                      className: "txt-fld",
+                      name: "sales_man",
+                      value: this.state.sales_man,
+                      events: {
+                        onChange: texthandle.bind(this, this)
+                      },
+                      others: {
+                        disabled: this.state.dataExists
+                      }
+                    }}
+                  />
+                )}
               </div>
               <div className="row">
                 <AlgaehDateHandler
@@ -553,11 +553,11 @@ class SalesOrder extends Component {
               {this.state.sales_order_mode === "S" ? (
                 <SalesOrdListService SALESIOputs={this.state} />
               ) : (
-                  <SalesOrdListItems
-                    SALESIOputs={this.state}
-                    sales_order_number={this.props.sales_order_number}
-                  />
-                )}
+                <SalesOrdListItems
+                  SALESIOputs={this.state}
+                  sales_order_number={this.props.sales_order_number}
+                />
+              )}
             </MyContext.Provider>
           </div>
         </div>
@@ -672,38 +672,39 @@ class SalesOrder extends Component {
                     </button>
                   ) : null}
 
-                  {this.state.cancelled === "N" ? <button
-                    type="button"
-                    className="btn btn-other"
-                    disabled={
-                      this.state.authBtnEnable === true
-                        ? true
-                        : this.state.authorize1 === "Y" &&
-                          this.state.authorize2 === "Y"
+                  {this.state.cancelled === "N" ? (
+                    <button
+                      type="button"
+                      className="btn btn-other"
+                      disabled={
+                        this.state.authBtnEnable === true
+                          ? true
+                          : this.state.authorize1 === "Y" &&
+                            this.state.authorize2 === "Y"
                           ? true
                           : false
-                    }
-                    onClick={AuthorizeOrderEntry.bind(
-                      this,
-                      this,
-                      this.state.authorize1 === "N"
-                        ? "authorize1"
-                        : "authorize2"
-                    )}
-                  >
-                    <AlgaehLabel
-                      label={{
-                        forceLabel:
-                          this.state.authorize1 === "N"
-                            ? "Authorize 1"
-                            : this.state.sales_order_auth_level === "2"
+                      }
+                      onClick={AuthorizeOrderEntry.bind(
+                        this,
+                        this,
+                        this.state.authorize1 === "N"
+                          ? "authorize1"
+                          : "authorize2"
+                      )}
+                    >
+                      <AlgaehLabel
+                        label={{
+                          forceLabel:
+                            this.state.authorize1 === "N"
+                              ? "Authorize 1"
+                              : this.state.sales_order_auth_level === "2"
                               ? "Authorize 2"
                               : "Authorize 1",
-                        returnText: true
-                      }}
-                    />
-                  </button> : null}
-
+                          returnText: true
+                        }}
+                      />
+                    </button>
+                  ) : null}
                 </div>
               ) : null}
             </div>
