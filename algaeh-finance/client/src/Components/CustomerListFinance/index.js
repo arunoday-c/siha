055fb9b8ba0375a8888e103654f957151d31800f
Aboobacker Sidhiqe <>
2020-02-17 09:50:48 +0530

    customerList infobar color

--- algaeh-finance/client/src/Components/CustomerListFinance/index.js
+++ algaeh-finance/client/src/Components/CustomerListFinance/index.js
@@ -247,7 +247,7 @@ export default memo(function(props) {
     <div className="row">
       <div className="col-12">
         <div className="row infoBar">
-          <div className="col">
+          <div className="col danger">
             <div className="text">
               <p>Overdue</p>
               0.00
