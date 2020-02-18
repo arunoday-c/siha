9e532276f0b91d14f87435f203abf89111fdcc0b
irfan.algaeh <irfan.algaeh@gmail.com>
2020-02-18 18:24:26 +0530

    added api getCustomerInvoiceDetails

--- algaeh-finance/src/controllers/finance_customer.js
+++ algaeh-finance/src/controllers/finance_customer.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 import utlities from "algaeh-utilities";
 import finance_customer from "../models/finance_customer";
 
-const { getCustomerReceivables } = finance_customer;
+const { getCustomerReceivables, getCustomerInvoiceDetails } = finance_customer;
 
 export default () => {
   const api = Router();
@@ -10,6 +10,29 @@ export default () => {
   api.get(
     "/getCustomerReceivables",
     getCustomerReceivables,
+    (req, res, next) => {
+      if (req.records.invalid_input == true) {
+        res
+          .status(utlities.AlgaehUtilities().httpStatus().internalServer)
+          .json({
+            success: false,
+            message: req.records.message
+          })
+          .end();
+      } else {
+        res
+          .status(utlities.AlgaehUtilities().httpStatus().ok)
+          .json({
+            success: true,
+            result: req.records
+          })
+          .end();
+      }
+    }
+  );
+  api.get(
+    "/getCustomerInvoiceDetails",
+    getCustomerInvoiceDetails,
     (req, res, next) => {
       if (req.records.invalid_input == true) {
         res
--- algaeh-finance/src/models/finance_customer.js
+++ algaeh-finance/src/models/finance_customer.js
@@ -6,7 +6,7 @@ import algaehUtilities from "algaeh-utilities/utilities";
 export default {
   //created by irfan:
   getCustomerReceivables: (req, res, next) => {
-    const utilities = new algaehUtilities();
+    // const utilities = new algaehUtilities();
     const _mysql = new algaehMysql();
     const decimal_places = req.userIdentity.decimal_places;
     _mysql
@@ -16,6 +16,34 @@ export default {
         as balance_amount from finance_account_child C left join finance_voucher_details VD
         on C.finance_account_child_id=VD.child_id and VD.auth_status='A'  where  finance_account_child_id in (
         select child_id from hims_d_customer) group by C.finance_account_child_id; `,
+        printQuery: true
+      })
+      .then(result => {
+        _mysql.releaseConnection();
+
+        req.records = result;
+        next();
+      })
+      .catch(e => {
+        _mysql.releaseConnection();
+        next(e);
+      });
+  },
+  //created by irfan:
+  getCustomerInvoiceDetails: (req, res, next) => {
+    // const utilities = new algaehUtilities();
+    const _mysql = new algaehMysql();
+    // const decimal_places = req.userIdentity.decimal_places;
+    _mysql
+      .executeQuery({
+        query: `select finance_voucher_header_id ,amount as invoice_amount,
+        settled_amount,(amount-settled_amount) as balace_amount,
+        invoice_no ,voucher_no,voucher_type,narration
+        from finance_voucher_header H inner join finance_voucher_details D on 
+        H.finance_voucher_header_id=D.voucher_header_id 
+        and voucher_type='payment' and invoice_no is not null   
+        where  D.child_id=?; `,
+        values: [req.query.child_id],
         printQuery: true
       })
       .then(result => {
