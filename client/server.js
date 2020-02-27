81fca32321b099ebb52a7b6cb7c31b795506b31d
mspriyan <mspriyang3@gmail.com>
2020-02-27 11:24:24 +0530

    added serve to client for single page routing

--- client/server.js
+++
@@ -1,5 +0,0 @@
-const express = require("express");
-const path = require("path");
-const app = express();
-app.use(express.static(path.join(__dirname, "build")));
-app.listen(1313, console.log("Client started on port 1313"));
