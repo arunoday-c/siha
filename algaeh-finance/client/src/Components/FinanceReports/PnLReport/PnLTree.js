aa8ca9781687167502483e31aeff252c2bfbf287
mspriyan <mspriyang3@gmail.com>
2020-03-13 20:01:17 +0530

    merged profit and loss into one report

---
+++ algaeh-finance/client/src/Components/FinanceReports/PnLReport/PnLTree.js
@@ -0,0 +1,75 @@
+import React, { useRef } from "react";
+import { Row, Col } from "antd";
+import ReactToPrint from "react-to-print";
+import { PlotUI } from "../FinanceStandardReports/plotui";
+
+export default function ProfitTree({ style, layout, data }) {
+  const createPrintObject = useRef(undefined);
+  if (data) {
+    return (
+      <>
+        <ReactToPrint
+          trigger={() => <i className="fas fa-print" />}
+          content={() => createPrintObject.current}
+          removeAfterPrint={true}
+          bodyClass="reportPreviewSecLeft"
+          pageStyle="@media print {
+            html, body {
+              height: initial !important;
+              overflow: initial !important;
+              -webkit-print-color-adjust: exact;
+            }
+          }
+          
+          @page {
+            size: auto;
+            margin: 20mm;
+          }"
+        />
+        <div ref={createPrintObject}>
+          <div className="financeReportHeader">
+            <div>Twareat Medical Centre</div>
+            <div>
+              Al Fanar Mall، 1 Street, Ar Rawabi, Al Khobar 34421, Saudi Arabia
+            </div>
+            <hr></hr>
+            <h3>Profit and Loss</h3>
+            <p>
+              As on: <b>12/02/2020</b>
+            </p>
+          </div>
+          <div className="reportBodyArea">
+            <Row gutter={[8, 8]}>
+              <Col span={layout.col}>
+                <div className="reportTableStyle">
+                  <ul className="treeListUL">
+                    {PlotUI(data["income"], style, [0], layout.expand)}
+                  </ul>
+                </div>
+              </Col>{" "}
+              <Col span={layout.col}>
+                <div className="reportTableStyle">
+                  <ul className="treeListUL">
+                    {PlotUI(data["expense"], style, [0], layout.expand)}
+                  </ul>
+                </div>
+              </Col>
+            </Row>
+          </div>
+          <div className="reportTotalArea">
+            <table style={{ width: "100%" }}>
+              <tbody>
+                <tr className="footerTotalArea">
+                  <td style={{ width: "100%" }} valign="top">
+                    <b> {<div>Profit : {data.profit}</div>}</b>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </>
+    );
+  }
+  return null;
+}
---
+++ algaeh-finance/client/src/Components/FinanceReports/PnLReport/pandLCostCenter.js
@@ -0,0 +1,56 @@
+import React, { memo, useEffect, useState } from "react";
+import { AlgaehTable, AlgaehMessagePop } from "algaeh-react-components";
+import { newAlgaehApi } from "../../../hooks";
+
+function PnLCostCenter({ data }) {
+  const [incomeExpenceData, setincomeExpence] = useState([]);
+  const [totals, setTotals] = useState({});
+  const [columns, setColumn] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const { cost_centers, expense, income, totals } = data;
+
+    let createColumns = cost_centers.map((column, index) => {
+      const { cost_center_id, cost_center } = column;
+      return {
+        fieldName: String(cost_center_id),
+        label: cost_center
+      };
+    });
+    createColumns.unshift({
+      fieldName: "label",
+      label: "Ledger Name",
+      freezable: true
+      // filterable: true
+    });
+    setColumn(createColumns);
+    setincomeExpence([income, expense]);
+    setTotals(totals);
+  }, [data]);
+
+  return (
+    <>
+      <div className="financeReportHeader">
+        <div>Twareat Medical Centre</div>
+        <div>
+          Al Fanar Mall, 1 Street, Ar Rawabi, Al Khobar 34421, Saudi Arabia
+        </div>
+        <hr></hr>
+        <h3>Profit & Loss - Cost Centre</h3>
+      </div>
+      <AlgaehTable
+        className="treeGridPL"
+        height="75vh"
+        columns={columns}
+        data={incomeExpenceData}
+        footer={true}
+        aggregate={field => {
+          return totals[field];
+        }}
+      />
+    </>
+  );
+}
+
+export default memo(PnLCostCenter);
