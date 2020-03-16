89729a8f79cd3f9a5a98154ea132dfe234b00f95
msp.algaeh <msp.algaeh@gmail.com>
2020-03-16 14:35:00 +0530

    changes in yearly leave process

--- HrManagement/src/models/leave.js
+++ HrManagement/src/models/leave.js
@@ -8115,6 +8115,8 @@ function yearlyLeaveProcess(inputs, req, mysql) {
                       (parseFloat(m.eligible_days) / parseFloat(365)) *
                         parseFloat(AllEmployees[i]["no_days_til_eoy"])
                     );
+                  } else {
+                    m["eligible_days"] = m.eligible_days;
                   }
                   return m;
                 });
