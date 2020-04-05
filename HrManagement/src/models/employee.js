9be82779b4f4c4e96be992b388bba1b435367947
Nowshad <nowshad.algaeh@gmail.com>
2020-04-05 16:03:51 +0300

    Employee Master GET API Changes done

--- HrManagement/src/models/employee.js
+++ HrManagement/src/models/employee.js
@@ -35,7 +35,7 @@ export default {
           ],
           query:
             "insert into  hims_f_miscellaneous_earning_deduction (??) values ? ON DUPLICATE KEY UPDATE ?",
-          printQuery: query => {},
+          printQuery: query => { },
           bulkInsertOrUpdate: true
         })
         .then(result => {
@@ -108,9 +108,9 @@ export default {
             query:
               "SELECT E.*, hims_d_employee_id as employee_id, SD.sub_department_name, D.department_name,N.nationality as nationality_name,\
                 R.religion_name, DE.designation,employee_group_id  FROM hims_d_employee E \
-                inner join hims_d_sub_department SD on E.sub_department_id = SD.hims_d_sub_department_id \
-                inner join hims_d_department D on SD.department_id = D.hims_d_department_id \
-                inner join hims_d_religion R on E.religion_id = R.hims_d_religion_id \
+                left join hims_d_sub_department SD on E.sub_department_id = SD.hims_d_sub_department_id \
+                left join hims_d_department D on SD.department_id = D.hims_d_department_id \
+                left join hims_d_religion R on E.religion_id = R.hims_d_religion_id \
                 left join hims_d_designation DE on E.employee_designation_id = DE.hims_d_designation_id left join hims_d_nationality N on N.hims_d_nationality_id = E.nationality WHERE \
                 E.record_status = 'A'  " +
               specificEmployee +
