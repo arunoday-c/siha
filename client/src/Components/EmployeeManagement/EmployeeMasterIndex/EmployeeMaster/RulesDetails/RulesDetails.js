d8c18361e52d1cb01d4c6c103ef9ac3b5829746e
Aboobacker Sidhiqe <>
2020-05-29 06:38:38 +0530

    Added KPI's for employee master

--- client/src/Components/EmployeeManagement/EmployeeMasterIndex/EmployeeMaster/RulesDetails/RulesDetails.js
+++ client/src/Components/EmployeeManagement/EmployeeMasterIndex/EmployeeMaster/RulesDetails/RulesDetails.js
@@ -1,6 +1,11 @@
 import React, { PureComponent } from "react";
 import "./RulesDetails.scss";
-import { AlagehFormGroup, AlgaehLabel } from "../../../../Wrapper/algaehWrapper";
+import {
+  AlagehFormGroup,
+  AlgaehLabel,
+  AlgaehDataGrid,
+  AlagehAutoComplete,
+} from "../../../../Wrapper/algaehWrapper";
 import { changeChecks } from "./RulesDetailsEvent.js";
 
 class RulesDetails extends PureComponent {
@@ -14,7 +19,7 @@ class RulesDetails extends PureComponent {
       ExcludeMachineData: false,
       GratuityApplicable: false,
       SuspendSalary: false,
-      PfApplicable: false
+      PfApplicable: false,
     };
   }
 
@@ -43,141 +48,120 @@ class RulesDetails extends PureComponent {
       <React.Fragment>
         <div className="hptl-phase1-add-employee-form popRightDiv">
           <div className="row">
-            <div className="col-12">
+            <div className="col-5">
               <h5>
                 <span>Applicable Rules</span>
               </h5>
-              <div className="row paddin-bottom-5">
-                <div
-                  className="col-3 customCheckbox form-group"
-                  style={{ border: "none" }}
-                >
-                  <label className="checkbox inline">
-                    <input
-                      type="checkbox"
-                      name="leave_salary_process"
-                      value="Y"
-                      checked={this.state.LeaveSalaryProcess}
-                      onChange={changeChecks.bind(this, this)}
-                    />
-                    <span>
-                      <AlgaehLabel
-                        label={{
-                          forceLabel: "Eligible for Annual Leave Salary"
-                        }}
+              <div className="row">
+                <div className="col-6 form-group">
+                  <label>Eligible for Annual Leave Salary</label>
+                  <div className="customCheckbox">
+                    <label className="checkbox inline">
+                      <input
+                        type="checkbox"
+                        name="leave_salary_process"
+                        value="Y"
+                        checked={this.state.LeaveSalaryProcess}
+                        onChange={changeChecks.bind(this, this)}
                       />
-                    </span>
-                  </label>
+                      <span>Yes</span>
+                    </label>
+                  </div>
                 </div>
-                {/* 
-                <div
-                  className="col-3 customCheckbox form-group"
-                  style={{ border: "none" }}
-                >
-                  <label className="checkbox inline">
-                    <input
-                      type="checkbox"
-                      name="late_coming_rule"
-                      checked={this.state.LateComingRule}
-                      onChange={changeChecks.bind(this, this)}
-                    />
-                    <span>
-                      <AlgaehLabel label={{ forceLabel: "Late Coming Rule" }} />
-                    </span>
-                  </label>
+
+                <div className="col-6 form-group">
+                  <label>Airfare Applicable</label>
+                  <div className="customCheckbox">
+                    <label className="checkbox inline">
+                      <input
+                        type="checkbox"
+                        name="airfare_process"
+                        checked={this.state.AirfareProcess}
+                        onChange={changeChecks.bind(this, this)}
+                      />
+                      <span>Yes</span>
+                    </label>
+                  </div>
+                </div>
+
+                <div className="col-6 form-group">
+                  <label>PF Applicable</label>
+                  <div className="customCheckbox">
+                    <label className="checkbox inline">
+                      <input
+                        type="checkbox"
+                        name="pf_applicable"
+                        onChange={changeChecks.bind(this, this)}
+                        checked={this.state.PfApplicable}
+                      />
+                      <span>Yes</span>
+                    </label>
+                  </div>
+                </div>
+
+                <div className="col-6 form-group">
+                  <label>Gratuity Applicable</label>
+                  <div className="customCheckbox">
+                    <label className="checkbox inline">
+                      <input
+                        type="checkbox"
+                        name="gratuity_applicable"
+                        checked={this.state.GratuityApplicable}
+                        onChange={changeChecks.bind(this, this)}
+                      />
+                      <span>Yes</span>
+                    </label>
+                  </div>
+                </div>
+
+                <div className="col-6 form-group">
+                  <label>Suspend from Salary</label>
+                  <div className="customCheckbox">
+                    <label className="checkbox inline">
+                      <input
+                        type="checkbox"
+                        name="suspend_salary"
+                        checked={this.state.SuspendSalary}
+                        onChange={changeChecks.bind(this, this)}
+                      />
+                      <span>Yes</span>
+                    </label>
+                  </div>
+                </div>
+
+                {/* <div className="col-6 form-group">
+                  <label>Late Coming Rule Applicable</label>
+                  <div className="customCheckbox">
+                    <label className="checkbox inline">
+                      <input
+                        type="checkbox"
+                        name="late_coming_rule"
+                        checked={this.state.LateComingRule}
+                        onChange={changeChecks.bind(this, this)}
+                      />
+                      <span>Yes</span>
+                    </label>
+                  </div>
+                </div>
+                <div className="col-6 form-group">
+                  <label>Exclude Machine Data</label>
+                  <div className="customCheckbox">
+                    <label className="checkbox inline">
+                      <input
+                        type="checkbox"
+                        name="exclude_machine_data"
+                        checked={this.state.ExcludeMachineData}
+                        onChange={changeChecks.bind(this, this)}
+                      />
+                      <span>Yes</span>
+                    </label>
+                  </div>
                 </div> */}
-
-                <div
-                  className="col-3 customCheckbox form-group"
-                  style={{ border: "none" }}
-                >
-                  <label className="checkbox inline">
-                    <input
-                      type="checkbox"
-                      name="airfare_process"
-                      checked={this.state.AirfareProcess}
-                      onChange={changeChecks.bind(this, this)}
-                    />
-                    <span>
-                      <AlgaehLabel label={{ forceLabel: "Airfare Process" }} />
-                    </span>
-                  </label>
-                </div>
-
-                {/* <div
-                  className="col-3 customCheckbox form-group"
-                  style={{ border: "none" }}
-                >
-                  <label className="checkbox inline">
-                    <input
-                      type="checkbox"
-                      name="pf_applicable"
-                      onChange={changeChecks.bind(this, this)}
-                      checked={this.state.PfApplicable}
-                    />
-                    <span>
-                      <AlgaehLabel label={{ forceLabel: "PF Applicable" }} />
-                    </span>
-                  </label>
-                </div> */}
-
-                <div
-                  className="col-3 customCheckbox form-group"
-                  style={{ border: "none" }}
-                >
-                  <label className="checkbox inline">
-                    <input
-                      type="checkbox"
-                      name="exclude_machine_data"
-                      checked={this.state.ExcludeMachineData}
-                      onChange={changeChecks.bind(this, this)}
-                    />
-                    <span>
-                      <AlgaehLabel
-                        label={{ forceLabel: "Exclude Machine Data" }}
-                      />
-                    </span>
-                  </label>
-                </div>
-                <div
-                  className="col-3 customCheckbox form-group"
-                  style={{ border: "none" }}
-                >
-                  <label className="checkbox inline">
-                    <input
-                      type="checkbox"
-                      name="gratuity_applicable"
-                      checked={this.state.GratuityApplicable}
-                      onChange={changeChecks.bind(this, this)}
-                    />
-                    <span>
-                      <AlgaehLabel
-                        label={{ forceLabel: "Gratuity Applicable" }}
-                      />
-                    </span>
-                  </label>
-                </div>
-                <div
-                  className="col-3 customCheckbox form-group"
-                  style={{ border: "none" }}
-                >
-                  <label className="checkbox inline">
-                    <input
-                      type="checkbox"
-                      name="suspend_salary"
-                      checked={this.state.SuspendSalary}
-                      onChange={changeChecks.bind(this, this)}
-                    />
-                    <span>
-                      <AlgaehLabel label={{ forceLabel: "Suspend Salary" }} />
-                    </span>
-                  </label>
-                </div>
 
                 <AlagehFormGroup
-                  div={{ className: "col-3" }}
+                  div={{ className: "col-6" }}
                   label={{
-                    forceLabel: "Gratuity Encash Amount",
+                    forceLabel: "Total Gratuity Encashed",
                   }}
                   textBox={{
                     className: "txt-fld",
@@ -185,11 +169,11 @@ class RulesDetails extends PureComponent {
                     value: this.state.gratuity_encash,
                     decimal: {
                       allowNegative: false,
-                      thousandSeparator: ","
+                      thousandSeparator: ",",
                     },
                     events: {
-                      onChange: changeChecks.bind(this, this)
-                    }
+                      onChange: changeChecks.bind(this, this),
+                    },
                   }}
                 />
                 {/* <AlagehFormGroup
@@ -208,6 +192,90 @@ class RulesDetails extends PureComponent {
                     }
                   }}
                 /> */}
+              </div>
+            </div>
+            <div className="col-7">
+              <div className="row">
+                <div className="col-12">
+                  {" "}
+                  <h5>
+                    <span>Employee KPI's</span>
+                  </h5>
+                </div>
+
+                <div className="col-12">
+                  <div className="row">
+                    <AlagehAutoComplete
+                      div={{ className: "col-2 form-group mandatory" }}
+                      label={{ forceLabel: "Select Year", isImp: true }}
+                      selector={{
+                        name: "",
+                        value: "",
+                        className: "select-fld",
+                        dataSource: {
+                          textField: "",
+                          valueField: "",
+                          // data: "",
+                        },
+                        //onChange: this.dropDownHandler.bind(this),
+                      }}
+                    />
+                    <AlagehAutoComplete
+                      div={{ className: "col form-group mandatory" }}
+                      label={{ forceLabel: "Select KPI's", isImp: true }}
+                      selector={{
+                        name: "",
+                        value: "",
+                        className: "select-fld",
+                        dataSource: {
+                          textField: "",
+                          valueField: "",
+                          // data: "",
+                        },
+                        //onChange: this.dropDownHandler.bind(this),
+                      }}
+                    />
+                    <div
+                      className="col-2 align-middle"
+                      style={{ paddingTop: 19 }}
+                    >
+                      <button className="btn btn-primary">Add</button>
+                    </div>
+                  </div>
+                  <div className="row">
+                    <div className="col-12" id="EmployeeKPGrid_Cntr">
+                      <AlgaehDataGrid
+                        id="ParamGroupGrid"
+                        columns={[
+                          {
+                            fieldName: "action",
+                            label: (
+                              <AlgaehLabel label={{ forceLabel: "Action" }} />
+                            ),
+                            displayTemplate: (row) => {
+                              return <i className="fas fa-trash-alt" />;
+                            },
+                            others: { maxWidth: 70 },
+                          },
+                          {
+                            fieldName: "paramGroupName",
+                            label: (
+                              <AlgaehLabel
+                                label={{ forceLabel: "Group Name" }}
+                              />
+                            ),
+                          },
+                        ]}
+                        keyId=""
+                        dataSource={{}}
+                        isEditable={false}
+                        paging={{ page: 0, rowsPerPage: 10 }}
+                        events={{}}
+                        others={{}}
+                      />
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
