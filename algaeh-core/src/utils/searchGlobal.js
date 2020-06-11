c6bc44bda28055ab8f64956ee766e8174f8b1f07
Syed Noor <syednoor.algaeh@gmail.com>
2020-06-11 14:36:50 +0530

    Recept entry SEARCH PURCHASE ORDER NO. hospital_id is remove

--- algaeh-core/src/utils/searchGlobal.js
+++ algaeh-core/src/utils/searchGlobal.js
@@ -287,8 +287,9 @@ let algaehSearchConfig = (searchName, req) => {
             inner join hims_d_vendor V on PO.vendor_id = V.hims_d_vendor_id \
             left join hims_d_pharmacy_location PL on PO.pharmcy_location_id = PL.hims_d_pharmacy_location_id\
             left join hims_d_inventory_location IL on PO.inventory_location_id = IL.hims_d_inventory_location_id \
-            where authorize1 = 'Y' and cancelled='N' and  PO.hospital_id=" +
-          hospitalId,
+            where authorize1 = 'Y' and cancelled='N' ",
+        //   and  PO.hospital_id=" +
+        // hospitalId,
         orderBy: "hims_f_procurement_po_header_id desc",
       },
       {
