1ab77ed5591c13649e1a24b25f49f0888e0acf03
irfan <irfan.algaeh@gmail.com>
2020-06-18 11:07:26 +0530

    added total_ot_hours,total_ot_cost in projectwise job cost and report

--- HrManagement/src/models/project_job_costing.js
+++ HrManagement/src/models/project_job_costing.js
@@ -1237,22 +1237,21 @@ export default {
       _mysql
         .executeQuery({
           query: `Select hims_f_project_wise_payroll_id,employee_id,  month, year,group_description,
-        E.employee_code,E.full_name,d.designation, project_id, P.project_code,P.project_desc,SD.sub_department_name,
-        COALESCE(worked_hours,0) + COALESCE(concat(floor(worked_minutes/60)  ,'.',worked_minutes%60),0)
-        as total_hours,COALESCE(basic_hours,0) + COALESCE(concat(floor(basic_minutes/60)  ,'.',basic_minutes%60),0)
-        as basic_hours,COALESCE(ot_hours,0) + COALESCE(concat(floor(ot_minutes/60)  ,'.',ot_minutes%60),0)
-        as ot_hours,COALESCE(wot_hours,0) + COALESCE(concat(floor(wot_minutes/60)  ,'.',wot_minutes%60),0)
-        as wot_hours,COALESCE(hot_hours,0) + COALESCE(concat(floor(hot_minutes/60)  ,'.',hot_minutes%60),0)
-        as hot_hours,basic_cost, ot_cost, wot_cost, hot_cost, cost,
-        COALESCE(ot_hours+wot_hours+hot_hours,0)+COALESCE(concat(floor((ot_minutes+wot_minutes+hot_minutes)/60) 
-        ,'.',(ot_minutes+wot_minutes+hot_minutes)%60),0) as total_ot_hours,
-        COALESCE(ot_cost+ wot_cost+hot_cost,0) as total_ot_cost
-        from hims_f_project_wise_payroll PWP 
-        inner join hims_d_employee  E on PWP.employee_id=E.hims_d_employee_id 
-        inner join hims_d_project  P on PWP.project_id=P.hims_d_project_id  
-        left join hims_d_employee_group EG on EG.hims_d_employee_group_id = E.employee_group_id  
-        left join hims_d_designation d on E.employee_designation_id = d.hims_d_designation_id 
-        left join hims_d_sub_department SD on SD.hims_d_sub_department_id = E.sub_department_id
+          E.employee_code,E.full_name,d.designation, project_id, P.project_code,P.project_desc,SD.sub_department_name,
+          concat(round((COALESCE(worked_hours,0) + COALESCE(concat(floor(worked_minutes/60)  ,'.',   right(concat(00,worked_minutes%60),2)),0)),2))
+          as total_hours,concat(round((COALESCE(basic_hours,0) + COALESCE(concat(floor(basic_minutes/60)  ,'.',   right(concat(00,basic_minutes%60),2)),0)),2)
+          )as basic_hours, concat( round((COALESCE(ot_hours,0) + COALESCE(concat(floor(ot_minutes/60)  ,'.',  right(concat(00,ot_minutes%60),2)),0)),2))
+          as ot_hours,concat(round((COALESCE(wot_hours,0) + COALESCE(concat(floor(wot_minutes/60)  ,'.', right(concat(00,wot_minutes%60),2)),0)),2)
+          ) as wot_hours,concat(round((COALESCE(hot_hours,0) + COALESCE(concat(floor(hot_minutes/60)  ,'.',  right(concat(00,hot_minutes%60),2)),0)),2)
+          ) as hot_hours,basic_cost, ot_cost, wot_cost, hot_cost, cost,
+          concat(round((COALESCE(ot_hours+wot_hours+hot_hours,0)+COALESCE(concat(floor((ot_minutes+wot_minutes+hot_minutes)/60) 
+          ,'.', right(concat(00,(ot_minutes+wot_minutes+hot_minutes)%60),2)),0)),2)) as total_ot_hours,
+          COALESCE(ot_cost+ wot_cost+hot_cost,0) as total_ot_cost  from hims_f_project_wise_payroll PWP 
+          inner join hims_d_employee  E on PWP.employee_id=E.hims_d_employee_id 
+          inner join hims_d_project  P on PWP.project_id=P.hims_d_project_id  
+          left join hims_d_employee_group EG on EG.hims_d_employee_group_id = E.employee_group_id  
+          left join hims_d_designation d on E.employee_designation_id = d.hims_d_designation_id 
+          left join hims_d_sub_department SD on SD.hims_d_sub_department_id = E.sub_department_id
         where year=? and month=?   ${strData}  order by project_id ;`,
           values: [input.year, input.month],
           printQuery: true,
