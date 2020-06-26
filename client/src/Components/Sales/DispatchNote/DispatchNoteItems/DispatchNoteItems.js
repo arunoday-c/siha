afafde611dd8256786d21e62a0d8e1ee376d4d72
Shanmuga Priyan <msp.algaeh@gmail.com>
2020-06-26 12:01:46 +0530

    fixed extended cost calculation issue in DN

--- client/src/Components/Sales/DispatchNote/DispatchNoteItems/DispatchNoteItems.js
+++ client/src/Components/Sales/DispatchNote/DispatchNoteItems/DispatchNoteItems.js
@@ -552,12 +552,10 @@ class DispatchNoteItems extends Component {
                                   />
                                 ),
                                 displayTemplate: (row) => {
-                                  if (
-                                    row.sale_price !== "" &&
-                                    row.dispatch_quantity !== ""
-                                  ) {
+                                  if (row.sale_price && row.dispatch_quantity) {
                                     return (
-                                      row.sale_price * row.dispatch_quantity
+                                      parseFloat(row.sale_price) *
+                                      parseFloat(row.dispatch_quantity)
                                     );
                                   }
                                 },
