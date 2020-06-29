142ea2de4589172bbbd045b9c497be411c69fc9e
Shanmuga Priyan <msp.algaeh@gmail.com>
2020-06-29 16:44:35 +0530

    added last seen functionality

--- Sockets/src/socketAuth.js
+++ Sockets/src/socketAuth.js
@@ -1,7 +1,6 @@
 export function authenticate(socket, data, callback) {
   const { token } = data;
   if (token && token.user_id) {
-    console.log("authed");
     return callback(null, true);
   } else {
     return callback(new Error("You are not allowed to access"));
@@ -12,10 +11,10 @@ export function postAuthenticate(socket, data) {
   const { token, moduleList } = data;
   socket.client.user = token.employee_id;
   socket.client.details = token;
-  socket.join(token.employee_id, err => {
+  socket.join(token.employee_id, (err) => {
     console.log(err);
   });
-  socket.join(moduleList, err => console.log(err));
+  socket.join(moduleList, (err) => console.log(err));
 }
 
 export function disconnect(socket) {
