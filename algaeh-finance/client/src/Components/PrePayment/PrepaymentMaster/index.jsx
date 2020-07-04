f6d3f97b71a9a256fdcb73fb97f8ee1e0c35034c
Aboobacker Sidhiqe <>
2020-07-05 05:10:26 +0530

    prepayment screen few changes

--- algaeh-finance/client/src/Components/PrePayment/PrepaymentMaster/index.jsx
+++ algaeh-finance/client/src/Components/PrePayment/PrepaymentMaster/index.jsx
@@ -1,14 +1,122 @@
 import React from "react";
-// import {
-//   AlgaehTabs,
-//   AlgaehLabel,
-//   AlagehFormGroup,
-//   AlagehAutoComplete,
-// } from "algaeh-react-components";
+import {
+  AlgaehFormGroup,
+  AlgaehDateHandler,
+  AlgaehAutoComplete,
+  AlgaehDataGrid,
+  AlgaehTreeSearch,
+  AlgaehMessagePop,
+  AlgaehFormGroupGrid,
+  AlgaehButton,
+} from "algaeh-react-components";
 export function PrepaymentMaster() {
   return (
     <div>
-      <div className="row inner-top-search"></div>
+      <div className="row inner-top-search">
+        <AlgaehFormGroup
+          div={{
+            className: "col form-group",
+          }}
+          label={{
+            forceLabel: "Prepayment Code",
+            isImp: true,
+          }}
+          textBox={{
+            type: "text",
+            className: "form-control",
+            placeholder: "",
+            value: "",
+          }}
+        />{" "}
+        <AlgaehFormGroup
+          div={{
+            className: "col-3 form-group",
+          }}
+          label={{
+            forceLabel: "Prepayment Desc.",
+            isImp: true,
+          }}
+          textBox={{
+            type: "text",
+            className: "form-control",
+            placeholder: "",
+            value: "",
+          }}
+        />
+        <AlgaehAutoComplete
+          div={{ className: "col form-group" }}
+          label={{
+            forceLabel: "Duration (Month)",
+            isImp: true,
+          }}
+          selector={{
+            value: "",
+            dataSource: {},
+          }}
+        />{" "}
+        <AlgaehAutoComplete
+          div={{ className: "col form-group" }}
+          label={{
+            forceLabel: "Prepayment GL",
+            isImp: true,
+          }}
+          selector={{
+            value: "",
+            dataSource: {},
+          }}
+        />
+        <AlgaehAutoComplete
+          div={{ className: "col form-group" }}
+          label={{
+            forceLabel: "Expense GL",
+            isImp: true,
+          }}
+          selector={{
+            value: "",
+            dataSource: {},
+          }}
+        />
+        <div className="col">
+          <button className="btn btn-primary bttn-sm" style={{ marginTop: 17 }}>
+            Add to list
+          </button>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-12">
+          <div className="portlet portlet-bordered margin-bottom-15">
+            <div className="portlet-title">
+              <div className="caption">
+                <h3 className="caption-subject">Journal Voucher List </h3>
+              </div>
+              <div className="actions"></div>
+            </div>
+            <div className="portlet-body">
+              <AlgaehDataGrid
+                columns={[
+                  {
+                    key: "slno",
+                    title: "Sl No.",
+                    sortable: true,
+                    others: {
+                      width: 80,
+                    },
+                  },
+                ]}
+                loading={false}
+                isEditable="onlyDelete"
+                height="34vh"
+                dataSource={{
+                  data: [],
+                }}
+                rowUnique="slno"
+                events={{}}
+                others={{}}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
