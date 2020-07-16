48b8aa822c1014eec58db4b936c3c31959b3d833
Aboobacker Sidhiqe <>
2020-07-16 14:11:00 +0530

    End of service Slip - Query find in trello

--- AlgaehReporting/algaeh_report_tool/templates/EndOfServiceSlip.js
+++ AlgaehReporting/algaeh_report_tool/templates/EndOfServiceSlip.js
@@ -1,8 +1,39 @@
 const executePDF = function executePDFMethod(options) {
   return new Promise(function (resolve, reject) {
     try {
-      const header = options.result[0][0];
-      resolve({ ...header });
+      const _ = options.loadash;
+      const moment = options.moment;
+      const writtenForm = options.writtenForm;
+      const result = options.result[0];
+
+      resolve({
+        ...result,
+        ...{
+          month: moment(result.month, "MM").format("MMMM"),
+          // total_gratutity_amount: options.currencyFormat(
+          //   result.total_gratutity_amount,
+          //   options.args.crypto
+          // ),
+          gratuity_encash: options.currencyFormat(
+            result.gratuity_encash,
+            options.args.crypto
+          ),
+          calculated_gratutity_amount: options.currencyFormat(
+            result.calculated_gratutity_amount,
+            options.args.crypto
+          ),
+          payable_amount: options.currencyFormat(
+            result.payable_amount,
+            options.args.crypto
+          ),
+
+          salary_in_words:
+            options.args.crypto.currency_symbol +
+            " " +
+            writtenForm(result.payable_amount) +
+            " Only",
+        },
+      });
     } catch (e) {
       reject(e);
     }
