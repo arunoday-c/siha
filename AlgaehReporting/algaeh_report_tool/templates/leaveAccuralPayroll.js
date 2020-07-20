5c1cd0bfc83ac77a590144c8840e6e3ab60a3de8
Aboobacker Sidhiqe <>
2020-07-20 19:41:12 +0530

    Leave Accural - Employee Type Issue - fixed

--- AlgaehReporting/algaeh_report_tool/templates/leaveAccuralPayroll.js
+++ AlgaehReporting/algaeh_report_tool/templates/leaveAccuralPayroll.js
@@ -6,11 +6,12 @@ const executePDF = function executePDFMethod(options) {
       let input = {};
       let params = options.args.reportParams;
       // const utilities = new algaehUtilities();
-      params.forEach(para => {
+      params.forEach((para) => {
         input[para["name"]] = para["value"];
       });
       const decimal_places = options.args.crypto.decimal_places;
 
+      console.log("abcd", input);
       let strData = "";
 
       if (input.employee_group_id > 0) {
@@ -25,32 +26,43 @@ const executePDF = function executePDFMethod(options) {
         strData += " and E.sub_department_id=" + input.sub_department_id;
       }
 
+      let is_local = "";
+
+      if (input.is_local === "Y") {
+        is_local = " and H.default_nationality=E.nationality ";
+      } else if (input.is_local === "N") {
+        is_local = " and H.default_nationality<>E.nationality ";
+      }
+
       options.mysql
         .executeQuery({
-          query: `select balance_leave_days,balance_leave_salary_amount,
-          balance_airticket_amount, E.employee_code, E.full_name, SD.sub_department_name, D.department_name, EG.group_description 
-          from hims_f_employee_leave_salary_header LA inner join hims_d_employee E on LA.employee_id=E.hims_d_employee_id 
-          left join hims_d_sub_department SD on E.sub_department_id=SD.hims_d_sub_department_id 
-          left join hims_d_department D on SD.department_id=D.hims_d_department_id 
-          left join hims_d_employee_group EG on E.employee_group_id=EG.hims_d_employee_group_id
-          where E.hospital_id=? ${strData} ;`,
+          query: `select balance_leave_days,balance_leave_salary_amount,\ 
+          balance_airticket_amount, E.employee_code, E.full_name, SD.sub_department_name, D.department_name, EG.group_description\ 
+          from hims_f_employee_leave_salary_header LA inner join hims_d_employee E on LA.employee_id=E.hims_d_employee_id\ 
+          left join hims_d_sub_department SD on E.sub_department_id=SD.hims_d_sub_department_id\ 
+          left join hims_d_department D on SD.department_id=D.hims_d_department_id\ 
+          left join hims_d_employee_group EG on E.employee_group_id=EG.hims_d_employee_group_id\ 
+          left join hims_d_hospital H  on E.hospital_id=H.hims_d_hospital_id \
+          where E.hospital_id=?  ${is_local}  ${strData} ;`,
           values: [input.hospital_id],
-          printQuery: true
+          printQuery: true,
         })
-        .then(ress => {
+        .then((ress) => {
           let sum_leave_salary = 0;
           let sum_airfare_amount = 0;
 
           if (ress.length > 0) {
-            sum_leave_salary = _.sumBy(ress, s => parseFloat(s.balance_leave_salary_amount));
-            sum_airfare_amount = _.sumBy(ress, s =>
+            sum_leave_salary = _.sumBy(ress, (s) =>
+              parseFloat(s.balance_leave_salary_amount)
+            );
+            sum_airfare_amount = _.sumBy(ress, (s) =>
               parseFloat(s.balance_airticket_amount)
             );
 
             const result = {
               detailsList: ress,
               sum_leave_salary: sum_leave_salary.toFixed(decimal_places),
-              sum_airfare_amount: sum_airfare_amount.toFixed(decimal_places)
+              sum_airfare_amount: sum_airfare_amount.toFixed(decimal_places),
             };
 
             // utilities.logger().log("outputArray:", result);
@@ -60,12 +72,12 @@ const executePDF = function executePDFMethod(options) {
               result: {
                 detailsList: ress,
                 sum_leave_salary: sum_leave_salary.toFixed(decimal_places),
-                sum_airfare_amount: sum_airfare_amount.toFixed(decimal_places)
-              }
+                sum_airfare_amount: sum_airfare_amount.toFixed(decimal_places),
+              },
             });
           }
         })
-        .catch(error => {
+        .catch((error) => {
           options.mysql.releaseConnection();
         });
     } catch (e) {
