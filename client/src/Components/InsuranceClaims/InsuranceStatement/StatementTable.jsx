a4cb064f5b25d649f652e2d13e64d845ac9bb6dc
Shanmuga Priyan <msp.algeah@gmail.com>
2020-08-17 18:17:20 +0530

    insurance statement done

---
+++ client/src/Components/InsuranceClaims/InsuranceStatement/StatementTable.jsx
@@ -0,0 +1,164 @@
+import React from "react";
+import { useQuery } from "react-query";
+import { AlgaehDataGrid, Spin, AlgaehLabel } from "algaeh-react-components";
+import { newAlgaehApi, useQueryParams } from "../../../hooks";
+
+const getStatements = async (key, { hims_f_insurance_statement_id }) => {
+  const res = await newAlgaehApi({
+    uri: "/insurance/getInsuranceStatement",
+    module: "insurance",
+    data: { hims_f_insurance_statement_id },
+    method: "GET",
+  });
+  return res?.data?.records;
+};
+
+export function StatementTable() {
+  const params = useQueryParams();
+  const hims_f_insurance_statement_id = params.get(
+    "hims_f_insurance_statement_id"
+  );
+
+  const { data, isLoading } = useQuery(
+    ["insurance", { hims_f_insurance_statement_id }],
+    getStatements,
+    {
+      enabled: !!hims_f_insurance_statement_id,
+      refetchOnWindowFocus: false,
+    }
+  );
+  return (
+    <Spin spinning={isLoading}>
+      <div className="portlet portlet-bordered margin-bottom-15">
+        <div className="portlet-title">
+          <div className="caption">
+            <h3 className="caption-subject">Insurance Statement List</h3>
+          </div>
+        </div>
+        <div className="portlet-body" id="InsuranceStatementGrid_Cntr">
+          <AlgaehDataGrid
+            id="InsuranceStatementGrid"
+            columns={[
+              {
+                fieldName: "patient_code",
+                label: <AlgaehLabel label={{ forceLabel: "Patient Code" }} />,
+              },
+              {
+                fieldName: "patient_name",
+                label: <AlgaehLabel label={{ forceLabel: "Patient Name" }} />,
+                disabled: true,
+                others: {
+                  resizable: false,
+                  style: { textAlign: "left" },
+                },
+              },
+              {
+                fieldName: "doctor_name",
+                label: <AlgaehLabel label={{ forceLabel: "Doctor Name" }} />,
+                disabled: true,
+                others: {
+                  resizable: false,
+                  style: { textAlign: "left" },
+                },
+              },
+              {
+                fieldName: "invoice_number",
+                label: <AlgaehLabel label={{ forceLabel: "Invoice No." }} />,
+                disabled: true,
+                others: {
+                  resizable: false,
+                  style: { textAlign: "left" },
+                },
+              },
+              {
+                fieldName: "invoice_date",
+                label: <AlgaehLabel label={{ forceLabel: "Invoice date." }} />,
+                disabled: true,
+                others: {
+                  resizable: false,
+                  style: { textAlign: "left" },
+                },
+              },
+              {
+                fieldName: "gross_amount",
+                label: <AlgaehLabel label={{ forceLabel: "Invoice Amt." }} />,
+                disabled: true,
+                others: {
+                  resizable: false,
+                  style: { textAlign: "left" },
+                },
+              },
+              {
+                fieldName: "company_resp",
+                label: (
+                  <AlgaehLabel label={{ forceLabel: "Co. Respo. Amt." }} />
+                ),
+              },
+              {
+                fieldName: "company_tax",
+                label: <AlgaehLabel label={{ forceLabel: "Co. Respo. Tax" }} />,
+                disabled: true,
+                others: {
+                  resizable: false,
+                  style: { textAlign: "left" },
+                },
+              },
+              {
+                fieldName: "company_payable",
+                label: <AlgaehLabel label={{ forceLabel: "Co. Net Payble" }} />,
+              },
+              {
+                fieldName: "denial_ammount",
+                label: <AlgaehLabel label={{ forceLabel: "Denial Amt. 1" }} />,
+              },
+              {
+                fieldName: "denial_amount_2",
+                label: <AlgaehLabel label={{ forceLabel: "Denial Amt. 2" }} />,
+              },
+              {
+                fieldName: "remittance_ammount",
+                label: (
+                  <AlgaehLabel label={{ forceLabel: "Remittance Amt. 1" }} />
+                ),
+              },
+              {
+                fieldName: "remittance_2",
+                label: (
+                  <AlgaehLabel label={{ forceLabel: "Remittance Amt. 2" }} />
+                ),
+              },
+            ]}
+            data={data?.claims ?? []}
+            // filter={true}
+            paging={{ page: 0, rowsPerPage: 20 }}
+          />
+        </div>{" "}
+      </div>{" "}
+      <div className="portlet portlet-bordered margin-bottom-15">
+        <div className="portlet-body">
+          <div className="row">
+            <div className="col-5"></div>
+            <div className="col-7">
+              <div className="row">
+                <div className="col">
+                  <label className="style_Label ">Total Claim Amount</label>
+                  <h6>{data?.total_company_payable || "0.00"}</h6>
+                </div>
+                <div className="col">
+                  <label className="style_Label ">Total Denial Amount</label>
+                  <h6>0.00</h6>
+                </div>{" "}
+                <div className="col">
+                  <label className="style_Label ">
+                    Total Remittance Amount
+                  </label>
+                  <h6>{data?.total_remittance_amount || "0.00"}</h6>
+                </div>{" "}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Spin>
+  );
+}
