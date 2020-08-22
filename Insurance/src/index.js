5fbb44c295fb789be0ed8e8af749357345b30d0a
suhail ulla khan <suhailkhan.algaeh@gmail.com>
2020-08-22 13:05:30 +0530

    added swagger for insurance statement

--- Insurance/src/index.js
+++ Insurance/src/index.js
@@ -8,19 +8,22 @@ import routes from "./routes";
 import compression from "compression";
 import { userSecurity } from "algaeh-utilities/checksecurity";
 import { authentication } from "algaeh-utilities/authentication";
+import SwaggerConfiguration from "algaeh-utilities/swagger";
 const app = express();
 app.server = http.createServer(app);
 app.use(cors());
 const _port = process.env.PORT;
 app.use(
   bodyParser.json({
-    limit: keys.bodyLimit
+    limit: keys.bodyLimit,
   })
 );
 app.use(compression());
 
 process.env.MYSQL_KEYS = JSON.stringify(keys.default);
-
+if (process.env.NODE_ENV === "development") {
+  new SwaggerConfiguration("Insurance Api's").Geteate(app);
+}
 app.use((req, res, next) => {
   authentication(req, res, next);
 });
@@ -89,13 +92,10 @@ app.use((req, res, next) => {
 
 app.use("/api/v1", routes);
 
-process.on("warning", warning => {
-  utliites
-    .AlgaehUtilities()
-    .logger()
-    .log("warn", warning, "warn");
+process.on("warning", (warning) => {
+  utliites.AlgaehUtilities().logger().log("warn", warning, "warn");
 });
-process.on("uncaughtException", error => {
+process.on("uncaughtException", (error) => {
   utliites
     .AlgaehUtilities()
     .logger()
@@ -128,17 +128,17 @@ app.use((error, req, res, next) => {
             host: reqH.host,
             "user-agent": reqH["user-agent"],
             "cache-control": reqH["cache-control"],
-            origin: reqH.origin
-          }
+            origin: reqH.origin,
+          },
         },
-        message: errorMessage
+        message: errorMessage,
       },
       "error"
     );
   res.status(error.status).json({
     success: false,
     isSql: error.sqlMessage != null ? true : false,
-    message: errorMessage
+    message: errorMessage,
   });
 });
 app.server.listen(_port);
---
+++ Insurance/src/swagger/index.js
@@ -0,0 +1,188 @@
+/**
+ * @swagger
+ * definitions:
+ *  Success:
+ *   type: object
+ *   required:
+ *    - success
+ *   properties:
+ *    success:
+ *     type: boolean
+ *    any:
+ *     type: object
+ *  Error:
+ *   type: object
+ *   required:
+ *    - success
+ *    - message
+ *   properties:
+ *    success:
+ *     type: boolean
+ *     default: false
+ *    message:
+ *     type: string
+ *  insuranceStatement:
+ *   type: object
+ *   properties:
+ *    claims:
+ *     type: array
+ *     items:
+ *      $ref: "#/definitions/ClaimsDetails"
+ *  ClaimsDetails:
+ *     type: object
+ *     properties:
+ *      card_class:
+ *       type: number
+ *      card_holder_age:
+ *       type: number
+ *      card_holder_gender:
+ *       type: string
+ *      card_holder_name:
+ *       type: string
+ *      card_number:
+ *       type: string
+ *       required: true
+ *      patient_code:
+ *       type: string
+ *      pat_name:
+ *       type: string
+ *      doc_name:
+ *       type: string
+ *      invoice_number:
+ *       type: string
+ *      invoice_date:
+ *       type: string
+ *       format : YYYY-MM-DD
+ *      gross_amount:
+ *       type: number
+ *       format: decimal
+ *      company_resp:
+ *       type: number
+ *       format: decimal
+ *      company_tax:
+ *       type: number
+ *       format: decimal
+ *      company_payable:
+ *       type: number
+ *       format: decimal
+ *      denial_ammount:
+ *       type: number
+ *       format: decimal
+ *      remittance_ammount:
+ *       type: number
+ *       format: decimal
+ *      claim_validated:
+ *       type: string
+ *      insurance_provider_id:
+ *       type: number
+ *      hims_f_invoice_header_id:
+ *       type: number
+ *      insurance_statement_id:
+ *       type: number
+ *      network_id:
+ *       type: number
+ *      network_office_id:
+ *       type: number
+ *      patient_id:
+ *       type: number
+ *      sub_insurance_id:
+ *       type: number
+ *      visit_id:
+ *       type: number
+ *  insuranceStamentDetails:
+ *      hims_f_insurance_statement_id:
+ *       type: number
+ *      insurance_provider_id:
+ *       type: number
+ *      insurance_statement_number:
+ *       type: string
+ *      insurance_status:
+ *       type: string
+ *      sub_insurance_id:
+ *       type: number
+ *      total_balance_amount:
+ *       type: number
+ *       format: decimal
+ *      total_company_payable:
+ *       type: number
+ *       format: decimal
+ *      total_denial_amount:
+ *       type: number
+ *       format: decimal
+ *      total_remittance_amount:
+ *       type: number
+ *       format: decimal
+ *  UpdateInsuranceStatement:
+ *      denial_ammount:
+ *       type: number
+ *       format: decimal
+ *      hims_f_invoice_header_id:
+ *       type: number
+ *      insurance_statement_id:
+ *       type: number
+ *      remittance_ammount:
+ *       type: number
+ *       format: decimal
+ */
+
+/**
+ * @swagger
+ *
+ * /insurance/getInsuranceStatement:
+ *  get:
+ *   tags:
+ *    - Insuarance Statement
+ *   produces:
+ *    -application/json
+ *   parameters:
+ *    - name: hims_f_insurance_statement_id
+ *      description: Get Insurance Statement For perticular Insuarance
+ *      in: query
+ *      required: true
+ *      type: string
+ *   responses:
+ *    200:
+ *     description: Return Insurance Statement Details
+ *     schema:
+ *      properties:
+ *       success:
+ *        type: boolean
+ *       records:
+ *        allOf:
+ *         - $ref: "#/definitions/insuranceStatement"
+ *         - $ref: "#/definitions/insuranceStamentDetails"
+ *    400:
+ *     decription: Return failure message
+ *     schema:
+ *      $ref: "#/definitions/Error"
+ */
+/**
+ * @swagger
+ * /insurance/updateInsuranceStatement:
+ *  put:
+ *   tags:
+ *    - Insurance Statement
+ *   produces:
+ *    -application/json
+ *   parameters:
+ *    - name: query
+ *      discription: Update Insurance Statement
+ *      in: query
+ *      required: true
+ *      schema:
+ *       $ref: "#/definitions/UpdateInsuranceStatement"
+ *   responses:
+ *    200:
+ *     description: Return success with messag
+ *     schema:
+ *      properties:
+ *       success:
+ *        type: boolean
+ *       message:
+ *        type: string
+ *        default: Updated Successfully
+ *    400:
+ *     decription: Return error message
+ *     schema:
+ *      $ref: "#/definitions/Error"
+ */
