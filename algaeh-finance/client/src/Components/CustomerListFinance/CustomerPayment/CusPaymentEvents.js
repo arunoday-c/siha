bd717dece3a8858edbfad92174c4911582b48701
Syed Noor <syednoor.algaeh@gmail.com>
2020-09-07 20:03:36 +0530

    Cutomer payments and algaeh-react-component version change

--- algaeh-finance/client/src/Components/CustomerListFinance/CustomerPayment/CusPaymentEvents.js
+++ algaeh-finance/client/src/Components/CustomerListFinance/CustomerPayment/CusPaymentEvents.js
@@ -1,12 +1,12 @@
 import { newAlgaehApi } from "../../../hooks";
 
-export async function getInvoicesForCustomer(child_id) {
+export async function getInvoicesForCustomer(child_id, is_opening_bal) {
   try {
     const result = await newAlgaehApi({
       uri: "/finance_customer/getCustomerInvoiceDetails",
       method: "GET",
       module: "finance",
-      data: { child_id }
+      data: { child_id, is_opening_bal },
     });
     return result;
   } catch (e) {
