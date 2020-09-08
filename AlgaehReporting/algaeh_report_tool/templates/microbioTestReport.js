f1df6336d96fe1bf82616dca6f0769521e6bdfdb
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2020-09-08 22:59:28 +0530

    added Collected and Validated date

--- AlgaehReporting/algaeh_report_tool/templates/microbioTestReport.js
+++ AlgaehReporting/algaeh_report_tool/templates/microbioTestReport.js
@@ -1,11 +1,13 @@
 const executePDF = function executePDFMethod(options) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     try {
       resolve({
         patient_code: options.result[0][0]["patient_code"],
         patient_name: options.result[0][0]["patient_name"],
         doctor_name: options.result[0][0]["doctor_name"],
         ordered_date: options.result[1][0]["ordered_date"],
+        entered_date: options.result[1][0]["entered_date"],
+        validated_date: options.result[1][0]["validated_date"],
         specimen: options.result[1][0]["specimen"],
         service_name: options.result[1][0]["service_name"],
         bacteria_type: options.result[1][0]["bacteria_type"],
@@ -13,7 +15,7 @@ const executePDF = function executePDFMethod(options) {
         bacteria_name: options.result[1][0]["bacteria_name"],
         validated_by: options.result[1][0]["validated_by"],
         comments: options.result[1][0]["comments"],
-        details: options.result[1]
+        details: options.result[1],
       });
     } catch (e) {
       reject(e);
