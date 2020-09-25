39cb671385fcdb7988799609b22ac549e1a48c8d
Nowshad <nowshad.algaeh@gmail.com>
2020-09-25 13:11:18 +0300

    Inventory Accountig entry issue

--- client/src/Components/Inventory/InvTransferEntry/InvTransferEntryEvents.js
+++ client/src/Components/Inventory/InvTransferEntry/InvTransferEntryEvents.js
@@ -196,8 +196,8 @@ const SaveTransferEntry = $this => {
     InputObj.inventory_stock_detail[i].quantity =
       InputObj.inventory_stock_detail[i].quantity_transfer;
 
-    data.inventory_stock_detail[i].ack_quantity =
-      data.inventory_stock_detail[i].quantity_transfer;
+    InputObj.inventory_stock_detail[i].ack_quantity =
+      InputObj.inventory_stock_detail[i].quantity_transfer;
 
     InputObj.inventory_stock_detail[i].grn_number =
       InputObj.inventory_stock_detail[i].grnno;
