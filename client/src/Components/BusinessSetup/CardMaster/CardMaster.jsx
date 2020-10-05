7c69c8a365921403bfca7e05f59b4cf8b85da16a
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2020-10-05 18:18:16 +0530

    card ui changes

--- client/src/Components/BusinessSetup/CardMaster/CardMaster.jsx
+++ client/src/Components/BusinessSetup/CardMaster/CardMaster.jsx
@@ -9,6 +9,7 @@ import {
   Tooltip,
 } from "algaeh-react-components";
 import React, { useState } from "react";
+import "./CardMaster.scss";
 import { useForm, Controller } from "react-hook-form";
 import { useMutation, useQuery } from "react-query";
 
@@ -120,253 +121,243 @@ export function CardMaster() {
 
   return (
     <Spin spinning={accLoading || cardLoading}>
-      <div className="row">
+      <div className="row inner-top-search">
         <div className="col-12">
-          <div className="portlet portlet-bordered margin-bottom-15">
-            <div className="portlet-body">
-              <form onSubmit={handleSubmit(onSubmit)}>
-                <div className="row">
-                  <Controller
-                    control={control}
-                    name="card_name"
-                    rules={{ required: "Required" }}
-                    render={(props) => (
-                      <AlgaehFormGroup
-                        div={{ className: "col-2 mandatory" }}
-                        label={{
-                          forceLabel: "Card Name",
-                          isImp: true,
-                        }}
-                        error={errors}
-                        textBox={{
-                          className: "txt-fld",
-                          name: "card_name",
-                          ...props,
-                        }}
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="row">
+              <Controller
+                control={control}
+                name="card_name"
+                rules={{ required: "Required" }}
+                render={(props) => (
+                  <AlgaehFormGroup
+                    div={{ className: "col  mandatory" }}
+                    label={{
+                      forceLabel: "Card Name",
+                      isImp: true,
+                    }}
+                    error={errors}
+                    textBox={{
+                      className: "txt-fld",
+                      name: "card_name",
+                      ...props,
+                    }}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="card_format"
+                rules={{ required: "Required" }}
+                render={(props) => (
+                  <div className="col form-group cardInputFld mandatory">
+                    <label className="styleLabel">
+                      Card Format<span className="imp">&nbsp;*</span>
+                    </label>
+                    <div className="ui input txt-fld">
+                      <Input
+                        placeholder="00-0000"
+                        name="masked_identity"
+                        {...props}
+                        suffix={
+                          <Tooltip title={ToolTipText}>
+                            <i className="fas fa-info-circle"></i>
+                          </Tooltip>
+                        }
                       />
-                    )}
-                  />
-
-                  <Controller
-                    control={control}
-                    name="card_format"
-                    rules={{ required: "Required" }}
-                    render={(props) => (
-                      <div className="col-2 form-group">
-                        <label className="styleLabel">ID Format</label>
-                        <div className="ui input txt-fld">
-                          <Input
-                            placeholder="##-####-####"
-                            name="masked_identity"
-                            {...props}
-                            suffix={
-                              <Tooltip title={ToolTipText}>
-                                <i className="fas fa-info-circle"></i>
-                              </Tooltip>
-                            }
-                          />
-                        </div>
-                      </div>
-                    )}
-                  />
-
-                  <Controller
-                    control={control}
-                    name="vat_percentage"
-                    rules={{ required: "Required" }}
-                    render={(props) => (
-                      <AlgaehFormGroup
-                        div={{ className: "col-2 mandatory" }}
-                        label={{
-                          forceLabel: "Vat %",
-                          isImp: true,
-                        }}
-                        error={errors}
-                        textBox={{
-                          className: "txt-fld",
-                          name: "vat_percentage",
-                          ...props,
-                        }}
-                      />
-                    )}
-                  />
-
-                  <Controller
-                    control={control}
-                    name="service_charge"
-                    rules={{ required: "Required" }}
-                    render={(props) => (
-                      <AlgaehFormGroup
-                        div={{ className: "col-2 mandatory" }}
-                        label={{
-                          forceLabel: "Service Charge",
-                          isImp: true,
-                        }}
-                        error={errors}
-                        textBox={{
-                          className: "txt-fld",
-                          name: "service_charge",
-                          ...props,
-                        }}
-                      />
-                    )}
-                  />
-
-                  <Controller
-                    control={control}
-                    name="account"
-                    rules={{ required: "Required" }}
-                    render={(props) => (
-                      <AlgaehTreeSearch
-                        div={{ className: "col-3 form-group" }}
-                        label={{
-                          forceLabel: "Account",
-                          isImp: true,
-                          align: "ltr",
-                        }}
-                        error={errors}
-                        tree={{
-                          treeDefaultExpandAll: true,
-                          name: "prepayment_gl",
-                          data: accounts,
-                          textField: "label",
-                          ...props,
-                          valueField: (node) => {
-                            if (node["leafnode"] === "Y") {
-                              return (
-                                node["head_id"] +
-                                "-" +
-                                node["finance_account_child_id"]
-                              );
-                            } else {
-                              return node["finance_account_head_id"];
-                            }
-                          },
-                        }}
-                      />
-                    )}
-                  />
-
-                  <div className="col-2">
-                    <button
-                      type="submit"
-                      style={{ marginTop: 20 }}
-                      className="btn btn-primary"
-                      //   onClick={this.addCardMaster.bind(this)}
-                    >
-                      Add to Card List
-                    </button>
+                    </div>
                   </div>
-                </div>
-              </form>
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="vat_percentage"
+                rules={{ required: "Required" }}
+                render={(props) => (
+                  <AlgaehFormGroup
+                    div={{ className: "col mandatory" }}
+                    label={{
+                      forceLabel: "Vat %",
+                      isImp: true,
+                    }}
+                    error={errors}
+                    textBox={{
+                      className: "txt-fld",
+                      name: "vat_percentage",
+                      ...props,
+                    }}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="service_charge"
+                rules={{ required: "Required" }}
+                render={(props) => (
+                  <AlgaehFormGroup
+                    div={{ className: "col mandatory" }}
+                    label={{
+                      forceLabel: "Service Charge %",
+                      isImp: true,
+                    }}
+                    error={errors}
+                    textBox={{
+                      className: "txt-fld",
+                      name: "service_charge",
+                      ...props,
+                    }}
+                  />
+                )}
+              />
+
+              <Controller
+                control={control}
+                name="account"
+                rules={{ required: "Required" }}
+                render={(props) => (
+                  <AlgaehTreeSearch
+                    div={{ className: "col form-group" }}
+                    label={{
+                      forceLabel: "Account",
+                      isImp: true,
+                      align: "ltr",
+                    }}
+                    error={errors}
+                    tree={{
+                      treeDefaultExpandAll: true,
+                      name: "prepayment_gl",
+                      data: accounts,
+                      textField: "label",
+                      ...props,
+                      valueField: (node) => {
+                        if (node["leafnode"] === "Y") {
+                          return (
+                            node["head_id"] +
+                            "-" +
+                            node["finance_account_child_id"]
+                          );
+                        } else {
+                          return node["finance_account_head_id"];
+                        }
+                      },
+                    }}
+                  />
+                )}
+              />
+
+              <div className="col">
+                <button
+                  type="submit"
+                  style={{ marginTop: 20 }}
+                  className="btn btn-primary"
+                  //   onClick={this.addCardMaster.bind(this)}
+                >
+                  Add to List
+                </button>
+              </div>
             </div>
-          </div>
-          <div className="portlet portlet-bordered margin-bottom-15">
-            <div className="portlet-body">
-              <div className="row">
-                <div className="col-12">
-                  <div data-validate="cardDiv" id="CardMasterGrid_Cntr">
-                    <AlgaehDataGrid
-                      id="CardMasterGrid"
-                      datavalidate="data-validate='cardDiv'"
-                      columns={[
-                        {
-                          fieldName: "",
-                          label: "Actions",
-                          displayTemplate: (row) => {
-                            return (
-                              <>
-                                <i
-                                  className="fas fa-pen"
-                                  onClick={() => {
-                                    reset({
-                                      ...row,
-                                      account: `${row?.head_id}-${row?.child_id}`,
-                                    });
-                                    setCurrent(row);
-                                  }}
-                                ></i>
+          </form>
+        </div>
+      </div>
+      <div className="portlet portlet-bordered margin-bottom-15">
+        <div className="portlet-body">
+          <div className="row">
+            <div className="col-12">
+              <div data-validate="cardDiv" id="CardMasterGrid_Cntr">
+                <AlgaehDataGrid
+                  id="CardMasterGrid"
+                  datavalidate="data-validate='cardDiv'"
+                  columns={[
+                    {
+                      fieldName: "",
+                      label: "Actions",
+                      displayTemplate: (row) => {
+                        return (
+                          <>
+                            <i
+                              className="fas fa-pen"
+                              onClick={() => {
+                                reset({
+                                  ...row,
+                                  account: `${row?.head_id}-${row?.child_id}`,
+                                });
+                                setCurrent(row);
+                              }}
+                            ></i>
 
-                                <i
-                                  className="fas fa-trash-alt"
-                                  onClick={() =>
-                                    deleteRow(row?.hims_d_bank_card_id)
+                            <i
+                              className="fas fa-trash-alt"
+                              onClick={() =>
+                                deleteRow(row?.hims_d_bank_card_id)
+                              }
+                            ></i>
+                          </>
+                        );
+                      },
+                    },
+                    {
+                      fieldName: "card_name",
+                      label: (
+                        <AlgaehLabel label={{ forceLabel: "Card Name" }} />
+                      ),
+                    },
+                    {
+                      fieldName: "card_format",
+                      label: (
+                        <AlgaehLabel label={{ forceLabel: "Card Format" }} />
+                      ),
+                    },
+                    {
+                      fieldName: "service_charge",
+                      label: (
+                        <AlgaehLabel label={{ forceLabel: "Service Charge" }} />
+                      ),
+                    },
+                    {
+                      fieldName: "vat_percentage",
+                      label: <AlgaehLabel label={{ forceLabel: "Vat %" }} />,
+                    },
+                    {
+                      fieldName: "child_id",
+                      label: <AlgaehLabel label={{ forceLabel: "Account" }} />,
+                      displayTemplate: (row) => {
+                        if (row.head_id) {
+                          return (
+                            <AlgaehTreeSearch
+                              div={{ className: "white" }}
+                              tree={{
+                                treeDefaultExpandAll: true,
+                                name: "prepayment_head_id",
+                                data: accounts,
+                                value: `${row?.head_id}-${row?.child_id}`,
+                                textField: "label",
+                                disabled: true,
+                                valueField: (node) => {
+                                  if (node["leafnode"] === "Y") {
+                                    return (
+                                      node["head_id"] +
+                                      "-" +
+                                      node["finance_account_child_id"]
+                                    );
+                                  } else {
+                                    return node["finance_account_head_id"];
                                   }
-                                ></i>
-                              </>
-                            );
-                          },
-                        },
-                        {
-                          fieldName: "card_name",
-                          label: (
-                            <AlgaehLabel label={{ forceLabel: "Card Name" }} />
-                          ),
-                        },
-                        {
-                          fieldName: "card_format",
-                          label: (
-                            <AlgaehLabel
-                              label={{ forceLabel: "Card Format" }}
+                                },
+                              }}
                             />
-                          ),
-                        },
-                        {
-                          fieldName: "service_charge",
-                          label: (
-                            <AlgaehLabel
-                              label={{ forceLabel: "Service Charge" }}
-                            />
-                          ),
-                        },
-                        {
-                          fieldName: "vat_percentage",
-                          label: (
-                            <AlgaehLabel label={{ forceLabel: "Vat %" }} />
-                          ),
-                        },
-                        {
-                          fieldName: "child_id",
-                          label: (
-                            <AlgaehLabel label={{ forceLabel: "Account" }} />
-                          ),
-                          displayTemplate: (row) => {
-                            if (row.head_id) {
-                              return (
-                                <AlgaehTreeSearch
-                                  div={{ className: "white" }}
-                                  tree={{
-                                    treeDefaultExpandAll: true,
-                                    name: "prepayment_head_id",
-                                    data: accounts,
-                                    value: `${row?.head_id}-${row?.child_id}`,
-                                    textField: "label",
-                                    disabled: true,
-                                    valueField: (node) => {
-                                      if (node["leafnode"] === "Y") {
-                                        return (
-                                          node["head_id"] +
-                                          "-" +
-                                          node["finance_account_child_id"]
-                                        );
-                                      } else {
-                                        return node["finance_account_head_id"];
-                                      }
-                                    },
-                                  }}
-                                />
-                              );
-                            } else {
-                              return null;
-                            }
-                          },
-                        },
-                      ]}
-                      rowUniqueId="hims_d_bank_card_id"
-                      data={cards || []}
-                    />
-                  </div>
-                </div>
+                          );
+                        } else {
+                          return null;
+                        }
+                      },
+                    },
+                  ]}
+                  rowUniqueId="hims_d_bank_card_id"
+                  data={cards || []}
+                />
               </div>
             </div>
           </div>
