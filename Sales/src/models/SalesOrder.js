a637c5b75f6a512c68b995ff7b6c744abb5cb0c4
Nowshad <nowshad.algaeh@gmail.com>
2020-11-11 13:39:14 +0530

    When Sales quotation select in slaes order if tax exists not calculating properly

--- Sales/src/models/SalesOrder.js
+++ Sales/src/models/SalesOrder.js
@@ -565,7 +565,7 @@ export function getSalesQuotationForOrder(req, res, next) {
 
                     if (req.query.sales_order_mode == "I") {
                         strQuery = mysql.format(
-                            "select QI.*, IM.item_description, IU.uom_description, quantity as quantity_outstanding \
+                            "select QI.*,(QI.net_extended_cost + QI.tax_amount) as total_amount, IM.item_description, IU.uom_description, quantity as quantity_outstanding \
                             from hims_f_sales_quotation_items QI \
                             inner join hims_d_inventory_item_master IM on IM.hims_d_inventory_item_master_id = QI.item_id \
                             inner join hims_d_inventory_uom IU on IU.hims_d_inventory_uom_id = QI.uom_id \
