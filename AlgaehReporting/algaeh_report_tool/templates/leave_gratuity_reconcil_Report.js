b3ea6812eb8085a9cb88a9e990abd590f2213a20
DESKTOP-E68C2AL\sidhi <sidhiqe.algeah@gmail.com>
2020-11-19 07:42:53 +0530

    leaveAccuralReport corrections

--- AlgaehReporting/algaeh_report_tool/templates/leave_gratuity_reconcil_Report.js
+++ AlgaehReporting/algaeh_report_tool/templates/leave_gratuity_reconcil_Report.js
@@ -6,7 +6,11 @@ const executePDF = function executePDFMethod(options) {
       let str = "";
       let input = {};
       let params = options.args.reportParams;
-      const decimal_places = options.args.crypto.decimal_places;
+      const {
+        decimal_places,
+        symbol_position,
+        currency_symbol,
+      } = options.args.crypto;
 
       params.forEach((para) => {
         input[para["name"]] = para["value"];
@@ -32,29 +36,6 @@ const executePDF = function executePDFMethod(options) {
 
       options.mysql
         .executeQuery({
-          // query: `select
-          // max(FA.leave_salary_amount) as leave_salary_amount,
-          // sum(FA.leave_salary_amount) as total_leave_salary_amount,
-          // max(FA.leave_days) as leave_days,
-          // sum(FA.leave_days) as total_leave_days,
-          // max(FA.airticket_amount) as airticket_amount,
-          // sum(FA.airticket_amount) as total_airticket_amount,
-          // FA.employee_id,
-          // max(FA.employee_code) as employee_code, max(FA.full_name) as full_name,
-          // max(FA.designation) as designation, max(FA.date_of_joining) as date_of_joining,
-          // max(FA.basic_salary) as basic_salary,
-          // max(FA.year) year,
-          // max(FA.deducting_month) deducting_month
-          // from ( SELECT LH.employee_id,EM.employee_code, EM.full_name,DS.designation, EM.date_of_joining,EE.amount as basic_salary, max(LD.year) as year, MONTHNAME(CONCAT('2011-',LD.month,'-01')) as deducting_month,
-          // LD.leave_salary_amount,
-          // LD.leave_days,
-          // LD.airticket_amount
-          // FROM hims_f_employee_leave_salary_header LH
-          // inner join hims_f_employee_leave_salary_detail LD on LD.employee_leave_salary_header_id = LH.hims_f_employee_leave_salary_header_id
-          // inner join hims_d_employee as EM on LH.employee_id = EM.hims_d_employee_id
-          // inner join hims_d_designation as DS on DS.hims_d_designation_id = EM.employee_designation_id
-          // inner join hims_d_employee_earnings as EE on EE.employee_id = EM.hims_d_employee_id and earnings_id=1
-          // where EM.employee_status <> 'I' and EM.hospital_id=? and LD.year=? and LD.month <= ?  ${str} group by LD.month, LD.leave_salary_amount,LD.leave_days,LD.airticket_amount, LH.employee_id, LH.hims_f_employee_leave_salary_header_id ) as FA group by FA.employee_id;`,
           query: `SELECT LH.employee_id,EM.employee_code, EM.full_name,DS.designation, EM.date_of_joining,EE.amount as basic_salary, max(LD.year) as year, MONTHNAME(CONCAT('2011-',LD.month,'-01')) as deducting_month, 
          LD.leave_salary_amount, 
          LD.leave_days, 
@@ -148,6 +129,18 @@ const executePDF = function executePDFMethod(options) {
             grand_total_leave_salary,
             grand_total_airticket_amount,
             grand_total_balance_airticket_amount,
+            decimalOnly: {
+              decimal_places,
+              addSymbol: false,
+              symbol_position,
+              currency_symbol,
+            },
+            currencyOnly: {
+              decimal_places,
+              addSymbol: true,
+              symbol_position,
+              currency_symbol,
+            },
           });
         })
         .catch((error) => {
