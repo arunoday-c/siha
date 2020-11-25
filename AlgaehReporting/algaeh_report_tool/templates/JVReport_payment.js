032ef913249411d506233a32e41fc6791231aa6f
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2020-11-25 19:19:45 +0530

    Journal Voucher Report Updated

--- AlgaehReporting/algaeh_report_tool/templates/JVReport_payment.js
+++ AlgaehReporting/algaeh_report_tool/templates/JVReport_payment.js
@@ -1,55 +1,64 @@
 const executePDF = function executePDFMethod(options) {
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
     try {
       const _ = options.loadash;
       const {
         decimal_places,
         symbol_position,
-        currency_symbol
+        currency_symbol,
       } = options.args.crypto;
 
       let str = "";
       let input = {};
       let params = options.args.reportParams;
 
-      params.forEach(para => {
+      params.forEach((para) => {
         input[para["name"]] = para["value"];
       });
 
       options.mysql
         .executeQuery({
-          query: `SELECT VH.voucher_no,VH.voucher_type,VH.amount,VH.payment_date,VH.settled_amount, VD.child_id, VN.vendor_name,VN.address,VN.contact_number,VH.narration
-          FROM finance_voucher_header as VH
-          left join finance_voucher_details as VD on  VD.voucher_header_id = VH.finance_voucher_header_id
-          inner join hims_d_vendor as VN on  VN.child_id = VD.child_id
-          where VH.finance_voucher_header_id=? group by VN.hims_d_vendor_id;
-          SELECT VH.voucher_no,VH.invoice_no,VH.due_date,VH.amount,VH.payment_date,VH.settled_amount,VH.voucher_type, (VH.amount - VH.settled_amount) as balance
-          FROM finance_voucher_header as VH
-          left join finance_voucher_sub_header as VSH on VH.invoice_no=VSH.invoice_ref_no
-          where VH.finance_voucher_header_id=?;`,
+          query: `
+          SELECT VD.voucher_type,VD.voucher_no,VD.amount,VD.payment_mode,VD.payment_date,
+          VD.narration,VD.due_date,VD.settled_amount,VD.created_by,
+          VN.vendor_name,VN.vendor_code,VN.contact_number,VN.address,VN.vat_number
+          FROM finance_voucher_header VD
+          inner join hims_f_procurement_grn_header IVH on IVH.inovice_number = VD.invoice_ref_no
+          inner join hims_d_vendor VN on VN.hims_d_vendor_id = IVH.vendor_id
+          where VD.finance_voucher_header_id=?;
+          SELECT IVH.grn_number,DATE(IVH.invoice_date) as invoice_date, IVH.inovice_number,
+          DATE(IVH.grn_date) as grn_date,IVH.po_id,IVH.sub_total,IVH.net_total,IVH.detail_discount,IVH.total_tax,IVH.net_payable,IVH.hospital_id
+          FROM finance_voucher_header VD
+          inner join hims_f_procurement_grn_header IVH on IVH.inovice_number = VD.invoice_ref_no
+          inner join hims_d_vendor VN on VN.hims_d_vendor_id = IVH.vendor_id
+          where VD.finance_voucher_header_id=?;
+          `,
           values: [input.voucher_header_id, input.voucher_header_id],
-          printQuery: true
+          printQuery: true,
         })
-        .then(result => {
+        .then((result) => {
+          // console.log(subTotal);
           resolve({
             resultHeader: result[0].length > 0 ? result[0][0] : {},
-            // subTotal:resultHeader.amount,
             resultInvoice: result[1],
+            totalNetPayable: _.sumBy(result[1], (s) =>
+              parseFloat(s.net_payable)
+            ),
             currency: {
               decimal_places,
               addSymbol: false,
               symbol_position,
-              currency_symbol
+              currency_symbol,
             },
             currencyHeader: {
               decimal_places,
               addSymbol: true,
               symbol_position,
-              currency_symbol
-            }
+              currency_symbol,
+            },
           });
         })
-        .catch(error => {
+        .catch((error) => {
           options.mysql.releaseConnection();
         });
     } catch (e) {
