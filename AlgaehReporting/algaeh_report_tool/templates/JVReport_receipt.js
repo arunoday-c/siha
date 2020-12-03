cf176fa7a0a36f6eb05b4862cfd7a59f82ae2c76
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2020-12-03 10:52:18 +0530

    JV Receipt Report Updated

--- AlgaehReporting/algaeh_report_tool/templates/JVReport_receipt.js
+++ AlgaehReporting/algaeh_report_tool/templates/JVReport_receipt.js
@@ -20,11 +20,14 @@ const executePDF = function executePDFMethod(options) {
         .executeQuery({
           query: `SELECT VD.voucher_type,VD.voucher_no,VD.amount,VD.payment_mode,VD.payment_date,
           VD.narration,VD.due_date,VD.settled_amount,VD.created_by,IVH.customer_id,IVH.sales_order_id,
-          CO.customer_name,CO.customer_code,CO.contact_number,CO.address,CO.vat_number
+          CO.customer_name,CO.customer_code,CO.contact_number,CO.address,CO.vat_number,
+          VDD.payment_type,VDD.head_id,VDD.child_id,FCD.head_id,FCD.finance_account_child_id,FCD.child_name,FCD.arabic_child_name
           -- if (VD.receipt_type = 'M', VD.invoice_ref_no,VD.invoice_no) as invoiceNum
           FROM finance_voucher_header VD
           inner join hims_f_sales_invoice_header IVH on IVH.invoice_number = VD.invoice_ref_no
           inner join hims_d_customer CO on CO.hims_d_customer_id = IVH.customer_id
+          inner join finance_voucher_details VDD on VDD.voucher_header_id = VD.finance_voucher_header_id and VDD.payment_type='DR'
+          inner join finance_account_child FCD on FCD.finance_account_child_id = VDD.child_id and FCD.head_id = VDD.head_id
           where VD.finance_voucher_header_id=?;
           SELECT IVH.invoice_number,
           IVH.invoice_date,IVH.sales_invoice_mode,IVH.sales_order_id,IVH.customer_id,
