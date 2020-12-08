21f98014453ad7f90767632c6b143d8b27b6848c
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2020-12-08 13:45:52 +0530

    Pharmacy Return Item Detail report changes

--- AlgaehReporting/algaeh_report_tool/templates/salesReturnListPharmacy.js
+++ AlgaehReporting/algaeh_report_tool/templates/salesReturnListPharmacy.js
@@ -1,19 +1,69 @@
-//const algaehUtilities = require('algaeh-utilities/utilities');
+// const algaehUtilities = require("algaeh-utilities/utilities");
 const executePDF = function executePDFMethod(options) {
-  //	const utilities = new algaehUtilities();
-  const _ = options.loadash;
-  return new Promise(function(resolve, reject) {
+  return new Promise(function (resolve, reject) {
+    // const utilities = new algaehUtilities();
     try {
-      const result = options.result;
+      const _ = options.loadash;
+      let str = "";
+      let input = {};
+      let params = options.args.reportParams;
       const decimal_places = options.args.crypto.decimal_places;
-      const output = {
-        details: result,
-        total_return: _.sumBy(result, s => parseFloat(s.return_amout)).toFixed(
-          decimal_places
-        )
-      };
-      //utilities.logger().log("output: ", output);
-      resolve(output);
+      params.forEach((para) => {
+        input[para["name"]] = para["value"];
+      });
+
+      options.mysql
+        .executeQuery({
+          query: `
+          select SRH.hims_f_pharmcy_sales_return_header_id,SRH.sales_return_number,SRH.sales_return_date,SRH.net_amount,
+          PD.item_id,ITM.item_code,ITM.item_description, PD.expiry_date,PD.return_quantity,PD.quantity,PD.batchno,PD.insurance_yesno,
+          PD.patient_responsibility as PD_patient_responsibility,PD.patient_tax as PD_patient_tax,PD.patient_payable as PD_patient_payable,
+          PD.company_responsibility as PD_company_responsibility,PD.company_tax as PD_company_tax,PD.company_payable as PD_company_payable,
+          PD.net_extended_cost as PD_net_extended_cost, (COALESCE(PD.patient_payable,0) + COALESCE(PD.company_payable,0)) as PD_net_payable
+          from hims_f_pharmacy_sales_return_detail PD
+          left join hims_f_pharmcy_sales_return_header SRH on PD.sales_return_header_id = SRH.hims_f_pharmcy_sales_return_header_id
+          left join hims_f_patient P on SRH.patient_id = P.hims_d_patient_id
+          inner join hims_d_item_master ITM on ITM.hims_d_item_master_id = PD.item_id
+          where SRH.record_status='A' and SRH.location_id=? and SRH.hospital_id=? 
+          and SRH.posted='Y' and date(SRH.sales_return_date) between date(?) and  date(?);
+          `,
+          values: [
+            input.location_id,
+            input.hospital_id,
+            input.from_date,
+            input.to_date,
+          ],
+          printQuery: true,
+        })
+        .then((results) => {
+          const result = _.chain(results)
+            .groupBy((g) => g.hims_f_pharmcy_sales_return_header_id)
+            .map(function (dtl, key) {
+              const {
+                sales_return_number,
+                sales_return_date,
+                net_amount,
+              } = _.head(dtl);
+              return {
+                service_type: key,
+                detailList: dtl,
+                sales_return_number,
+                sales_return_date,
+                net_amount,
+              };
+            })
+            .value();
+
+          const net_total = options.currencyFormat(
+            _.sumBy(result, (s) => parseFloat(s.net_amount)),
+            options.args.crypto
+          );
+          console.log("result=======", result);
+          resolve({ detail: result, net_total: net_total });
+        })
+        .catch((error) => {
+          options.mysql.releaseConnection();
+        });
     } catch (e) {
       reject(e);
     }
