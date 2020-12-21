6630456cd5d1a83d41ef3c9a5721adf150dda99e
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2020-12-21 16:21:50 +0530

    UCAF Report UI Changes

--- AlgaehReporting/algaeh_report_tool/templates/ucaf.js
+++ AlgaehReporting/algaeh_report_tool/templates/ucaf.js
@@ -1,4 +1,5 @@
 const executePDF = function executePDFMethod(options) {
+  const _ = options.loadash;
   return new Promise(function (resolve, reject) {
     try {
       resolve({
@@ -6,6 +7,9 @@ const executePDF = function executePDFMethod(options) {
         insurance: options.result[1],
         medication: options.result[2],
         services: options.result[3],
+        sum_service_net_amout: _.sumBy(options.result[3], (s) =>
+          parseFloat(s.service_net_amout)
+        ),
       });
     } catch (e) {
       reject(e);
