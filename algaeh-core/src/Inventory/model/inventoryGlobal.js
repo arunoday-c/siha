e81b8c7eba548437306bb3a1d6a8c694be467b7b
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2020-12-22 16:39:05 +0530

    changes reverted

--- algaeh-core/src/Inventory/model/inventoryGlobal.js
+++ algaeh-core/src/Inventory/model/inventoryGlobal.js
@@ -113,7 +113,7 @@ let getItemLocationStock = (req, res, next) => {
 
     db.getConnection((error, connection) => {
       connection.query(
-        "SELECT hims_m_inventory_item_location_id, item_id, inventory_location_id, item_location_status, vendor_batchno,batchno, expirydt, barcode, qtyhand, qtypo, cost_uom,\
+        "SELECT hims_m_inventory_item_location_id, item_id, inventory_location_id, item_location_status,batchno, expirydt, barcode, qtyhand, qtypo, cost_uom,\
         avgcost, last_purchase_cost, item_type, grn_id, grnno, sale_price, mrp_price, sales_uom \
         from hims_m_inventory_item_location where record_status='A'  and item_id=? and inventory_location_id=? \
         and qtyhand>0  order by expirydt",
