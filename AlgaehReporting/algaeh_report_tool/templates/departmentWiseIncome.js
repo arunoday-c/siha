ea30df32361da439c54148c50292d2d994aa728f
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2021-01-05 07:09:10 +0530

    All the Income report added Total Row and done ui changes

--- AlgaehReporting/algaeh_report_tool/templates/departmentWiseIncome.js
+++ AlgaehReporting/algaeh_report_tool/templates/departmentWiseIncome.js
@@ -1,9 +1,39 @@
 const executePDF = function executePDFMethod(options) {
-  //const _ = options.loadash;
-  return new Promise(function(resolve, reject) {
+  const _ = options.loadash;
+  return new Promise(function (resolve, reject) {
     try {
-      const result = { details: options.result };
-      resolve(result);
+      let str = "";
+      let input = {};
+      let params = options.args.reportParams;
+
+      params.forEach((para) => {
+        input[para["name"]] = para["value"];
+      });
+
+      options.mysql
+        .executeQuery({
+          query: `select S.sub_department_id,SD.sub_department_code,SD.sub_department_name , sum(BD.net_amout) as total_amount 
+            from hims_f_billing_header BH 
+            inner join hims_f_billing_details BD on BH.hims_f_billing_header_id=BD.hims_f_billing_header_id  
+            inner join hims_d_services S on BD.services_id = S.hims_d_services_id and S.record_status='A' 
+            inner join hims_d_sub_department SD on S.sub_department_id=SD.hims_d_sub_department_id 
+            where BH.record_status='A' and BD.record_status='A' and BH.hospital_id=? and date(BH.bill_date) between date(?) and date(?) group by S.sub_department_id`,
+          values: [input.hospital_id, input.from_date, input.to_date],
+          printQuery: true,
+        })
+        .then((result) => {
+          const data = {
+            details: result,
+            net_payable: options.currencyFormat(
+              _.sumBy(result, (s) => parseFloat(s.total_amount)),
+              options.args.crypto
+            ),
+          };
+          resolve(data);
+        })
+        .catch((error) => {
+          options.mysql.releaseConnection();
+        });
     } catch (e) {
       reject(e);
     }
--- AlgaehReporting/algaeh_report_tool/templates/opBillSummary.js
+++ AlgaehReporting/algaeh_report_tool/templates/opBillSummary.js
@@ -1,12 +1,41 @@
 const executePDF = function executePDFMethod(options) {
-	const _ = options.loadash;
-	return new Promise(function(resolve, reject) {
-		try {
-			const result = { details: options.result };
-			resolve(result);
-		} catch (e) {
-			reject(e);
-		}
-	});
+  const _ = options.loadash;
+  return new Promise(function (resolve, reject) {
+    try {
+      let str = "";
+      let input = {};
+      let params = options.args.reportParams;
+
+      params.forEach((para) => {
+        input[para["name"]] = para["value"];
+      });
+
+      options.mysql
+        .executeQuery({
+          query: `select BH.hims_f_billing_header_id,BH.bill_date,BD.hims_f_billing_details_id,BD.service_type_id, ST.service_type_code,ST.service_type, sum(BD.net_amout)as total_amount 
+			from hims_f_billing_header BH 
+			inner join hims_f_billing_details BD on BH.hims_f_billing_header_id=BD.hims_f_billing_header_id 
+			inner join hims_d_service_type ST on BD.service_type_id=ST.hims_d_service_type_id and ST.record_status='A' 
+			where BH.hospital_id=? and  BH.record_status='A' and BD.cancel_yes_no='N' and adjusted='N' and BD.record_status='A' and date(BH.bill_date) between date(?) and date(?)  group by BD.service_type_id `,
+          values: [input.hospital_id, input.from_date, input.to_date],
+          printQuery: true,
+        })
+        .then((result) => {
+          const data = {
+            details: result,
+            net_payable: options.currencyFormat(
+              _.sumBy(result, (s) => parseFloat(s.total_amount)),
+              options.args.crypto
+            ),
+          };
+          resolve(data);
+        })
+        .catch((error) => {
+          options.mysql.releaseConnection();
+        });
+    } catch (e) {
+      reject(e);
+    }
+  });
 };
 module.exports = { executePDF };
