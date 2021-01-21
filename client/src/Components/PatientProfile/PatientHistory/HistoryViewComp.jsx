ec8c20357e8421dfbdec78ec0e0b1892379f0308
suhailkhan.algaeh <suhailkhan.algaeh@gmail.com>
2021-01-21 13:25:40 +0530

    editable history and delete history

---
+++ client/src/Components/PatientProfile/PatientHistory/HistoryViewComp.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import { useForm } from "react-hook-form";
+import { newAlgaehApi } from "../../../hooks/";
+import { AlgaehMessagePop } from "algaeh-react-components";
+
+export default function HistoryViewComp({ data, remarks }) {
+  const [editable, setEditable] = useState(true);
+  // const [historyData, setHistoryData] = useState([]);
+  const { register, errors, handleSubmit } = useForm({
+    defaultValues: { historyRemarks: remarks },
+  });
+
+  const updateHistory = (e) => {
+    newAlgaehApi({
+      uri: "/doctorsWorkBench/updatePatientHistory",
+      method: "PUT",
+
+      data: {
+        remarks: e.historyRemarks,
+        hims_f_patient_history_id: data.hims_f_patient_history_id,
+      },
+    })
+      .then((res) => {
+        if (res.data.success) {
+          AlgaehMessagePop({
+            type: "info",
+            display: "Successfully updated...",
+          });
+          setEditable((pre) => !pre);
+        }
+      })
+      .catch((e) => {
+        AlgaehMessagePop({
+          type: "error",
+          display: e.message,
+        });
+      });
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit(updateHistory)}>
+        {editable ? (
+          <i
+            // key={index}
+
+            className="fas fa-pen"
+            onClick={() => {
+              setEditable(false);
+              // setHistoryData(data);
+            }}
+          ></i>
+        ) : (
+          <button
+            type="submit"
+            className="fas fa-save"
+            style={{ marginTop: 18 }}
+          />
+        )}
+        <textarea
+          name="historyRemarks"
+          ref={register({ required: true })}
+          // value={data.remarks}
+          // onChange={(e) => setSms(e.target.value)}
+          // name="sms_template"
+          disabled={editable}
+          maxLength={160}
+        />
+        {errors.historyRemarks && <p> Required Field</p>}
+      </form>
+    </div>
+  );
+}
