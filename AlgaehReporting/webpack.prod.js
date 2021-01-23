1ad7a978a7a9fbc84bec6c3efac515db3e8268f2
Syed Noor <syednoor@Syeds-MacBook-Air.local>
2021-01-24 00:06:37 +0530

    Build changes two different builds

--- AlgaehReporting/webpack.prod.js
+++ AlgaehReporting/webpack.prod.js
@@ -1,28 +1,73 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const path = require("path");
-
-module.exports = merge(common, {
-  mode: "production",
-  output: {
-    path: path.resolve(__dirname, "../Client_Builds/AlgaehReporting", "build"),
-    // path: "/Users/apple/Documents/Client/Build/App/build",
-    filename: "[name].js",
-  },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.js$/,
-  //       include: [path.resolve(__dirname, "src")],
-  //       enforce: "post",
-  //       use: {
-  //         loader: "obfuscator-loader",
-  //         options: {
-  //           compact: true,
-  //           selfDefending: false
-  //         }
-  //       }
-  //     }
-  //   ]
-  // }
-});
+const fs = require("fs-extra");
+module.exports = (env) => {
+  const cPath = "../Client_Builds/algaeh-reports";
+  const _path =
+    env && env === "Client_Builds"
+      ? path.resolve(__dirname, cPath, "build")
+      : path.resolve(__dirname, "build");
+  return {
+    ...merge(common, {
+      mode: "production",
+      output: {
+        path: _path,
+        // path: "/Users/apple/Documents/Client/Build/App/build",
+        filename: "[name].js",
+      },
+      plugins: [
+        {
+          apply: (compiler) => {
+            compiler.hooks.afterEmit.tap("AfterEmitPlugin", (compilation) => {
+              if (env === "Client_Builds") {
+                const pJSON = fs.readFileSync(
+                  path.join(__dirname, "package.json"),
+                  { encoding: "utf-8" }
+                );
+                let { devDependencies, ...packageDependencies } = JSON.parse(
+                  pJSON
+                );
+                const packagePath = path.resolve(
+                  __dirname,
+                  cPath,
+                  "package.json"
+                );
+                if (fs.existsSync(packagePath)) {
+                  fs.unlinkSync(packagePath);
+                }
+                fs.writeFileSync(
+                  packagePath,
+                  JSON.stringify(packageDependencies, null, 2),
+                  {
+                    encoding: "utf-8",
+                  }
+                );
+                fs.copySync(
+                  path.join(__dirname, "algaeh_report_tool"),
+                  path.join(__dirname, cPath, "algaeh_report_tool")
+                );
+              }
+            });
+          },
+        },
+      ],
+      // module: {
+      //   rules: [
+      //     {
+      //       test: /\.js$/,
+      //       include: [path.resolve(__dirname, "src")],
+      //       enforce: "post",
+      //       use: {
+      //         loader: "obfuscator-loader",
+      //         options: {
+      //           compact: true,
+      //           selfDefending: false
+      //         }
+      //       }
+      //     }
+      //   ]
+      // }
+    }),
+  };
+};
--- Insurance/webpack.prod.js
+++ Insurance/webpack.prod.js
@@ -1,28 +1,70 @@
 const merge = require("webpack-merge");
 const common = require("./webpack.common.js");
 const path = require("path");
+const fs = require("fs");
 
-module.exports = merge(common, {
-  mode: "production",
-  output: {
-    path: path.resolve(__dirname, "../Client_Builds/Insurance", "build"),
-    // path: "/Users/apple/Documents/Client/Build/App/build",
-    filename: "[name].js",
-  },
-  // module: {
-  //   rules: [
-  //     {
-  //       test: /\.js$/,
-  //       include: [path.resolve(__dirname, "src")],
-  //       enforce: "post",
-  //       use: {
-  //         loader: "obfuscator-loader",
-  //         options: {
-  //           compact: true,
-  //           selfDefending: false
-  //         }
-  //       }
-  //     }
-  //   ]
-  // }
-});
+module.exports = (env) => {
+  const cPath = "../Client_Builds/algaeh-insurance";
+  const _path =
+    env && env === "Client_Builds"
+      ? path.resolve(__dirname, cPath, "build")
+      : path.resolve(__dirname, "build");
+  return {
+    ...merge(common, {
+      mode: "production",
+      output: {
+        path: _path,
+        // path: "/Users/apple/Documents/Client/Build/App/build",
+        filename: "[name].js",
+      },
+      plugins: [
+        {
+          apply: (compiler) => {
+            compiler.hooks.afterEmit.tap("AfterEmitPlugin", (compilation) => {
+              if (env === "Client_Builds") {
+                const pJSON = fs.readFileSync(
+                  path.join(__dirname, "package.json"),
+                  { encoding: "utf-8" }
+                );
+                let { devDependencies, ...packageDependencies } = JSON.parse(
+                  pJSON
+                );
+                const packagePath = path.resolve(
+                  __dirname,
+                  cPath,
+                  "package.json"
+                );
+                if (fs.existsSync(packagePath)) {
+                  fs.unlinkSync(packagePath);
+                }
+                fs.writeFileSync(
+                  packagePath,
+                  JSON.stringify(packageDependencies, null, 2),
+                  {
+                    encoding: "utf-8",
+                  }
+                );
+              }
+            });
+          },
+        },
+      ],
+      // module: {
+      //   rules: [
+      //     {
+      //       test: /\.js$/,
+      //       include: [path.resolve(__dirname, "src")],
+      //       enforce: "post",
+      //       use: {
+      //         loader: "obfuscator-loader",
+      //         options: {
+      //           compact: true,
+      //           selfDefending: false
+      //         }
+      //       }
+      //     }
+      //   ]
+      // }
+    }),
+  };
+};
