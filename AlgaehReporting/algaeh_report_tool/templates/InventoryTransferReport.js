93b7fd5dd9fdcc783bf63aaaf2b086b08817477c
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2021-01-27 12:26:25 +0530

    Transfer Report with proper Grouping

--- AlgaehReporting/algaeh_report_tool/templates/InventoryTransferReport.js
+++ AlgaehReporting/algaeh_report_tool/templates/InventoryTransferReport.js
@@ -11,7 +11,7 @@ const executePDF = function executePDFMethod(options) {
 
       const params = options.args.reportParams;
 
-      params.forEach(para => {
+      params.forEach((para) => {
         input[para["name"]] = para["value"];
       });
 
@@ -19,45 +19,56 @@ const executePDF = function executePDFMethod(options) {
 
       let strQuery = "";
 
+      if (input.location_id > 0) {
+        strQuery += ` and from_location_id=?= ${input.location_id}`;
+      }
+
       options.mysql
         .executeQuery({
           query: `
-          select H.*,B.*, ROUND(B.unit_cost, 3) as unit_cost, ROUND(B.quantity_transfer * B.unit_cost, 3) as net_extended_cost,IM.item_code, IM.item_description, IU.uom_description, IL.location_description 
-          from hims_f_inventory_transfer_header H 
+          select H.*,B.*, ROUND(B.unit_cost, 3) as unit_cost, ROUND(B.quantity_transfer * B.unit_cost, 3) as net_extended_cost,
+          IM.item_code, IM.item_description, IU.uom_description, 
+          ILF.location_description as from_location,ILT.location_description as to_location
+          from hims_f_inventory_transfer_header H
           inner join hims_f_inventory_transfer_detail D on H.hims_f_inventory_transfer_header_id = D.transfer_header_id
           inner join hims_f_inventory_transfer_batches B on D.hims_f_inventory_transfer_detail_id = B.transfer_detail_id
-          inner join hims_d_inventory_item_master IM on IM.hims_d_inventory_item_master_id = D.item_id 
-          inner join hims_d_inventory_uom IU on IU.hims_d_inventory_uom_id = D.uom_transferred_id 
-          inner join hims_d_inventory_location IL on IL.hims_d_inventory_location_id = H.to_location_id 
-          where date(transfer_date)  between date(?) and date(?) and from_location_id=? ${strQuery}; `,
-          values: [input.from_date, input.to_date, input.location_id],
-          printQuery: true
+          inner join hims_d_inventory_item_master IM on IM.hims_d_inventory_item_master_id = D.item_id
+          inner join hims_d_inventory_uom IU on IU.hims_d_inventory_uom_id = D.uom_transferred_id
+          inner join hims_d_inventory_location ILF on ILF.hims_d_inventory_location_id = H.from_location_id
+          inner join hims_d_inventory_location ILT on ILT.hims_d_inventory_location_id = H.to_location_id 
+          where date(transfer_date)  between date(?) and date(?)  ${strQuery}; `,
+          values: [input.from_date, input.to_date],
+          printQuery: true,
         })
-        .then(result => {
+        .then((result) => {
           options.mysql.releaseConnection();
 
           if (result.length > 0) {
             const item_details = _.chain(result)
-              .groupBy(g => g.hims_f_inventory_transfer_header_id)
-              .map(m => {
+              .groupBy((g) => g.hims_f_inventory_transfer_header_id)
+              .map((m) => {
                 return {
                   transfer_number: m[0].transfer_number,
-                  transfer_date: moment(m[0].transfer_date).format("DD/MM/YYYY"),
-                  transfer_items: m
+                  from_location: m[0].from_location,
+                  to_location: m[0].to_location,
+                  transfer_date: moment(m[0].transfer_date).format(
+                    "DD/MM/YYYY"
+                  ),
+                  transfer_items: m,
                 };
               })
               .value();
 
             resolve({
-              result: item_details
+              result: item_details,
             });
           } else {
             resolve({
-              result: result
+              result: result,
             });
           }
         })
-        .catch(e => {
+        .catch((e) => {
           console.log("e:", e);
           options.mysql.releaseConnection();
           reject(e);
