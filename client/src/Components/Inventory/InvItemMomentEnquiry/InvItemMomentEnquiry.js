67c89da4d440bd21dd3b92806c2ba6e3e6768616
Nowshad <nowshad.algaeh@gmail.com>
2021-02-15 16:19:57 +0530

    Average cost added in item moment enquiry in inventory and pharmacy

--- client/src/Components/Inventory/InvItemMomentEnquiry/InvItemMomentEnquiry.js
+++ client/src/Components/Inventory/InvItemMomentEnquiry/InvItemMomentEnquiry.js
@@ -29,6 +29,7 @@ import {
   // AlgaehTable,
   Tooltip,
 } from "algaeh-react-components";
+import { GetAmountFormart } from "../../../utils/GlobalFunctions";
 
 class InvItemMomentEnquiry extends Component {
   constructor(props) {
@@ -269,32 +270,32 @@ class InvItemMomentEnquiry extends Component {
                         return row.transaction_type === "MR"
                           ? "Material Requisition"
                           : row.transaction_type === "ST"
-                          ? "Stock Transfer"
-                          : row.transaction_type === "POS"
-                          ? "Point of Sale"
-                          : row.transaction_type === "SRT"
-                          ? "Sales Return"
-                          : row.transaction_type === "INT"
-                          ? "Opening Stock"
-                          : row.transaction_type === "CS"
-                          ? "Consumption"
-                          : row.transaction_type === "CSN"
-                          ? "Consumption Cancel"
-                          : row.transaction_type === "REC"
-                          ? "Receipt"
-                          : row.transaction_type === "PO"
-                          ? "Purchase Order"
-                          : row.transaction_type === "DNA"
-                          ? "Delivery Note"
-                          : row.transaction_type === "ACK"
-                          ? "Transfer Acknowledge"
-                          : row.transaction_type === "PR"
-                          ? "Purchase Return"
-                          : row.transaction_type === "AD"
-                          ? "Stock Adjustment"
-                          : row.transaction_type === "SDN"
-                          ? "Sales Dispatch Note"
-                          : "";
+                            ? "Stock Transfer"
+                            : row.transaction_type === "POS"
+                              ? "Point of Sale"
+                              : row.transaction_type === "SRT"
+                                ? "Sales Return"
+                                : row.transaction_type === "INT"
+                                  ? "Opening Stock"
+                                  : row.transaction_type === "CS"
+                                    ? "Consumption"
+                                    : row.transaction_type === "CSN"
+                                      ? "Consumption Cancel"
+                                      : row.transaction_type === "REC"
+                                        ? "Receipt"
+                                        : row.transaction_type === "PO"
+                                          ? "Purchase Order"
+                                          : row.transaction_type === "DNA"
+                                            ? "Delivery Note"
+                                            : row.transaction_type === "ACK"
+                                              ? "Transfer Acknowledge"
+                                              : row.transaction_type === "PR"
+                                                ? "Purchase Return"
+                                                : row.transaction_type === "AD"
+                                                  ? "Stock Adjustment"
+                                                  : row.transaction_type === "SDN"
+                                                    ? "Sales Dispatch Note"
+                                                    : "";
                       },
                       others: {
                         filterable: false,
@@ -324,10 +325,10 @@ class InvItemMomentEnquiry extends Component {
                           this.props.inventorylocations === undefined
                             ? []
                             : this.props.inventorylocations.filter(
-                                (f) =>
-                                  f.hims_d_inventory_location_id ===
-                                  row.from_location_id
-                              );
+                              (f) =>
+                                f.hims_d_inventory_location_id ===
+                                row.from_location_id
+                            );
 
                         return (
                           <span>
@@ -351,10 +352,10 @@ class InvItemMomentEnquiry extends Component {
                           this.props.inventoryitemlist === undefined
                             ? []
                             : this.props.inventoryitemlist.filter(
-                                (f) =>
-                                  f.hims_d_inventory_item_master_id ===
-                                  row.item_code_id
-                              );
+                              (f) =>
+                                f.hims_d_inventory_item_master_id ===
+                                row.item_code_id
+                            );
 
                         return (
                           <span>
@@ -367,13 +368,13 @@ class InvItemMomentEnquiry extends Component {
                                   row.operation === "+"
                                     ? "fas fa-arrow-up green"
                                     : row.operation === "-"
-                                    ? "fas fa-arrow-down red"
-                                    : ""
+                                      ? "fas fa-arrow-down red"
+                                      : ""
                                 }
                               />
                             ) : (
-                              ""
-                            )}
+                                ""
+                              )}
                           </span>
                         );
                       },
@@ -390,10 +391,10 @@ class InvItemMomentEnquiry extends Component {
                           this.props.inventoryitemuom === undefined
                             ? []
                             : this.props.inventoryitemuom.filter(
-                                (f) =>
-                                  f.hims_d_inventory_uom_id ===
-                                  row.transaction_uom
-                              );
+                              (f) =>
+                                f.hims_d_inventory_uom_id ===
+                                row.transaction_uom
+                            );
 
                         return (
                           <span>
@@ -458,21 +459,21 @@ class InvItemMomentEnquiry extends Component {
                         filterable: false,
                       },
                     },
-                    // {
-                    //   fieldName: "average_cost",
-                    //   label: (
-                    //     <AlgaehLabel label={{ forceLabel: "Average Cost" }} />
-                    //   ),
-                    //   displayTemplate: row => {
-                    //     return (
-                    //       <span>
-                    //         {getAmountFormart(row.average_cost, {
-                    //           appendSymbol: false
-                    //         })}
-                    //       </span>
-                    //     );
-                    //   }
-                    // }
+                    {
+                      fieldName: "average_cost",
+                      label: (
+                        <AlgaehLabel label={{ forceLabel: "Average Cost" }} />
+                      ),
+                      displayTemplate: row => {
+                        return (
+                          <span>
+                            {GetAmountFormart(row.average_cost, {
+                              appendSymbol: false
+                            })}
+                          </span>
+                        );
+                      }
+                    }
                   ]}
                   keyId="item_id"
                   dataSource={{
