e3ce4e97f29b903373619fb5bcb14949b2297ce2
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2021-02-23 16:06:55 +0530

    Process Account UI Corrected

--- algaeh-finance/client/src/Components/ProcessAccountingEntry/index.js
+++ algaeh-finance/client/src/Components/ProcessAccountingEntry/index.js
@@ -62,128 +62,125 @@ export default memo(function ProcessAccountingEntry(props) {
   }
 
   return (
-    <div className="row">
-      <div className="col-12">
-        <div className="portlet portlet-bordered margin-bottom-15">
-          <div className="portlet-title">
-            <AlgaehAutoComplete
-              div={{
-                className: "col form-group mandatory",
-              }}
-              label={{
-                forceLabel: "Select Transation",
-                isImp: true,
-              }}
-              selector={{
-                dataSource: {
-                  data: [
-                    { text: "Billing", value: "1" },
-                    { text: "Bill Cancellation", value: "2" },
-                  ],
-                  valueField: "value",
-                  textField: "text",
-                },
-                value: transation_type,
+    <div className="counter">
+      <div className="row inner-top-search">
+        <AlgaehAutoComplete
+          div={{
+            className: "col-3 form-group mandatory",
+          }}
+          label={{
+            forceLabel: "Select Transation",
+            isImp: true,
+          }}
+          selector={{
+            dataSource: {
+              data: [
+                { text: "Billing", value: "1" },
+                { text: "Bill Cancellation", value: "2" },
+              ],
+              valueField: "value",
+              textField: "text",
+            },
+            value: transation_type,
 
-                onChange: (selected) => {
-                  setTransationType(selected.value);
+            onChange: (selected) => {
+              setTransationType(selected.value);
+              setDateRange(previousMonthDate);
+              setBillNumber(null);
+            },
+            onClear: () => {
+              setTransationType(null);
+              setDateRange(previousMonthDate);
+              setBillNumber(null);
+            },
+          }}
+        />
+
+        <div className="col-3">
+          <label>Transation Type</label>
+          <div className="customRadio">
+            <label className="radio inline">
+              <input
+                type="radio"
+                value="P"
+                name="cost_center_type"
+                onChange={(e) => {
+                  setTransationWise("D");
                   setDateRange(previousMonthDate);
                   setBillNumber(null);
-                },
-                onClear: () => {
-                  setTransationType(null);
+                }}
+                checked={transation_wise === "D"}
+              />
+              <span>Date Wise</span>
+            </label>
+
+            <label className="radio inline">
+              <input
+                type="radio"
+                value="SD"
+                onChange={(e) => {
+                  setTransationWise("T");
                   setDateRange(previousMonthDate);
                   setBillNumber(null);
-                },
-              }}
-            />
-
-            <div className="col">
-              <label>Transation Type</label>
-              <div className="customRadio">
-                <label className="radio inline">
-                  <input
-                    type="radio"
-                    value="P"
-                    name="cost_center_type"
-                    onChange={(e) => {
-                      setTransationWise("D");
-                      setDateRange(previousMonthDate);
-                      setBillNumber(null);
-                    }}
-                    checked={transation_wise === "D"}
-                  />
-                  <span>Date Wise</span>
-                </label>
-
-                <label className="radio inline">
-                  <input
-                    type="radio"
-                    value="SD"
-                    onChange={(e) => {
-                      setTransationWise("T");
-                      setDateRange(previousMonthDate);
-                      setBillNumber(null);
-                    }}
-                    name="cost_center_type"
-                    checked={transation_wise === "T"}
-                  />
-                  <span>Transation Wise</span>
-                </label>
-              </div>
-            </div>
-
-            {transation_wise === "D" ? (
-              <AlgaehDateHandler
-                type={"range"}
-                div={{
-                  className: "col form-group mandatory",
                 }}
-                label={{
-                  forceLabel: "Select Date Range",
-                }}
-                textBox={{
-                  name: "selectRange",
-                  value: dateRange,
-                }}
-                maxDate={moment().add(1, "days")}
-                events={{
-                  onChange: (dateSelected) => {
-                    setDateRange(dateSelected);
-                  },
-                }}
+                name="cost_center_type"
+                checked={transation_wise === "T"}
               />
-            ) : (
-              <AlgaehFormGroup
-                div={{
-                  className: "col form-group  mandatory",
-                }}
-                label={{
-                  forceLabel: "Bill Number",
-                  isImp: true,
-                }}
-                textBox={{
-                  type: "text",
-                  value: bill_number,
-                  className: "form-control",
-                  id: "name",
-                  onChange: (e) => {
-                    setBillNumber(e.target.value);
-                  },
-                  autoComplete: false,
-                }}
-              />
-            )}
-
-            <AlgaehButton
-              className="btn btn-primary"
-              loading={loading}
-              onClick={onClickSendSelected}
-            >
-              Generate
-            </AlgaehButton>
+              <span>Transation Wise</span>
+            </label>
           </div>
         </div>
+
+        {transation_wise === "D" ? (
+          <AlgaehDateHandler
+            type={"range"}
+            div={{
+              className: "col-3 form-group mandatory",
+            }}
+            label={{
+              forceLabel: "Select Date Range",
+            }}
+            textBox={{
+              name: "selectRange",
+              value: dateRange,
+            }}
+            maxDate={moment().add(1, "days")}
+            events={{
+              onChange: (dateSelected) => {
+                setDateRange(dateSelected);
+              },
+            }}
+          />
+        ) : (
+          <AlgaehFormGroup
+            div={{
+              className: "col-2 form-group  mandatory",
+            }}
+            label={{
+              forceLabel: "Bill Number",
+              isImp: true,
+            }}
+            textBox={{
+              type: "text",
+              value: bill_number,
+              className: "form-control",
+              id: "name",
+              onChange: (e) => {
+                setBillNumber(e.target.value);
+              },
+              autoComplete: false,
+            }}
+          />
+        )}
+
+        <AlgaehButton
+          className="btn btn-default"
+          style={{ marginTop: 17 }}
+          loading={loading}
+          onClick={onClickSendSelected}
+        >
+          Generate
+        </AlgaehButton>
       </div>
     </div>
   );
