b5c43ef404b520db3108a094d00e125c1366a359
Nowshad <nowshad.algaeh@gmail.com>
2021-03-16 17:20:18 +0530

    Accounts opening balance excel export partally done

--- algaeh-finance/client/src/Components/FinanceAccounts/FinanceAccounts.js
+++ algaeh-finance/client/src/Components/FinanceAccounts/FinanceAccounts.js
@@ -1,6 +1,9 @@
 import React from "react";
 import TreeComponent from "./TreeComponent";
 import { AlgaehTabs, AlgaehLabel } from "algaeh-react-components";
+import { algaehApiCall, swalMessage } from "../../utils/algaehApiCall";
+import moment from "moment";
+
 export default function FinanceAccounts({ inDrawer = false }) {
   const TABS = [
     { label: "assets", assetCode: 1 },
@@ -29,9 +32,62 @@ export default function FinanceAccounts({ inDrawer = false }) {
     };
   });
 
+  function exportExcelAccountOB() {
+    debugger;
+    algaehApiCall({
+      uri: "/finance/getAccountsExport",
+      method: "GET",
+      module: "finance",
+      onSuccess: (response) => {
+        let blob = new Blob([response.data], {
+          type: "application/octet-stream",
+        });
+
+        var objectUrl = URL.createObjectURL(blob);
+        var link = document.createElement("a");
+        link.setAttribute("href", objectUrl);
+        link.setAttribute(
+          "download",
+          `AccountsOpeningBalance-${moment(new Date()).format(
+            "YYYY-MM-DD"
+          )}.xlsx`
+        );
+        link.click();
+        // AlgaehLoader({ show: false });
+      },
+      onCatch: (error) => {
+        var reader = new FileReader();
+        reader.onload = function () {
+          // AlgaehLoader({ show: false });
+          const parse = JSON.parse(reader.result);
+
+          swalMessage({
+            type: "error",
+            title: parse !== undefined ? parse.result.message : parse,
+          });
+        };
+        reader.readAsText(error.response.data);
+      },
+    });
+  }
+
   return (
     <div className="">
       <AlgaehTabs content={content} />
+
+      <div className="hptl-phase1-footer">
+        <div className="row">
+          <div className="col-lg-12">
+            <button
+              type="button"
+              className="btn btn-primary"
+              onClick={exportExcelAccountOB}
+            >
+              <AlgaehLabel label={{ forceLabel: "Export" }} />
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
