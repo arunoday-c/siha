c54c73bb0d73683b18ea9cbcb1149839b0bacee7
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2021-03-18 10:16:56 +0530

    PCR Report changes with condition of primary report

--- AlgaehReporting/algaeh_report_tool/templates/PCRTestReport.js
+++ AlgaehReporting/algaeh_report_tool/templates/PCRTestReport.js
@@ -25,11 +25,11 @@ const executePDF = function executePDFMethod(options) {
           P.secondary_id_no,IDD.identity_document_name as secondaryIDName,IDD.arabic_identity_document_name  as secondaryAraIDName,
           REF.institute_name, REF.tel_code, REF.contact_number, REF.inCharge_name
           from hims_f_patient P
-          inner join hims_d_nationality N on N.hims_d_nationality_id = P.nationality_id
-          inner join hims_d_referring_institute REF on REF.hims_d_referring_institute_id = P.referring_institute_id
-          inner join hims_f_patient_visit V on P.hims_d_patient_id = V.patient_id
-          inner join hims_d_employee E on E.hims_d_employee_id = V.doctor_id
-          inner join hims_d_sub_department SD on E.sub_department_id=SD.hims_d_sub_department_id
+          left join hims_d_nationality N on N.hims_d_nationality_id = P.nationality_id
+          left join hims_d_referring_institute REF on REF.hims_d_referring_institute_id = P.referring_institute_id
+          left join hims_f_patient_visit V on P.hims_d_patient_id = V.patient_id
+          left join hims_d_employee E on E.hims_d_employee_id = V.doctor_id
+          left join hims_d_sub_department SD on E.sub_department_id=SD.hims_d_sub_department_id
           left join hims_m_patient_insurance_mapping IM on IM.patient_visit_id=V.hims_f_patient_visit_id
           left join hims_d_insurance_provider IP on IM.primary_insurance_provider_id=IP.hims_d_insurance_provider_id
           left join hims_d_identity_document ID on ID.hims_d_identity_document_id = P.primary_identity_id
@@ -44,16 +44,16 @@ const executePDF = function executePDFMethod(options) {
           S.service_name, E.full_name as validated_by,OA.critical_type, TC.category_name, OA.text_value, OA.analyte_type,
           CASE WHEN OA.analyte_type = 'QU' THEN OA.normal_qualitative_value WHEN OA.analyte_type = 'T' THEN OA.text_value ELSE CONCAT(TRIM(TRAILING '.' FROM TRIM(TRAILING '0' from OA.normal_low)), '-',  TRIM(TRAILING '.' FROM TRIM(TRAILING '0' from OA.normal_high))) END AS analyte_ranges
           from hims_f_lab_order LO
-          inner join hims_f_lab_sample LS on LO.hims_f_lab_order_id = LS.order_id
-          inner join hims_f_ord_analytes OA on LO.hims_f_lab_order_id = OA.order_id
-          inner join hims_d_lab_specimen MS on LS.sample_id = MS.hims_d_lab_specimen_id
-          inner join hims_d_lab_analytes LA on OA.analyte_id = LA.hims_d_lab_analytes_id
-          inner join hims_d_services S on S.hims_d_services_id= LO.service_id
-          inner join algaeh_d_app_user U on LO.validated_by=U.algaeh_d_app_user_id
-          inner join hims_d_employee E on  U.employee_id=E.hims_d_employee_id
-          inner join hims_d_investigation_test IT on IT.services_id= LO.service_id
-          inner join hims_d_test_category TC on TC.hims_d_test_category_id= IT.category_id
-          inner join hims_m_lab_analyte LM on LM.analyte_id = LA.hims_d_lab_analytes_id and LM.test_id = LO.test_id
+          left join hims_f_lab_sample LS on LO.hims_f_lab_order_id = LS.order_id
+          left join hims_f_ord_analytes OA on LO.hims_f_lab_order_id = OA.order_id
+          left join hims_d_lab_specimen MS on LS.sample_id = MS.hims_d_lab_specimen_id
+          left join hims_d_lab_analytes LA on OA.analyte_id = LA.hims_d_lab_analytes_id
+          left join hims_d_services S on S.hims_d_services_id= LO.service_id
+          left join algaeh_d_app_user U on LO.validated_by=U.algaeh_d_app_user_id
+          left join hims_d_employee E on  U.employee_id=E.hims_d_employee_id
+          left join hims_d_investigation_test IT on IT.services_id= LO.service_id
+          left join hims_d_test_category TC on TC.hims_d_test_category_id= IT.category_id
+          left join hims_m_lab_analyte LM on LM.analyte_id = LA.hims_d_lab_analytes_id and LM.test_id = LO.test_id
           where LO.visit_id = ? and LO.hims_f_lab_order_id=? group by LA.hims_d_lab_analytes_id order by hims_f_ord_analytes_id;
           select H.hospital_name,H.hospital_address,H.arabic_hospital_name, 
           O.organization_name,O.business_registration_number,O.legal_name,O.tax_number,O.address1,O.address2 ,
