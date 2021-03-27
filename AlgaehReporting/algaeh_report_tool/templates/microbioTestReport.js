778f610da8faadc409d81b640b33964eb4fa54e9
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2021-03-27 13:59:37 +0530

    Microbiology Report Changes

--- AlgaehReporting/algaeh_report_tool/templates/microbioTestReport.js
+++ AlgaehReporting/algaeh_report_tool/templates/microbioTestReport.js
@@ -18,14 +18,20 @@ const executePDF = function executePDFMethod(options) {
       options.mysql
         .executeQuery({
           query: `
-          select P.patient_code,trim(E.full_name) as doctor_name,P.full_name as patient_name from hims_f_patient P    
-          inner join hims_f_patient_visit V on P.hims_d_patient_id = V.patient_id    
-          inner join hims_d_employee E on E.hims_d_employee_id = V.doctor_id   
+          select P.patient_code,trim(E.full_name) as doctor_name,P.full_name as patient_name,SD.sub_department_name,
+          gender, age_in_years,   age_in_months,age_in_days, IP.insurance_provider_name, P.primary_id_no
+          from hims_f_patient P
+          inner join hims_f_patient_visit V on P.hims_d_patient_id = V.patient_id
+          inner join hims_d_employee E on E.hims_d_employee_id = V.doctor_id
+          inner join hims_d_sub_department SD on E.sub_department_id=SD.hims_d_sub_department_id
+          left join hims_m_patient_insurance_mapping IM on IM.patient_visit_id=V.hims_f_patient_visit_id
+          left join hims_d_insurance_provider IP on IM.primary_insurance_provider_id=IP.hims_d_insurance_provider_id   
           where P.hims_d_patient_id=? and V.hims_f_patient_visit_id=?;  
+          
           select MS.hims_d_lab_specimen_id,MS.description as  specimen,S.service_name , A.antibiotic_name, 
-          case MR.susceptible when 'Y' then 'YES' else 'NO' end as  susceptible, 
-          case MR.intermediate when 'Y' then 'YES' else 'NO' end as  intermediate, 
-          case MR.resistant when 'Y' then 'YES' else 'NO' end as  resistant, 
+          MR.susceptible,case MR.susceptible when 'Y' then 'YES' else 'NO' end as  susceptibleText,
+          MR.intermediate,case MR.intermediate when 'Y' then 'YES' else 'NO' end as  intermediateText,
+          MR.resistant, case MR.resistant when 'Y' then 'YES' else 'NO' end as  resistantText, 
           CASE WHEN LO.organism_type='F' THEN 'Fascideous' else 'Non-Fascideous' END as organism_type, 
           CASE WHEN LO.bacteria_type='G' THEN 'Growth' else 'No Growth' END as bacteria_type,LO.bacteria_name, 
           E.full_name as validated_by, LO.ordered_date,LO.entered_date,LO.validated_date,LO.comments 
@@ -42,36 +48,16 @@ const executePDF = function executePDFMethod(options) {
         })
         .then((res) => {
           options.mysql.releaseConnection();
-
-          // console.log("header---", header);
-          const result = res;
-
-          if (result[0].length > 0) {
-            resolve({
-              patient_code: result[0][0]["patient_code"],
-              patient_name: result[0][0]["patient_name"],
-              doctor_name: result[0][0]["doctor_name"],
-              ordered_date: result[1][0]["ordered_date"],
-              entered_date: result[1][0]["entered_date"],
-              validated_date: result[1][0]["validated_date"],
-              specimen: result[1][0]["specimen"],
-              service_name: result[1][0]["service_name"],
-              bacteria_type: result[1][0]["bacteria_type"],
-              organism_type: result[1][0]["organism_type"],
-              bacteria_name: result[1][0]["bacteria_name"],
-              validated_by: result[1][0]["validated_by"],
-              comments: result[1][0]["comments"],
-              details: result[1],
-            });
-            // console.log(result);
-          } else {
-            resolve({
-              header: header,
-              // no_employees: result.length,
-              result: result,
-            });
-            // console.log(result);
-          }
+          const header = {
+            ..._.head(res[0]),
+          };
+          const result = res[1];
+          resolve({
+            header: header,
+            result: result,
+          });
+          console.log("header---", header);
+          console.log("result---", result);
         })
         .catch((e) => {
           options.mysql.releaseConnection();
@@ -83,29 +69,3 @@ const executePDF = function executePDFMethod(options) {
   });
 };
 module.exports = { executePDF };
-
-// const executePDF = function executePDFMethod(options) {
-//   return new Promise(function (resolve, reject) {
-//     try {
-//       resolve({
-//         patient_code: options.result[0][0]["patient_code"],
-//         patient_name: options.result[0][0]["patient_name"],
-//         doctor_name: options.result[0][0]["doctor_name"],
-//         ordered_date: options.result[1][0]["ordered_date"],
-//         entered_date: options.result[1][0]["entered_date"],
-//         validated_date: options.result[1][0]["validated_date"],
-//         specimen: options.result[1][0]["specimen"],
-//         service_name: options.result[1][0]["service_name"],
-//         bacteria_type: options.result[1][0]["bacteria_type"],
-//         organism_type: options.result[1][0]["organism_type"],
-//         bacteria_name: options.result[1][0]["bacteria_name"],
-//         validated_by: options.result[1][0]["validated_by"],
-//         comments: options.result[1][0]["comments"],
-//         details: options.result[1],
-//       });
-//     } catch (e) {
-//       reject(e);
-//     }
-//   });
-// };
-// module.exports = { executePDF };
