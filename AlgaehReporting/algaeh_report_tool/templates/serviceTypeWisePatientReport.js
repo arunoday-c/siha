ceb5e7b4be80f00c9c99a351d554d41b3a79b072
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2021-05-06 13:06:50 +0530

    Service Type and Service wise patient report

--- AlgaehReporting/algaeh_report_tool/templates/serviceTypeWisePatientReport.js
+++ AlgaehReporting/algaeh_report_tool/templates/serviceTypeWisePatientReport.js
@@ -17,38 +17,37 @@ const executePDF = function executePDFMethod(options) {
       params.forEach((para) => {
         input[para["name"]] = para["value"];
       });
-      console.log("input=", input);
+
       let strData = "";
 
       if (input.nationality_id > 0) {
         strData += ` and P.nationality_id= ${input.nationality_id}`;
       }
-      if (input.item_id > 0) {
-        strData += ` and BD.services_id= ${input.item_id}`;
+      if (input.service_type_id > 0) {
+        strData += ` and BD.service_type_id= ${input.service_type_id}`;
       }
 
       options.mysql
         .executeQuery({
-          query: `
-          SELECT BD.services_id,BH.bill_date,BH.bill_number,V.visit_date, P.hims_d_patient_id,P.full_name, P.patient_code,  N.nationality,
+          query: `SELECT BH.bill_number,BH.bill_date, P.hims_d_patient_id,P.full_name, P.patient_code,  N.nationality,
           S.service_code,S.service_name
-          FROM hims_f_billing_details as BD
-          left join hims_f_billing_header BH on BD.hims_f_billing_header_id = BH.hims_f_billing_header_id
-          inner join hims_f_patient P on P.hims_d_patient_id = BH.patient_id
-          inner join  hims_f_patient_visit V on BH.visit_id = hims_f_patient_visit_id
-          inner join hims_d_nationality N on N.hims_d_nationality_id = P.nationality_id
-          inner join hims_d_services S on BD.services_id=S.hims_d_services_id
-          where BD.cancel_yes_no='N' and adjusted='N' and date(BH.bill_date) between date(?) and date(?) and BH.hospital_id=? ${strData};`,
+              FROM hims_f_billing_details as BD
+              left join hims_f_billing_header BH on BD.hims_f_billing_header_id = BH.hims_f_billing_header_id
+              inner join hims_f_patient P on P.hims_d_patient_id = BH.patient_id
+              inner join  hims_f_patient_visit V on BH.visit_id = hims_f_patient_visit_id
+              inner join hims_d_nationality N on N.hims_d_nationality_id = P.nationality_id
+              inner join hims_d_services S on BD.services_id=S.hims_d_services_id
+             where BD.cancel_yes_no='N' and adjusted='N' and date(BH.bill_date) between date(?) and date(?) and BH.hospital_id=? ${strData};`,
           values: [input.from_date, input.to_date, input.hospital_id],
           printQuery: true,
         })
         .then((ress) => {
           const BillWise = _.chain(ress)
-            .groupBy((g) => g.services_id)
+            .groupBy((g) => g.hims_d_patient_id)
             .map((billNo) => {
-              const { service_name } = billNo[0];
+              const { full_name, patient_code, nationality } = billNo[0];
               const patientList = _.chain(billNo)
-                .groupBy((g) => g.services_id)
+                .groupBy((g) => g.patient_id)
                 .map((docs) => {
                   // const { full_name, patient_code } = docs[0];
                   return {
@@ -63,7 +62,9 @@ const executePDF = function executePDFMethod(options) {
                 .value();
 
               return {
-                service_name,
+                full_name,
+                patient_code,
+                nationality,
                 patientList: patientList,
               };
             })
