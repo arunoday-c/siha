3aa98e7af12efbb679ac50be5e73e5badc3245d2
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2021-06-05 15:45:52 +0530

    JV report changes to show Insurance Customer

--- AlgaehReporting/algaeh_report_tool/templates/JVReport_receipt.js
+++ AlgaehReporting/algaeh_report_tool/templates/JVReport_receipt.js
@@ -16,21 +16,26 @@ const executePDF = function executePDFMethod(options) {
       console.log("input===", input);
 
       if (input.receipt_type === "S") {
-        str = `SELECT IVH.invoice_number,
-        IVH.invoice_date,IVH.sales_invoice_mode,IVH.sales_order_id,IVH.customer_id,
+        str = `SELECT 
+        COALESCE(IVH.invoice_number,INS.insurance_statement_number) as invoice_number,
+        COALESCE(IVH.invoice_date,INS.created_date) as invoice_date,
+        IVH.sales_invoice_mode,
         IVH.sub_total,IVH.net_total,IVH.discount_amount,IVH.total_tax,IVH.net_payable,
         IVH.narration as invNarration,IVH.hospital_id
         FROM finance_voucher_header VD
-        inner join hims_f_sales_invoice_header IVH on IVH.invoice_number = VD.invoice_ref_no
+        left join hims_f_sales_invoice_header IVH on IVH.invoice_number = VD.invoice_ref_no
+        left join hims_f_insurance_statement INS on INS.insurance_statement_number = VD.invoice_ref_no
         where VD.finance_voucher_header_id=?;`;
       } else {
-        str = `select IVH.invoice_number,
-        IVH.invoice_date,IVH.sales_invoice_mode,IVH.sales_order_id,IVH.customer_id,
+        str = `select COALESCE(IVH.invoice_number,INS.insurance_statement_number) as invoice_number,
+        COALESCE(IVH.invoice_date,INS.created_date) as invoice_date,
+        IVH.sales_invoice_mode,
         FH.amount as sub_total, FSH.amount as net_total,FSH.amount as net_payable,
         FSH.invoice_ref_no
         from finance_voucher_sub_header FSH
         inner join finance_voucher_header FH on FH.invoice_no in ( FSH.invoice_ref_no)
-        inner join hims_f_sales_invoice_header IVH on IVH.invoice_number = FSH.invoice_ref_no
+        left join hims_f_sales_invoice_header IVH on IVH.invoice_number = FSH.invoice_ref_no
+        left join hims_f_insurance_statement INS on INS.insurance_statement_number = FSH.invoice_ref_no
         left join finance_voucher_sub_header FSHH on FSHH.finance_voucher_header_id<? and FSHH.invoice_ref_no in ( FSH.invoice_ref_no)
         where FSH.finance_voucher_header_id=? and FH.voucher_type='sales' group by FSH.finance_voucher_sub_header_id;`;
       }
@@ -39,15 +44,19 @@ const executePDF = function executePDFMethod(options) {
         .executeQuery({
           query:
             `SELECT VD.voucher_type,VD.voucher_no,VD.amount,VD.payment_mode,VD.payment_date,
-          VD.narration,VD.due_date,VD.settled_amount,VD.created_by,IVH.customer_id,IVH.sales_order_id,
-          CO.customer_name,CO.customer_code,CO.contact_number,CO.address,CO.vat_number,
-          VDD.payment_type,VDD.head_id,VDD.child_id,FCD.head_id,FCD.finance_account_child_id,FCD.child_name,FCD.arabic_child_name
-          FROM finance_voucher_header VD
-          inner join hims_f_sales_invoice_header IVH on IVH.invoice_number = VD.invoice_ref_no
-          inner join hims_d_customer CO on CO.hims_d_customer_id = IVH.customer_id
-          inner join finance_voucher_details VDD on VDD.voucher_header_id = VD.finance_voucher_header_id and VDD.payment_type='DR'
-          inner join finance_account_child FCD on FCD.finance_account_child_id = VDD.child_id and FCD.head_id = VDD.head_id
-          where VD.finance_voucher_header_id=?;` + str,
+            VD.narration,VD.due_date,VD.settled_amount,VD.created_by,
+            CO.contact_number,CO.address,CO.vat_number,
+            COALESCE(CO.customer_name,ISB.insurance_sub_name) as customer_name,
+            COALESCE(CO.customer_code,ISB.insurance_sub_code) as customer_code,
+            VDD.payment_type,VDD.head_id,VDD.child_id,FCD.head_id,FCD.finance_account_child_id,FCD.child_name,FCD.arabic_child_name
+            FROM finance_voucher_header VD
+            left join hims_f_sales_invoice_header IVH on IVH.invoice_number = VD.invoice_ref_no
+            left join hims_d_customer CO on CO.hims_d_customer_id = IVH.customer_id
+            left join hims_f_insurance_statement INS on INS.insurance_statement_number = VD.invoice_ref_no
+            left join hims_d_insurance_sub ISB on ISB.hims_d_insurance_sub_id = INS.sub_insurance_id
+            inner join finance_voucher_details VDD on VDD.voucher_header_id = VD.finance_voucher_header_id and VDD.payment_type='DR'
+            inner join finance_account_child FCD on FCD.finance_account_child_id = VDD.child_id and FCD.head_id = VDD.head_id
+            where VD.finance_voucher_header_id=?;` + str,
           values: [
             input.voucher_header_id,
             input.voucher_header_id,
