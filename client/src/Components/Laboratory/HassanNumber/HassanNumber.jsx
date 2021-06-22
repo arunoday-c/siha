fe7fdd98c3485cb04efe8840fb00d7f460847251
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2021-06-22 19:27:19 +0530

    Corrected Hassan Updated by name

--- client/src/Components/Laboratory/HassanNumber/HassanNumber.jsx
+++ client/src/Components/Laboratory/HassanNumber/HassanNumber.jsx
@@ -71,7 +71,7 @@ function HassanNumber() {
     getLabOrderedServices,
     {
       onSuccess: (data) => {
-        debugger;
+        // debugger;
       },
       onError: (err) => {
         AlgaehMessagePop({
@@ -231,7 +231,7 @@ function HassanNumber() {
           <div className="portlet portlet-bordered margin-bottom-15">
             <div className="portlet-title">
               <div className="caption">
-                <h3 className="caption-subject">Result Entry List</h3>
+                <h3 className="caption-subject">Hassan PCR List</h3>
               </div>
             </div>
 
@@ -254,7 +254,7 @@ function HassanNumber() {
                     filterable: true,
                     others: {
                       maxWidth: 150,
-                      resizable: false,
+                      // resizable: false,
                       style: { textAlign: "center" },
                     },
                     editorTemplate: (row) => {
@@ -272,7 +272,7 @@ function HassanNumber() {
                     disabled: true,
                     others: {
                       maxWidth: 130,
-                      resizable: false,
+                      // resizable: false,
                       style: { textAlign: "center" },
                     },
                     editorTemplate: (row) => {
@@ -289,7 +289,7 @@ function HassanNumber() {
                     disabled: false,
                     others: {
                       maxWidth: 150,
-                      resizable: false,
+                      // resizable: false,
                       style: { textAlign: "center" },
                     },
                     editorTemplate: (row) => {
@@ -306,7 +306,7 @@ function HassanNumber() {
                     disabled: false,
                     others: {
                       maxWidth: 150,
-                      resizable: false,
+                      // resizable: false,
                       style: { textAlign: "center" },
                     },
                     editorTemplate: (row) => {
@@ -322,28 +322,14 @@ function HassanNumber() {
                     filterable: true,
                     disabled: true,
                     others: {
-                      resizable: false,
+                      // resizable: false,
                       style: { textAlign: "left" },
                     },
                     editorTemplate: (row) => {
                       return row.full_name;
                     },
                   },
-                  {
-                    fieldName: "service_name",
-                    label: <AlgaehLabel label={{ forceLabel: "Test Name" }} />,
 
-                    disabled: true,
-                    sortable: true,
-                    filterable: true,
-                    others: {
-                      resizable: false,
-                      style: { textAlign: "center" },
-                    },
-                    editorTemplate: (row) => {
-                      return row.service_name;
-                    },
-                  },
                   {
                     fieldName: "hassan_number",
                     label: <AlgaehLabel label={{ forceLabel: "Hassan No." }} />,
@@ -351,8 +337,9 @@ function HassanNumber() {
                     filterable: true,
                     disabled: true,
                     others: {
-                      resizable: false,
+                      // resizable: false,
                       style: { textAlign: "center" },
+                      maxWidth: 150,
                     },
                     editorTemplate: (row) => {
                       return (
@@ -381,16 +368,12 @@ function HassanNumber() {
 
                   {
                     fieldName: "haasan_updated_by_name",
-                    label: (
-                      <AlgaehLabel
-                        label={{ forceLabel: "Hassan No. Updated By" }}
-                      />
-                    ),
+                    label: <AlgaehLabel label={{ forceLabel: "Updated By" }} />,
                     sortable: true,
                     filterable: true,
                     disabled: true,
                     others: {
-                      resizable: false,
+                      // resizable: false,
                       style: { textAlign: "center" },
                     },
                     editorTemplate: (row) => {
@@ -400,121 +383,27 @@ function HassanNumber() {
                   {
                     fieldName: "hassan_number_updated_date",
                     label: (
-                      <AlgaehLabel
-                        label={{ forceLabel: "Hassan No. Updated Date" }}
-                      />
+                      <AlgaehLabel label={{ forceLabel: "Updated Date" }} />
                     ),
                     sortable: true,
                     filterable: true,
                     disabled: true,
                     others: {
-                      resizable: false,
+                      // resizable: false,
                       style: { textAlign: "center" },
                     },
                     editorTemplate: (row) => {
                       return row.hassan_number_updated_date;
                     },
                   },
-
-                  {
-                    fieldName: "status",
-                    label: <AlgaehLabel label={{ fieldName: "status" }} />,
-                    displayTemplate: (row) => {
-                      return row.status === "CL" ? (
-                        <span className="badge badge-secondary">Collected</span>
-                      ) : row.status === "CN" ? (
-                        <span className="badge badge-danger">Cancelled</span>
-                      ) : row.status === "CF" ? (
-                        <span className="badge badge-primary">Confirmed</span>
-                      ) : (
-                        <span className="badge badge-success">Validated</span>
-                      );
-                    },
-                    editorTemplate: (row) => {
-                      return row.status === "CL" ? (
-                        <span className="badge badge-secondary">Collected</span>
-                      ) : row.status === "CN" ? (
-                        <span className="badge badge-danger">Cancelled</span>
-                      ) : row.status === "CF" ? (
-                        <span className="badge badge-primary">Confirmed</span>
-                      ) : (
-                        <span className="badge badge-success">Validated</span>
-                      );
-                    },
-                    // sortable: true,
-                    // filterable: true,
-                    disabled: true,
-                    others: {
-                      maxWidth: 130,
-                      resizable: false,
-                      style: { textAlign: "center" },
-                    },
-                  },
-                  // {
-                  //   fieldName: "critical_status",
-                  //   label: (
-                  //     <AlgaehLabel
-                  //       label={{ forceLabel: "Critical Result" }}
-                  //     />
-                  //   ),
-                  //   displayTemplate: (row) => {
-                  //     return row.critical_status === "N" ? (
-                  //       <span className="badge badge-primary">No</span>
-                  //     ) : (
-                  //       <span className="badge badge-danger">Yes</span>
-                  //     );
-                  //   },
-                  //   disabled: true,
-                  //   others: {
-                  //     maxWidth: 130,
-                  //     resizable: false,
-                  //     style: { textAlign: "center" },
-                  //   },
-                  // },
                 ]}
                 keyId="patient_code"
-                // dataSource={{
                 data={isPCRRecords ?? []}
-                // data: this.state.sample_collection
-                // }}
-                // filter={true}
-
                 isEditable={"editOnly"}
                 events={{
-                  // onCancel: (row) => {},
-                  // onDeleteShow: (row) => {},
-                  // onSaveShow: (row) => {},
-                  // onEdit: (row) => {
-                  //   setDisabledSave(true);
-                  // },
                   onSave: (row) => {
                     updateHassanNo(row);
                   },
-                  // onDelete: (row) => {
-                  //   if (row.isInserted === 1) {
-                  //     onDeleteDetails(row);
-                  //     setWardDetailsData((data: any) => {
-                  //       const otherDetails = data.filter(
-                  //         (f: any) =>
-                  //           f.hims_adm_ward_detail_id !==
-                  //           row["hims_adm_ward_detail_id"]
-                  //       );
-                  //       return [...otherDetails];
-                  //     });
-                  //     getWardHeaderData();
-                  //   } else {
-                  //     setWardDetailsData((data: any) => {
-                  //       const otherDetails = data
-                  //         .filter(
-                  //           (f: any) => f.rIndex !== row["rIndex"]
-                  //         )
-                  //         .map((m: any, i: number) => {
-                  //           return { ...m, rIndex: i + 1 };
-                  //         });
-                  //       return [...otherDetails];
-                  //     });
-                  //   }
-                  // },
                 }}
                 isFilterable={true}
                 pagination={true}
