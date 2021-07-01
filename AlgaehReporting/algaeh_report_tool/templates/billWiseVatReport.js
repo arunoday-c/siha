469e801ca78181793784cda97b8905f6d500529d
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2021-07-01 23:02:31 +0530

    Bill wise vat report bug - fixed

--- AlgaehReporting/algaeh_report_tool/templates/billWiseVatReport.js
+++ AlgaehReporting/algaeh_report_tool/templates/billWiseVatReport.js
@@ -29,9 +29,11 @@ const executePDF = function executePDFMethod(options) {
 
       options.mysql
         .executeQuery({
-          query: `SELECT BH.bill_number as doc_number,BH.bill_date as bill_date, V.visit_date, P.hims_d_patient_id as patient_id,P.full_name, P.patient_code,  N.nationality,
+          query: `SELECT BH.bill_number as doc_number,BH.bill_date as bill_date, V.visit_date, P.hims_d_patient_id as patient_id,P.full_name, 
+          P.patient_code,  N.nationality,
           CASE WHEN BD.insurance_yesno='Y' THEN 'Insurance' else 'Cash' END as insurance_yesno,"Billing" as data_from, 
-          BD.net_amout as total_before_vat,BD.total_tax,BD.patient_tax,BD.company_tax,(coalesce(BD.patient_payable,0)+coalesce(BD.company_payble,0)) as total_after_vat, S.service_name,BH.hims_f_billing_header_id as bill_id, SI.insurance_sub_name as company_name
+          BD.net_amout as total_before_vat,BD.total_tax,BD.patient_tax,BD.company_tax,(coalesce(BD.patient_payable,0)+coalesce(BD.company_payble,0)) as total_after_vat, 
+          S.service_name,BH.hims_f_billing_header_id as bill_id, SI.insurance_sub_name as company_name
             FROM hims_f_billing_details as BD
             left join hims_f_billing_header BH on BD.hims_f_billing_header_id = BH.hims_f_billing_header_id
             inner join hims_f_patient P on P.hims_d_patient_id = BH.patient_id
@@ -41,16 +43,18 @@ const executePDF = function executePDFMethod(options) {
             left join hims_m_patient_insurance_mapping PI on BH.visit_id = PI.patient_visit_id
             left join hims_d_insurance_sub SI on PI.primary_sub_id = SI.hims_d_insurance_sub_id
             where BD.cancel_yes_no='N' and adjusted='N' and date(BH.bill_date) between date(?) and date(?) and BH.hospital_id=? ${strData};\
+            
             SELECT PH.pos_number as doc_number,PH.pos_date as bill_date, V.visit_date, P.hims_d_patient_id as patient_id,
-            P.full_name, P.patient_code, N.nationality,CASE WHEN PD.insurance_yesno='Y' THEN 'Insurance' else 'Cash' END as insurance_yesno,"Pharmacy" as data_from,
+            P.full_name, P.patient_code, N.nationality,
+            CASE WHEN PD.insurance_yesno='Y' THEN 'Insurance' else 'Cash' END as insurance_yesno,"Pharmacy" as data_from,
             PD.net_extended_cost as total_before_vat,  (coalesce(PD.patient_payable,0)+coalesce(PD.company_payable,0)) as total_after_vat,
             (PD.patient_tax + PD.company_tax) as total_tax,PD.patient_tax,PD.company_tax,S.service_name,PH.hims_f_pharmacy_pos_header_id as bill_id, SI.insurance_sub_name as company_name
             FROM hims_f_pharmacy_pos_detail as PD
             left join hims_f_pharmacy_pos_header PH on PD.pharmacy_pos_header_id = PH.hims_f_pharmacy_pos_header_id
             left join hims_f_patient P on P.hims_d_patient_id = PH.patient_id
             left join  hims_f_patient_visit V on PH.visit_id = hims_f_patient_visit_id
-            inner join hims_d_nationality N on N.hims_d_nationality_id = P.nationality_id
-            inner join hims_d_services S on PD.service_id=S.hims_d_services_id
+            left join hims_d_nationality N on N.hims_d_nationality_id = P.nationality_id
+            left join hims_d_services S on PD.service_id=S.hims_d_services_id
             left join hims_m_patient_insurance_mapping PI on PH.visit_id = PI.patient_visit_id
             left join hims_d_insurance_sub SI on PI.primary_sub_id = SI.hims_d_insurance_sub_id
             where PH.cancelled='N' and PH.posted='Y'  and date(PH.pos_date) between date(?) and date(?) and PH.hospital_id=? ${strData};`,
@@ -101,7 +105,6 @@ const executePDF = function executePDFMethod(options) {
               };
             })
             .value();
-          console.log(final_result);
           resolve({
             result: BillWise,
             sum_total_before_vat: _.sumBy(final_result, (s) =>
