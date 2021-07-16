1ed7fc6b69462568e0458753583a76b1c1124d5d
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2021-07-16 16:19:24 +0530

    specimen barcode change from investigation name to service name.

--- AlgaehReporting/algaeh_report_tool/templates/specimenBarcode.js
+++ AlgaehReporting/algaeh_report_tool/templates/specimenBarcode.js
@@ -11,13 +11,14 @@ const executePDF = function executePDFMethod(options) {
       });
       options.mysql
         .executeQuery({
-          query: `SELECT INV.description,LS.barcode_gen,LO.lab_id_number,P.date_of_birth, P.full_name,P.patient_code, 
+          query: `SELECT S.service_name,LS.barcode_gen,LO.lab_id_number,P.date_of_birth, P.full_name,P.patient_code, 
           age_in_years, age_in_months,age_in_days,gender,LC.color_name,LC.color_code,INV.isPCR,LO.hassan_number
           FROM  hims_f_lab_order LO 
           inner join hims_f_lab_sample LS on LO.hims_f_lab_order_id = LS.order_id 
           inner join hims_f_patient P on P.hims_d_patient_id = LO.patient_id 
           inner join hims_f_patient_visit V on LO.visit_id = V.hims_f_patient_visit_id
           inner join hims_d_investigation_test INV on INV.hims_d_investigation_test_id=LO.test_id
+          inner join hims_d_services S on S.hims_d_services_id=LO.service_id
           inner join hims_d_lab_container LC on LC.hims_d_lab_container_id=LS.container_id
           where LO.hims_f_lab_order_id=?;`,
           values: [input.hims_f_lab_order_id],
