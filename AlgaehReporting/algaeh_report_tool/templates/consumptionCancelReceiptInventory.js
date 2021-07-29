464e8bd7d7f64862515fbf285b7d71ecd7a483be
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2021-07-29 17:12:45 +0530

    consumption cancel report added.

---
+++ AlgaehReporting/algaeh_report_tool/templates/consumptionCancelReceiptInventory.js
@@ -0,0 +1,21 @@
+const executePDF = function executePDFMethod(options) {
+  return new Promise(function (resolve, reject) {
+    try {
+      if (options.result.length > 0) {
+        resolve({
+          employee_code: options.result[0].employee_code,
+          full_name: options.result[0].full_name,
+          can_consumption_number: options.result[0].can_consumption_number,
+          can_consumption_date: options.result[0].can_consumption_date,
+          location_description: options.result[0].location_description,
+          detailList: options.result,
+        });
+      } else {
+        resolve({ detailList: options.result });
+      }
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+module.exports = { executePDF };
