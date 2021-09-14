bb0016e9bb740699d6373d980d427e551565a5b0
Nowshad <nowshad.algaeh@gmail.com>
2021-09-14 10:27:22 +0530

    Consumption Entry After save cancel conumption it should get clear and disable cancel btn

--- client/src/Components/Inventory/InvConsumptionEntry/InvConsumptionEntryEvents.js
+++ client/src/Components/Inventory/InvConsumptionEntry/InvConsumptionEntryEvents.js
@@ -1,9 +1,6 @@
 import AlgaehLoader from "../../Wrapper/fullPageLoader";
 import ConsumptionIOputs from "../../../Models/InventoryConsumption";
-import {
-  algaehApiCall,
-  swalMessage
-} from "../../../utils/algaehApiCall";
+import { algaehApiCall, swalMessage } from "../../../utils/algaehApiCall";
 
 export default function ConsumptionItemsEvents() {
   return {
@@ -17,23 +14,24 @@ export default function ConsumptionItemsEvents() {
           module: "inventory",
           method: "GET",
           data: { consumption_number: docNumber },
-          onSuccess: response => {
+          onSuccess: (response) => {
             if (response.data.success === true) {
               let data = response.data.records;
               data.saveEnable = true;
 
               data.addedItem = true;
               data.ItemDisable = true;
+              data.addItemButton = true;
               $this.setState(data);
               AlgaehLoader({ show: false });
             }
           },
-          onFailure: err => {
+          onFailure: (err) => {
             swalMessage({
               title: err.message,
-              type: "error"
+              type: "error",
             });
-          }
+          },
         });
       });
     },
@@ -46,57 +44,60 @@ export default function ConsumptionItemsEvents() {
         module: "inventory",
         method: "GET",
         data: { transaction_id: transaction_id },
-        onSuccess: response => {
+        onSuccess: (response) => {
           if (response.data.success === true) {
             let data = response.data.records;
             data.saveEnable = true;
 
             data.addedItem = true;
             data.ItemDisable = true;
+            data.addItemButton = true;
             $this.setState(data);
             AlgaehLoader({ show: false });
           }
         },
-        onFailure: err => {
+        onFailure: (err) => {
           swalMessage({
             title: err.message,
-            type: "error"
+            type: "error",
           });
-        }
+        },
       });
     },
 
-    ClearData: $this => {
+    ClearData: ($this) => {
       let IOputs = ConsumptionIOputs.inputParam();
       $this.setState(IOputs);
     },
 
-    SaveConsumptionEntry: $this => {
+    SaveConsumptionEntry: ($this) => {
       AlgaehLoader({ show: true });
       $this.state.ScreenCode = "INV0007";
       algaehApiCall({
         uri: "/inventoryconsumption/addInventoryConsumption",
         module: "inventory",
         data: $this.state,
-        onSuccess: response => {
+        onSuccess: (response) => {
           if (response.data.success === true) {
             $this.setState({
               consumption_number: response.data.records.consumption_number,
               saveEnable: true,
+              ItemDisable: true,
+              addItemButton: true,
 
               hims_f_inventory_consumption_header_id:
                 response.data.records.hims_f_inventory_consumption_header_id,
               year: response.data.records.year,
-              period: response.data.records.period
+              period: response.data.records.period,
             });
 
             swalMessage({
               title: "Saved successfully . .",
-              type: "success"
+              type: "success",
             });
           }
           AlgaehLoader({ show: false });
-        }
+        },
       });
     },
     LocationchangeTexts: ($this, ctrl, e) => {
@@ -107,7 +108,7 @@ export default function ConsumptionItemsEvents() {
       $this.setState({
         [name]: value,
         location_type: e.selected.location_type,
-        location_name: e.selected.location_description
+        location_name: e.selected.location_description,
       });
     },
 
@@ -117,7 +118,7 @@ export default function ConsumptionItemsEvents() {
         method: "GET",
         module: "reports",
         headers: {
-          Accept: "blob"
+          Accept: "blob",
         },
         others: { responseType: "blob" },
         data: {
@@ -126,20 +127,20 @@ export default function ConsumptionItemsEvents() {
             reportParams: [
               {
                 name: "consumption_number",
-                value: consumption_number
-              }
+                value: consumption_number,
+              },
             ],
-            outputFileType: "PDF"
-          }
+            outputFileType: "PDF",
+          },
         },
-        onSuccess: res => {
+        onSuccess: (res) => {
           const urlBlob = URL.createObjectURL(res.data);
-          const reportName = `${consumption_number}-Consumption Receipt - Inventory`
+          const reportName = `${consumption_number}-Consumption Receipt - Inventory`;
           const origin = `${window.location.origin}/reportviewer/web/viewer.html?file=${urlBlob}&filename= ${reportName}`;
           window.open(origin);
           // window.document.title = "Consumption Receipt - Inventory";
-        }
+        },
       });
-    }
+    },
   };
 }
