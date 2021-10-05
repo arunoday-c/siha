70111e0e1913dfff0ee970bad28839cecd71a3d2
suhailkhan.algaeh <suhailkhan.algaeh@gmail.com>
2021-10-05 13:40:38 +0530

    customer and supplier list printng options

--- AlgaehReporting/algaeh_report_tool/templates/customerStatementSummary.js
+++ AlgaehReporting/algaeh_report_tool/templates/customerStatementSummary.js
@@ -17,91 +17,129 @@ const executePDF = function executePDFMethod(options) {
       //     str += ` and S.hospital_id=${input.hospital_id}`;
       //   }
 
+      // options.mysql
+      //   .executeQuery({
+      //     query: `select FC.child_name,H.finance_voucher_header_id,H.day_end_header_id,
+      //       round(H.amount ,${decimal_places}) as invoice_amount,
+      //       round(H.amount-coalesce(FSH.amount,settled_amount),${decimal_places}) as balance_amount,
+      //       round(coalesce(FSH.amount,settled_amount),${decimal_places}) as settled_amount,
+      //       coalesce(FSH.invoice_ref_no, invoice_no) as invoice_no ,
+      //       H.voucher_type,H.narration,
+      //       H.payment_date as invoice_date,
+      //       due_date, H.updated_date as last_modified,
+      //       case when settlement_status  ='S' then 'closed'
+      //       when settlement_status='P' and curdate()> due_date then 'Over Due'
+      //       when settlement_status='P' and settled_amount <> H.amount then 'Open'
+      //       when settlement_status='P' and settled_amount = H.amount then 'Paid' end as invoice_status,
+      //       D.child_id,D.head_id, D.is_opening_bal, H.voucher_no
+      //       from finance_voucher_header H
+      //       inner join finance_voucher_details D on
+      //       H.finance_voucher_header_id=D.voucher_header_id
+      //       and H.voucher_type='sales' and H.invoice_no is not null
+      //       left join finance_voucher_sub_header FSH on
+      //       H.invoice_no = FSH.invoice_ref_no
+      //       left join finance_account_child FC on FC.finance_account_child_id = D.child_id
+      //       where D.child_id in(?) and date(H.payment_date) between date(?) and date(?)`,
+      //     values: [input.childIds, input.from_date, input.to_date],
+      //     printQuery: true,
+      //   })
+      //   .then((results) => {
+      //     if (results.length > 0) {
+      //       const rptResult = _.chain(results)
+      //         .groupBy((g) => g.invoice_no)
+      //         .map((item, key) => {
+      //           const header = _.head(item);
+      //           const settled_amount = _.sumBy(item, (s) =>
+      //             parseFloat(s.settled_amount)
+      //           );
+      //           const balance_amount =
+      //             parseFloat(header ? header.invoice_amount : "0") -
+      //             settled_amount;
+
+      //           return {
+      //             ...header,
+      //             settled_amount,
+      //             balance_amount,
+      //             // child_name,
+      //           };
+      //         })
+      //         .orderBy((o) => o.invoice_date, "desc")
+      //         .value();
+      //       const overDueAmt = _.sumBy(rptResult, (s) => {
+      //         if (s.invoice_status === "Over Due") {
+      //           return parseFloat(s.balance_amount);
+      //         } else {
+      //           return 0.0;
+      //         }
+      //       });
+
+      //       resolve({
+      //         detail: rptResult,
+      //         child_name: results[0].child_name,
+      //         total_invoice_amount: _.sumBy(rptResult, (s) =>
+      //           parseFloat(s.invoice_amount)
+      //         ),
+      //         total_settled_amount: _.sumBy(rptResult, (s) =>
+      //           parseFloat(s.settled_amount)
+      //         ),
+      //         total_amount: _.sumBy(rptResult, (s) =>
+      //           parseFloat(s.balance_amount)
+      //         ),
+      //         overDueAmt: overDueAmt,
+      //         decimalOnly: {
+      //           decimal_places,
+      //           addSymbol: false,
+      //           symbol_position,
+      //           currency_symbol,
+      //         },
+      //         currencyOnly: {
+      //           decimal_places,
+      //           addSymbol: true,
+      //           symbol_position,
+      //           currency_symbol,
+      //         },
+      //       });
+      //     } else {
+      //       resolve({});
+      //     }
+      //   })
       options.mysql
         .executeQuery({
-          query: `select FC.child_name,H.finance_voucher_header_id,H.day_end_header_id,
-            round(H.amount ,${decimal_places}) as invoice_amount,
-            round(H.amount-coalesce(FSH.amount,settled_amount),${decimal_places}) as balance_amount,
-            round(coalesce(FSH.amount,settled_amount),${decimal_places}) as settled_amount,
-            coalesce(FSH.invoice_ref_no, invoice_no) as invoice_no ,
-            H.voucher_type,H.narration,
-            H.payment_date as invoice_date,
-            due_date, H.updated_date as last_modified,
-            case when settlement_status  ='S' then 'closed'
-            when settlement_status='P' and curdate()> due_date then 'Over Due'
-            when settlement_status='P' and settled_amount <> H.amount then 'Open'
-            when settlement_status='P' and settled_amount = H.amount then 'Paid' end as invoice_status,
-            D.child_id,D.head_id, D.is_opening_bal, H.voucher_no
-            from finance_voucher_header H
-            inner join finance_voucher_details D on
-            H.finance_voucher_header_id=D.voucher_header_id
-            and H.voucher_type='sales' and H.invoice_no is not null  and  D.child_id=?
-            left join finance_voucher_sub_header FSH on
-            H.invoice_no = FSH.invoice_ref_no 
-            left join finance_account_child FC on FC.finance_account_child_id = D.child_id            
-            where D.child_id in() and date(H.payment_date) between date(?) and date(?)`,
-          values: [input.child_id, input.from_date, input.to_date],
+          query: `select C.finance_account_child_id ,C.ledger_code,C.child_name,VD.is_opening_bal,customer_type,
+          ROUND( (coalesce(sum(debit_amount) ,0.0000)- coalesce(sum(credit_amount) ,0.0000)),${decimal_places})
+         as balance_amount from finance_account_child C left join finance_voucher_details VD
+         on C.finance_account_child_id=VD.child_id and VD.auth_status='A'  where  finance_account_child_id in 
+  (?) group by C.finance_account_child_id;
+`,
+          values: [input.childIds],
           printQuery: true,
         })
         .then((results) => {
+          console.log("results", results);
           if (results.length > 0) {
-            const rptResult = _.chain(results)
-              .groupBy((g) => g.invoice_no)
-              .map((item, key) => {
-                const header = _.head(item);
-                const settled_amount = _.sumBy(item, (s) =>
-                  parseFloat(s.settled_amount)
-                );
-                const balance_amount =
-                  parseFloat(header ? header.invoice_amount : "0") -
-                  settled_amount;
-
-                return {
-                  ...header,
-                  settled_amount,
-                  balance_amount,
-                  // child_name,
-                };
-              })
-              .orderBy((o) => o.invoice_date, "desc")
-              .value();
-            const overDueAmt = _.sumBy(rptResult, (s) => {
-              if (s.invoice_status === "Over Due") {
-                return parseFloat(s.balance_amount);
-              } else {
-                return 0.0;
-              }
-            });
-
             resolve({
-              detail: rptResult,
-              child_name: results[0].child_name,
-              total_invoice_amount: _.sumBy(rptResult, (s) =>
-                parseFloat(s.invoice_amount)
-              ),
-              total_settled_amount: _.sumBy(rptResult, (s) =>
-                parseFloat(s.settled_amount)
-              ),
-              total_amount: _.sumBy(rptResult, (s) =>
-                parseFloat(s.balance_amount)
-              ),
-              overDueAmt: overDueAmt,
+              detail: results,
+              // total_amount: _.sumBy(rptResult, (s) =>
+              //   parseFloat(s.balance_amount)
+              // ),
               decimalOnly: {
                 decimal_places,
-                // addSymbol: false,
-                symbol_position,
-                currency_symbol,
-              },
-              currencyOnly: {
-                decimal_places,
-                addSymbol: true,
+                addSymbol: false,
                 symbol_position,
                 currency_symbol,
               },
             });
           } else {
-            resolve({});
+            resolve({
+              currencyOnly: {
+                decimal_places,
+                addSymbol: false,
+                symbol_position,
+                currency_symbol,
+              },
+            });
           }
+          //   console.log("rptResult", rptResult);
         })
         .catch((error) => {
           options.mysql.releaseConnection();
--- algaeh-finance/client/src/Components/SupplierListFinance/index.js
+++ algaeh-finance/client/src/Components/SupplierListFinance/index.js
@@ -1,11 +1,21 @@
-import React, { memo, useState, useEffect } from "react";
+import React, { memo, useState, useEffect, useRef } from "react";
 import { useHistory } from "react-router-dom";
-import { AlgaehMessagePop, AlgaehTable } from "algaeh-react-components";
+import {
+  AlgaehMessagePop,
+  AlgaehTable,
+  AlgaehButton,
+} from "algaeh-react-components";
 import { LoadSupplierPayable } from "./event";
 import { InfoBar } from "../../Wrappers";
 import { getAmountFormart } from "../../utils/GlobalFunctions";
-
+import ModalPrintCustomerAndSupplier from "../CustomerListFinance/ModalPrintCustomerAndSupplier";
+const STATUS = {
+  CHECK: true,
+  UNCHECK: false,
+  INDETERMINATE: true,
+};
 function CustomerList(props) {
+  let allChecked = useRef(undefined);
   const [supplier_payable, setSupplierPayable] = useState([]);
   const history = useHistory();
   const [info, setInfo] = useState({
@@ -13,7 +23,9 @@ function CustomerList(props) {
     total_receivable: "",
     day_end_pending: "",
   });
-
+  const [checkAll, setCheckAll] = useState(STATUS.UNCHECK);
+  const [visible, setVisible] = useState(false);
+  const [childIds, setChildIds] = useState("");
   useEffect(() => {
     LoadSupplierPayable()
       .then((data) => {
@@ -32,101 +44,192 @@ function CustomerList(props) {
       });
   }, []);
 
+  const bulkPrintReport = () => {
+    const data = supplier_payable;
+
+    const filterData = data.filter((f) => f.checked);
+
+    const childIdsForReport = filterData.map((item) => {
+      return item.finance_account_child_id;
+    });
+
+    if (childIdsForReport.length > 0) {
+      setVisible(true);
+      setChildIds(childIdsForReport);
+    }
+  };
+  const selectAll = (e) => {
+    const status = e.target.checked;
+    const myState = supplier_payable.map((f) => {
+      return {
+        ...f,
+        checked: status,
+      };
+    });
+
+    const hasUncheck = myState.filter((f) => {
+      return f.checked === undefined || f.checked === false;
+    });
+
+    const totalRecords = myState.length;
+    setCheckAll(
+      totalRecords === hasUncheck.length
+        ? "UNCHECK"
+        : hasUncheck.length === 0
+        ? "CHECK"
+        : "INDETERMINATE"
+    );
+    setSupplierPayable([...myState]);
+  };
+  const selectToPrintReport = (row, e) => {
+    const status = e.target.checked;
+    row.checked = status;
+    const records = supplier_payable;
+    const hasUncheck = records.filter((f) => {
+      return f.checked === undefined || f.checked === false;
+    });
+
+    const totalRecords = records.length;
+    let ckStatus =
+      totalRecords === hasUncheck.length
+        ? "UNCHECK"
+        : hasUncheck.length === 0
+        ? "CHECK"
+        : "INDETERMINATE";
+    if (ckStatus === "INDETERMINATE") {
+      allChecked.indeterminate = true;
+    } else {
+      allChecked.indeterminate = false;
+    }
+    setCheckAll(ckStatus);
+    setSupplierPayable([...records]);
+  };
   return (
-    <div className="row">
-      <div className="col-12">
-        <InfoBar data={info} />
-        <div className="row">
-          <div className="col-12">
-            <div className="portlet portlet-bordered margin-bottom-15">
-              <div className="portlet-title">
-                <div className="caption">
-                  <h3 className="caption-subject">Supplier List</h3>
+    <>
+      <div className="row">
+        <div className="col-12">
+          <InfoBar data={info} />
+          <div className="row">
+            <div className="col-12">
+              <div className="portlet portlet-bordered margin-bottom-15">
+                <div className="portlet-title">
+                  <div className="caption">
+                    <h3 className="caption-subject">Supplier List</h3>
+                  </div>
+                  <div className="actions">
+                    <button
+                      className="btn btn-primary"
+                      onClick={() => {
+                        history.push("VendorSetup", { data: "placeholder" });
+                      }}
+                    >
+                      <i className="fas fa-plus"></i>
+                    </button>
+                  </div>
                 </div>
-                <div className="actions">
-                  <button
-                    className="btn btn-primary"
-                    onClick={() => {
-                      history.push("VendorSetup", { data: "placeholder" });
-                    }}
-                  >
-                    <i className="fas fa-plus"></i>
-                  </button>
-                </div>
-              </div>
-              <div className="portlet-body">
-                <div className="row">
-                  <div
-                    className="col-lg-12 customCheckboxGrid"
-                    id="supplierGrid_Cntr"
-                  >
-                    <AlgaehTable
-                      columns={[
-                        {
-                          label: "Supplier / Company",
-                          sortable: true,
-                          filterable: true,
-                          fieldName: "child_name",
-                          filtered: true,
-                          displayTemplate: (record) => {
-                            return (
-                              <p
-                                className="p-link"
-                                // type="link"
-                                onClick={() =>
-                                  history.push("/SupplierPayment", {
-                                    data: record,
-                                  })
+                <div className="portlet-body">
+                  <div className="row">
+                    <div
+                      className="col-lg-12 customCheckboxGrid"
+                      id="supplierGrid_Cntr"
+                    >
+                      <AlgaehTable
+                        columns={[
+                          {
+                            label: (
+                              <input
+                                type="checkbox"
+                                defaultChecked={
+                                  checkAll === "CHECK" ? true : false
                                 }
-                              >
-                                {record.child_name}
-                              </p>
-                            );
+                                ref={(input) => {
+                                  allChecked = input;
+                                }}
+                                onChange={selectAll}
+                              />
+                            ),
+                            fieldName: "select",
+                            displayTemplate: (row) => {
+                              return (
+                                <input
+                                  type="checkbox"
+                                  checked={row.checked}
+                                  onChange={(e) => selectToPrintReport(row, e)}
+                                />
+                              );
+                            },
+                            others: {
+                              maxWidth: 50,
+                              filterable: false,
+                              sortable: false,
+                            },
                           },
-                        },
-                        {
-                          label: "Contact Number",
-                          sortable: true,
-                          filterable: true,
-                          fieldName: "contact_number",
-                          others: {
-                            width: 200,
+                          {
+                            label: "Supplier / Company",
+                            sortable: true,
+                            filterable: true,
+                            fieldName: "child_name",
+                            filtered: true,
+                            displayTemplate: (record) => {
+                              return (
+                                <p
+                                  className="p-link"
+                                  // type="link"
+                                  onClick={() =>
+                                    history.push("/SupplierPayment", {
+                                      data: record,
+                                    })
+                                  }
+                                >
+                                  {record.child_name}
+                                </p>
+                              );
+                            },
                           },
-                        },
-                        {
-                          label: "Account Number",
-                          sortable: true,
-                          filterable: true,
-                          fieldName: "bank_account_no",
-                          others: {
-                            width: 200,
+                          {
+                            label: "Contact Number",
+                            sortable: true,
+                            filterable: true,
+                            fieldName: "contact_number",
+                            others: {
+                              width: 200,
+                            },
                           },
-                        },
-                        {
-                          label: "Balance",
-                          sortable: true,
-                          displayTemplate: (row) => {
-                            return (
-                              <span>
-                                {getAmountFormart(row.balance_amount, {
-                                  appendSymbol: false,
-                                })}
-                              </span>
-                            );
+                          {
+                            label: "Account Number",
+                            sortable: true,
+                            filterable: true,
+                            fieldName: "bank_account_no",
+                            others: {
+                              width: 200,
+                            },
                           },
-                          filterable: true,
-                          fieldName: "balance_amount",
-                          others: {
-                            width: 200,
+                          {
+                            label: "Balance",
+                            sortable: true,
+                            displayTemplate: (row) => {
+                              return (
+                                <span>
+                                  {getAmountFormart(row.balance_amount, {
+                                    appendSymbol: false,
+                                  })}
+                                </span>
+                              );
+                            },
+                            filterable: true,
+                            fieldName: "balance_amount",
+                            others: {
+                              width: 200,
+                            },
                           },
-                        },
-                      ]}
-                      height="80vh"
-                      isFilterable={true}
-                      rowUniqueId="finance_voucher_header_id"
-                      // dataSource={{ data: supplier_payable }}
-                      data={supplier_payable}
-                    />
+                        ]}
+                        height="80vh"
+                        isFilterable={true}
+                        rowUniqueId="finance_voucher_header_id"
+                        // dataSource={{ data: supplier_payable }}
+                        data={supplier_payable}
+                      />
+                    </div>
                   </div>
                 </div>
               </div>
@@ -134,7 +237,35 @@ function CustomerList(props) {
           </div>
         </div>
       </div>
-    </div>
+      <div className="hptl-phase1-footer">
+        <div className="row">
+          <div className="col-12">
+            <AlgaehButton
+              className="btn btn-default"
+              // disabled={!processList.length}
+              // loading={loading}
+              onClick={() => bulkPrintReport()}
+            >
+              Print
+            </AlgaehButton>
+          </div>
+        </div>
+      </div>
+      {visible ? (
+        <ModalPrintCustomerAndSupplier
+          title="Supplier Report"
+          visible={visible}
+          screenFrom="SUPPLIER"
+          childIds={childIds}
+          onCancel={() => {
+            setVisible(false);
+          }}
+          onOk={() => {
+            setVisible(false);
+          }}
+        />
+      ) : null}
+    </>
   );
 }
 
