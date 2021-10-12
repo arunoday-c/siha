5b758f57d534e7c57dce1ca8363ed7d5e936a183
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2021-10-12 09:28:48 +0530

    Added Advance Button

--- algaeh-finance/client/src/Components/CustomerListFinance/CustomerPayment/index.js
+++ algaeh-finance/client/src/Components/CustomerListFinance/CustomerPayment/index.js
@@ -823,7 +823,14 @@ export default memo(function (props) {
               loading={loading}
               onClick={onClickCreditNotes}
             >
-              Include Credit Note
+              Add Credit Note
+            </AlgaehButton>
+            <AlgaehButton
+              className="btn btn-default"
+              loading={loading}
+              // onClick={onClickCreditNotes}
+            >
+              Add Advance
             </AlgaehButton>
             <AlgaehButton
               className="btn btn-default"
