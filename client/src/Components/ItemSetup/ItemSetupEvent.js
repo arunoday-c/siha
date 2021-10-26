aa2c0d79de552e792f6fe677c3172db293f75f38
Nowshad <nowshad.algaeh@gmail.com>
2021-10-26 14:30:55 +0530

    Pharmacy same stocking UOM repeating

--- client/src/Components/ItemSetup/ItemSetupEvent.js
+++ client/src/Components/ItemSetup/ItemSetupEvent.js
@@ -1,10 +1,12 @@
-import { algaehApiCall } from "../../utils/algaehApiCall";
+import { algaehApiCall, swalMessage } from "../../utils/algaehApiCall";
+import _ from "lodash";
+import Enumerable from "linq";
 
 export default function ItemSetupEvent() {
   return {
     getItems: ($this) => {
       $this.props.getItemMaster({
-        uri: "/pharmacy/getItemMasterAndItemUom",
+        uri: "/pharmacy/getItemMasterPharmacyData",
         module: "pharmacy",
         method: "GET",
         redux: {
@@ -14,12 +16,76 @@ export default function ItemSetupEvent() {
       });
     },
     EditItemMaster: ($this, row) => {
-      row.addNew = false;
-      $this.setState({
-        isOpen: !$this.state.isOpen,
-        itemPop: row,
-        addNew: false,
+      algaehApiCall({
+        uri: "/pharmacy/getItemMasterAndItemUom",
+        module: "pharmacy",
+        method: "GET",
+        data: {
+          hims_d_item_master_id: row.hims_d_item_master_id,
+        },
+        onSuccess: (response) => {
+          debugger;
+          if (response.data.success) {
+            let ItemList = Enumerable.from(response.data.records)
+              .groupBy("$.hims_d_item_master_id", null, (k, g) => {
+                let firstRecordSet = Enumerable.from(g).firstOrDefault();
+
+                return {
+                  item_code: firstRecordSet.item_code,
+                  hims_d_item_master_id: firstRecordSet.hims_d_item_master_id,
+                  item_description: firstRecordSet.item_description,
+                  generic_id: firstRecordSet.generic_id,
+                  category_id: firstRecordSet.category_id,
+                  group_id: firstRecordSet.group_id,
+                  form_id: firstRecordSet.form_id,
+                  storage_id: firstRecordSet.storage_id,
+                  item_uom_id: firstRecordSet.item_uom_id,
+                  purchase_uom_id: firstRecordSet.purchase_uom_id,
+                  sales_uom_id: firstRecordSet.sales_uom_id,
+                  stocking_uom_id: firstRecordSet.stocking_uom_id,
+                  item_status: firstRecordSet.item_status,
+                  radioActive:
+                    firstRecordSet.item_status === "A" ? true : false,
+                  radioInactive:
+                    firstRecordSet.item_status === "I" ? true : false,
+                  service_id: firstRecordSet.service_id,
+                  purchase_cost: firstRecordSet.purchase_cost,
+                  addl_information: firstRecordSet.addl_information,
+                  exp_date_required: firstRecordSet.exp_date_required,
+                  sfda_code: firstRecordSet.sfda_code,
+                  reorder_qty: firstRecordSet.reorder_qty,
+                  standard_fee: firstRecordSet.sales_price,
+                  vat_applicable: firstRecordSet.vat_applicable,
+                  vat_percent: firstRecordSet.vat_percent,
+                  detail_item_uom:
+                    firstRecordSet.hims_m_item_uom_id === null
+                      ? []
+                      : g.getSource(),
+                };
+              })
+              .toArray();
+            const _head = _.head(ItemList);
+            _head.addNew = false;
+            $this.setState({
+              isOpen: !$this.state.isOpen,
+              itemPop: _head,
+              addNew: false,
+            });
+          }
+        },
+        onError: (error) => {
+          swalMessage({
+            title: error.message,
+            type: "warning",
+          });
+        },
       });
+      // row.addNew = false;
+      // $this.setState({
+      //   isOpen: !$this.state.isOpen,
+      //   itemPop: row,
+      //   addNew: false,
+      // });
     },
     OpenReQtyLocation: ($this, row) => {
       algaehApiCall({
