cf0f388e8fa0e3c6b43e29e7c53f609f43713b1c
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2021-11-10 17:47:37 +0530

    Prescription showing wrong in Clinical Summary

--- client/src/Components/MRD/PatientMRD/HistoricalData/PrescriptionHistoryTable.jsx
+++ client/src/Components/MRD/PatientMRD/HistoricalData/PrescriptionHistoryTable.jsx
@@ -12,17 +12,18 @@ function PrescriptionHistory({ columnsArray, columnData }) {
       </thead>
       <tbody>
         {columnData?.map((item, index) => {
-          const { generic_name, start_date, instructions } = item;
+          const { generic_name, item_description, start_date, instructions } =
+            item;
 
           return (
             <tr key={index}>
-              {/* <td style={{ textAlign: "left", fontWeight: "bold" }}>
-                {item_description}
-              </td>{" "} */}
-              <td width="350" style={{ textAlign: "left", fontWeight: "bold" }}>
+              <td width="100">{start_date}</td>
+              <td style={{ textAlign: "left", fontWeight: "bold" }}>
                 {generic_name}
               </td>
-              <td width="100">{start_date}</td>
+              <td style={{ textAlign: "left", fontWeight: "bold" }}>
+                {item_description}
+              </td>
               <td style={{ textAlign: "left" }}>{instructions}</td>
             </tr>
           );
