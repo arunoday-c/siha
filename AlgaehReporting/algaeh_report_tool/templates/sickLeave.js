637a4143c84370973068b2c306c03b3110676ad9
Aboobacker Sidhiqe <sidhiqe.algeah@gmail.com>
2021-11-26 10:07:08 +0530

    sick leave report changes

--- AlgaehReporting/algaeh_report_tool/templates/sickLeave.js
+++ AlgaehReporting/algaeh_report_tool/templates/sickLeave.js
@@ -9,14 +9,6 @@ const executePDF = function executePDFMethod(options) {
       params.forEach((para) => {
         input[para["name"]] = para["value"];
       });
-
-      if (input.visit_id != null) {
-        str += " and H.visit_id=" + input.visit_id;
-      }
-      if (input.ip_id != null) {
-        str += " and H.ip_id=" + input.ip_id;
-      }
-      console.log(params);
       options.mysql
         .executeQuery({
           query: `select P.patient_code,P.full_name as patient_full_name,P.arabic_name as patient_arabaic_full_name,P.primary_id_no, P.registration_date, PV.visit_date,EC.comment, ID.icd_description, SDEP.sub_department_name,SDEP.arabic_sub_department_name, EMP.full_name, EMP.arabic_name,EMP.license_number,SL.from_date,SL.to_date,SL.no_of_days,SL.remarks,SL.diagnosis_data,
@@ -29,16 +21,13 @@ const executePDF = function executePDFMethod(options) {
           from hims_f_patient_sick_leave as SL
           left join hims_f_patient P on P.hims_d_patient_id = SL.patient_id
           left join hims_f_patient_visit PV on PV.hims_f_patient_visit_id = SL.visit_id
-          left join hims_adm_atd_admission as ADM on H.ip_id = ADM.hims_adm_atd_admission_id
           left join hims_f_episode_chief_complaint EC on EC.episode_id = SL.episode_id
           left join hims_f_patient_diagnosis PD on  PD.episode_id = SL.episode_id
           left join hims_d_icd ID on ID.hims_d_icd_id = PD.daignosis_id
           inner join hims_d_employee EMP on PV.doctor_id = EMP.hims_d_employee_id
           inner join hims_d_sub_department SDEP on PV.sub_department_id = SDEP.hims_d_sub_department_id
-          left join hims_d_sub_department SDEP on (SDEP.hims_d_sub_department_id = V.sub_department_id or SDEP.hims_d_sub_department_id = ADM.sub_department_id)
-
-          where SL.patient_id=? and SL.episode_id = ? ${str} ;`,
-          values: [input.patient_id, input.episode_id],
+          where SL.patient_id=? and SL.visit_id=? and SL.episode_id = ? ;`,
+          values: [input.patient_id, input.visit_id, input.episode_id],
           printQuery: true,
         })
         .then((result) => {
